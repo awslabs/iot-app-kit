6bb4b43aaab13e61234806bb6b6435b077d71069
Gareth Farrington <gcharles@amazon.com>
2021-11-19 13:57:17 -0800

    swap any for generics

--- packages/core/src/asset-modules/sitewise/session.ts
+++ packages/core/src/asset-modules/sitewise/session.ts
@@ -1,5 +1,13 @@
 import { Observable, Subscription } from 'rxjs';
-import { AssetHierarchyQuery, AssetModelQuery, AssetPropertyValueQuery, AssetQuery, AssetSummaryQuery } from './types';
+import {
+  AssetHierarchyQuery,
+  AssetModelQuery,
+  AssetPropertyValueQuery,
+  AssetQuery,
+  AssetSummaryQuery, isAssetHierarchyQuery,
+  isAssetModelQuery,
+  isAssetPropertyValueQuery, isAssetSummaryQuery
+} from './types';
 import { AssetSummary, DescribeAssetModelResponse } from '@aws-sdk/client-iotsitewise/dist-types';
 import { RequestProcessor } from './requestProcessor';
 import { AssetPropertyValue } from '@aws-sdk/client-iotsitewise';
@@ -18,27 +26,27 @@ export class SiteWiseAssetSession {
   public addRequest(query: AssetPropertyValueQuery, observer: (assetPropertyValue: AssetPropertyValue) => void): Subscription;
   public addRequest(query: AssetModelQuery, observer: (assetModel: DescribeAssetModelResponse) => void): Subscription;
   public addRequest(query: AssetHierarchyQuery, observer: (assetSummary: AssetSummary[]) => void): Subscription;
-  public addRequest(queryAny: any, observerAny:  (consumedType: any) => void): Subscription {
+  public addRequest<Result>(query: AssetQuery, observerAny:  (consumedType: Result) => void): Subscription {
     let observable: Observable<any>;
-    if ('assetModelId' in queryAny) {
+    if (isAssetModelQuery(query)) {
       observable = new Observable<DescribeAssetModelResponse>(observer => {
-        this.processor.getAssetModel(queryAny, observer);
+        this.processor.getAssetModel(query, observer);
       });
-    } else if ('propertyId' in queryAny) {
+    } else if (isAssetPropertyValueQuery(query)) {
       observable = new Observable<AssetPropertyValue>(observer => {
-        this.processor.getAssetPropertyValue(queryAny, observer);
+        this.processor.getAssetPropertyValue(query, observer);
       });
-    } else if (`assetHierarchyId` in queryAny) {
+    } else if (isAssetHierarchyQuery(query)) {
       throw "NotImplementedException"
-    } else if (`assetId` in queryAny) {
+    } else if (isAssetSummaryQuery(query)) {
       observable = new Observable<AssetSummary>(observer => {
-        this.processor.getAssetSummary(queryAny, observer);
+        this.processor.getAssetSummary(query, observer);
       });
     } else {
       throw "Unexpected request type: the type of the request object could not be determined"
     }
 
-    this.requestQueue.push({query: queryAny, observable: observable});
+    this.requestQueue.push({query: query, observable: observable});
     return observable.subscribe(observerAny);
   }
 
--- packages/core/src/asset-modules/sitewise/types.ts
+++ packages/core/src/asset-modules/sitewise/types.ts
@@ -6,17 +6,21 @@ export type AssetQuery = {}
 export type AssetSummaryQuery = AssetQuery & {
   assetId: string
 }
+export const isAssetSummaryQuery = (query: AssetQuery): query is AssetSummaryQuery => (query as AssetSummaryQuery).assetId != undefined;
 
 export type AssetModelQuery = AssetQuery & {
   assetModelId: string
 }
+export const isAssetModelQuery = (query: AssetQuery): query is AssetModelQuery => (query as AssetModelQuery).assetModelId != undefined;
 
 export type AssetPropertyValueQuery = AssetQuery & {
   assetId: string,
   propertyId: string
 }
+export const isAssetPropertyValueQuery = (query: AssetQuery): query is AssetPropertyValueQuery => (query as AssetPropertyValueQuery).propertyId != undefined;
 
 export type AssetHierarchyQuery = AssetQuery & {
   assetHierarchyId: string
 }
+export const isAssetHierarchyQuery = (query: AssetQuery): query is AssetHierarchyQuery => (query as AssetHierarchyQuery).assetHierarchyId != undefined;
 
