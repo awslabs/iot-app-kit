f7ff327557756cbbb262d7ed1ae695ece9152a7d
Gareth Farrington <gcharles@amazon.com>
2022-02-01 13:09:20 -0800

    more details on Session idea

--- packages/core/src/query/demo.ts
+++ packages/core/src/query/demo.ts
@@ -1,16 +1,52 @@
 import { Observer, PartialObserver, Subscriber, Unsubscribable } from "rxjs";
 import { DataStream } from '../data-module/types';
+import { Component, Prop } from '@stencil/core';
+import { DataModule } from '../../dist/types/data-module/types';
 
-// namespace for data modules
+// namespace for "Data Modules" - the functions that produce sessions on a Data Module
 namespace DataModules {
 
 }
 
+// namespace for holding queries
+namespace Query {
+
+}
+
+interface CloseableSession {
+  close(): void;
+}
+
+interface DataModuleSession extends CloseableSession {
+}
+
+interface SessionMetrics {
+  startRequest(requestId: string): void
+  endRequest(requestId: string, wasCached?: boolean): void
+  errorRequest(requestId: string, error: string): void
+  cancelRequest(requestId: string, error: string): void
+  // Return statistics on the session, time taken by each request, total time etc
+  statistics(): SessionStatistics;
+}
+
+type SessionStatistics = {
+  // we should look at how other frameworks handle this, e.g. the browsers network tab.
+  // entry for each request
+    // requestId
+    // start and end time
+    // total time taken
+    // result: error, cached, success, canceled
+    // any error message that was captured
+  // total requests
+  // total wall clock time used
+}
+
 /**
- * interface that returns a sessionized interface of a Data Module
+ *
  */
-interface Sessionizable<T> {
-  getSession(clients: Map<string, object>): T;
+interface AppKitComponentSession extends CloseableSession {
+  attachDataModuleSession(session: DataModuleSession): void;
+  getSessionMetrics(dataModuleName: string): SessionMetrics;
 }
 
 type TimeSeriesSettings = {
@@ -40,39 +76,50 @@ interface Query<T> {
    * @param session
    * @param props any additional parameters you want to pass into the build method
    */
-  build(clients: Map<string, object>, props?: Map<string, any>): Provider<T>;
+  build(session: AppKitComponentSession, props?: Map<string, any>): Provider<T>;
 }
 
 type QueryBuilder<T> = {
   (): Query<T>;
 }
 
-// declare root namespace
-namespace Query {
-}
-
 // Bananas IoT data module:
 class BananasIotServiceClient {
   static CLIENT_NAME: string = "BananasIotServiceClient";
+  getSession(session: AppKitComponentSession): BananasIotDataSession {
+    // get a metrics object for logging
+    let bananaSession = new BananasIotDataSession(this, session.getSessionMetrics("BananasIoTDataModule"));
+    // attach the session to the component scoped session so it gets close() calls
+    session.attachDataModuleSession(bananaSession);
+    return bananaSession;
+  }
 }
 
-class BananasIotDataSession {
-  private client:BananasIotServiceClient ;
+class BananasIotDataSession implements DataModuleSession {
+  private client: BananasIotServiceClient;
+  private metrics: SessionMetrics;
 
-  constructor(client: BananasIotServiceClient ) {
+  constructor(client: BananasIotServiceClient, metrics: SessionMetrics) {
     this.client = client;
+    this.metrics = metrics;
   }
 
   public fetchBananaData(variety: string, country: string): Promise<DataStream[]> {
+    // do metrics logging:
+    this.metrics.startRequest("r1");
+    this.metrics.endRequest("r1");
     return new Promise(() => []);
+  }
+
+  close(): void {
+    // terminate all requests in session
   }
 }
 
 namespace DataModules {
-  export class BananasIotData implements Sessionizable<BananasIotDataSession> {
-    getSession(clients: Map<string, object>): BananasIotDataSession {
-      return new BananasIotDataSession(clients.get(BananasIotServiceClient.CLIENT_NAME) as BananasIotServiceClient);
-    }
+  export function BananasIotData(session: AppKitComponentSession): BananasIotDataSession {
+    /** TK: how this function gets the data module is not described. It could do that a number of ways. **/
+    return new BananasIotServiceClient().getSession(session);
   }
 }
 
@@ -80,9 +127,9 @@ namespace DataModules {
 namespace Query {
   export function bananasIotQuery(bananaType: string, bananaCountry: string): Query<TimeSeriesChartData> {
     return {
-      build: function(clients: Map<string, object>, props: Map<string, any>): Provider<TimeSeriesChartData> {
+      build: function(session: AppKitComponentSession): Provider<TimeSeriesChartData> {
         // actualize a BananasIotDataSession from the client pool and the DataModules namespace
-        const bananasIot: BananasIotDataSession = new DataModules.BananasIotData().getSession(clients);
+        const bananasIot: BananasIotDataSession = DataModules.BananasIotData(session);
 
         return {
           subscribe: function (subscriber: PartialObserver<TimeSeriesChartData>) {
@@ -102,27 +149,46 @@ namespace Query {
   }
 }
 
+
 /**
  *  In the components layer
  */
 
-<iot-line-chart>
-  // a session is created for the component
-  let session: ModuleSession = AppKit.getSession();  // namespaces are not instances, so we will have to work this out
-
-  // get Query from the props on iot-line-chart
-  let query: Query<TimeSeriesChartData> = Query.bananasIotQuery("Cavandish", "Equador");
-
+@Component({
+  tag: 'base-iot-line-chart',
+  shadow: false,
+})
+export class BaseLineChart {
   // inside the iot-line-chart, convert the query to a provider:
-  let provider: Provider<TimeSeriesChartData> =  query.build(session);
+  @Prop() provider: Provider<TimeSeriesChartData>;
 
-  <base-line-chart>
-    // inside the base-iot-line-chart, subscribe to the data once:
-    provider.subscribe( {
-      next: (data:TimeSeriesChartData) => {}
-      // save data in props
-      // re-render
-    });
-  </base-line-chart>
+  private latestValue: TimeSeriesChartData;
 
-</iot-line-chart>
+  onWillMount(): void {
+    let subscription = this.provider.subscribe({next: value => {
+        this.latestValue = value;
+    }});
+  }
+
+  render() {
+    // render data from latestValue
+  }
+}
+
+@Component({
+  tag: 'iot-line-chart',
+  shadow: false,
+})
+export class IotLineChart {
+  @Prop() appKit: AppKit = appKit;
+
+  @Prop() session: AppKitComponentSession = this.appKit.newSession();
+
+  @Prop() query: Query<TimeSeriesChartData> = Query.bananasIotQuery("Cavandish", "Equador");
+
+  render() {
+    <base-line-chart
+      provider=query.build(session)
+    />
+  }
+}
