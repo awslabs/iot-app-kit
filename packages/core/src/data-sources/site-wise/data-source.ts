b2968832297961aa7121d6420b264c6bdaab6ee2
Norbert Nader <Norbert.Nader@gmail.com>
2022-02-04 10:01:35 -0800

    feat: increase sitewise default resolution mapping thresholds (#46)
    * feat: increase sitewise default resolution mapping thresholds
    
    Co-authored-by: Norbert Nader <nnader@amazon.com>

--- packages/core/src/data-sources/site-wise/data-source.ts
+++ packages/core/src/data-sources/site-wise/data-source.ts
@@ -11,9 +11,9 @@ import { RESOLUTION_TO_MS_MAPPING, SupportedResolutions } from './util/resolutio
 export const SITEWISE_DATA_SOURCE = 'site-wise';
 
 const DEFAULT_RESOLUTION_MAPPING = {
-  [MINUTE_IN_MS]: SupportedResolutions.ONE_MINUTE,
-  [HOUR_IN_MS]: SupportedResolutions.ONE_HOUR,
-  [DAY_IN_MS]: SupportedResolutions.ONE_DAY,
+  [MINUTE_IN_MS * 15]: SupportedResolutions.ONE_MINUTE,
+  [HOUR_IN_MS * 15]: SupportedResolutions.ONE_HOUR,
+  [DAY_IN_MS * 60]: SupportedResolutions.ONE_DAY,
 };
 
 const isSiteWiseResolution = (resolution: string | SupportedResolutions): resolution is SupportedResolutions => {
