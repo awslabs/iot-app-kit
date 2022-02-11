1ac0bb116b52a625a45035b81142220ee22b4173
Norbert Nader <nnader@amazon.com>
2022-02-11 13:45:22 -0800

    fix: backfill on requestBuffer tests

--- packages/core/src/testing/mock-data-source/types.d.ts
+++
@@ -1,6 +0,0 @@
-import { DataStreamInfo } from '@synchro-charts/core';
-import { DataStreamQuery } from '../../data-module/types';
-
-/**
- * Learn more about AWS IoT SiteWise assets at https://docs.aws.amazon.com/iot-sitewise/latest/userguide/industrial-asset-models.html
- */
