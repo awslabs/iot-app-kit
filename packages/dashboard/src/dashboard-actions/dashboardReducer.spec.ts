6d8aee8265e59f0876e5aa82aa6d71c9d55e940c
sydopy <106272253+sydopy@users.noreply.github.com>
2022-06-15 13:03:44 -0400

    Get rid of redundancies

--- packages/dashboard/src/dashboard-actions/dashboardReducer.spec.ts
+++ packages/dashboard/src/dashboard-actions/dashboardReducer.spec.ts
@@ -1,23 +1,16 @@
 import { dashboardReducer } from './dashboardReducer';
 import { onMoveAction } from './actions';
-import { Position } from '../types';
 
-describe('dashboardReducer', () => {
-  it('returns empty dashboard configuration when provided an empty dashboard', () => {
-    const x: number = 7;
-    const y: number = 3;
-    const origin: Position = { x, y };
-
-    expect(
-      dashboardReducer(
-        [],
-        onMoveAction({
-          widgetIds: [],
-          cellSize: 10,
-          position: origin,
-          prevPosition: origin,
-        })
-      )
-    ).toEqual([]);
-  });
+it('returns empty dashboard configuration when provided an empty dashboard', () => {
+  expect(
+    dashboardReducer(
+      [],
+      onMoveAction({
+        widgetIds: [],
+        cellSize: 10,
+        position: { x: 10, y: 10 },
+        prevPosition: { x: 14, y: 10 },
+      })
+    )
+  ).toEqual([]);
 });
