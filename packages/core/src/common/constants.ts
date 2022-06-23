807755df61f56f3fe223ee6a0b3eb2da7c211b5b
Jane <qianweiz@amazon.com>
2022-06-23 11:53:09 -0700

    fix eslint

--- packages/core/src/common/constants.ts
+++ packages/core/src/common/constants.ts
@@ -1,8 +1,4 @@
-export const minimumSeverity: number = 3;
-export const testSound = 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/858/outfoxing.mp3'
+export const minimumSeverity = 3;
+export const testSound = 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/858/outfoxing.mp3';
 
-
-export const testAudioSrc = ['./Cute-Chime-2.mp3',
-'./new-message-5.ogg',
-'./success-3.ogg',
-];
+export const testAudioSrc = ['./Cute-Chime-2.mp3', './new-message-5.ogg', './success-3.ogg'];
