a2a373098494eb451ab231c2bd8cfdff6400ad4a
Xiao(Bill) Li <limxiao@amazon.com>
2022-07-07 13:39:16 -0400

    fix(table): fix an issue when key in columnDefinition doesn't exist in TableItems (#146)
    fix: remove unused variables.

--- packages/table/src/table/index.tsx
+++ packages/table/src/table/index.tsx
@@ -5,14 +5,16 @@ import { TableProps } from '../utils';
 import { defaultI18nStrings, getDefaultColumnDefinitions } from '../utils/tableHelpers';
 
 export const Table: FunctionComponent<TableProps> = (props) => {
-  const { items, useCollectionOption = { sorting: {} }, columnDefinitions } = props;
-  const { collectionProps, propertyFilterProps } = useCollection(items, useCollectionOption);
+  const { items: initalItems, useCollectionOption = { sorting: {} }, columnDefinitions } = props;
+  const { items, collectionProps, propertyFilterProps } = useCollection(initalItems, useCollectionOption);
+  const { propertyFiltering } = useCollectionOption;
   return (
     <AWSUITable
       {...props}
+      items={items}
       {...collectionProps}
       columnDefinitions={getDefaultColumnDefinitions(columnDefinitions)}
-      filter={<PropertyFilter {...propertyFilterProps} i18nStrings={defaultI18nStrings} />}
+      filter={propertyFiltering && <PropertyFilter {...propertyFilterProps} i18nStrings={defaultI18nStrings} />}
     />
   );
 };
