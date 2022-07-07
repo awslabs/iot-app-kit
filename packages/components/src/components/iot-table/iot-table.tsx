6d7ec23e5c0005df29677c4160cd604051f80e2c
Xiao(Bill) Li <limxiao@amazon.com>
2022-07-07 14:59:57 -0400

    another fix for import

--- packages/components/src/components/iot-table/iot-table.tsx
+++ packages/components/src/components/iot-table/iot-table.tsx
@@ -1,4 +1,4 @@
-import { Component, Listen, Prop, State, Watch } from '@stencil/core';
+import { Component, Listen, Prop, State, Watch, h } from '@stencil/core';
 import { Annotations, getThresholds } from '@synchro-charts/core';
 import {
   combineProviders,
