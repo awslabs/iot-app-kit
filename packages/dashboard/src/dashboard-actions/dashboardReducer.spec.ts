9f5b73d7328a5615ce2c2d12207d377ce8271c40
Sydney Opyrchal <sydopy@amazon.com>
2022-07-07 12:47:03 -0400

    Create Dashboard Wrapper (#141)
    Feature: dashboard copy and paste widgets (#139)
    
    Feature: Implement z-index on widgets (#140)
    
    Clean up errors
    
    Allow widgets z axis
    
    update
    
    Feature: dashboard copy and paste widgets (#139)
    
    Feature: Implement z-index on widgets (#140)
    
    remove extra copy function
    
    clean up
    
    connect paste to reducer
    
    integration test test
    
    integration test
    
    testing dashboard w integ test
    
    testing dashboard w integ test
    
    testing dashboard w integ test
    
    revert back to original after testing
    
    Change name to dashboard and dashboard-internal
    
    Change name to dashboard and dashboard-internal
    
    Edit .spec
    
    Fix unit tests
    
    fix tests
    
    fix tests
    
    fix names
    
    fix tests
    
    add z component
    
    squash
    
    Feature: dashboard copy and paste widgets (#139)
    
    Feature: Implement z-index on widgets (#140)
    
    Clean up errors
    
    Allow widgets z axis
    
    update
    
    Feature: dashboard copy and paste widgets (#139)
    
    Feature: Implement z-index on widgets (#140)
    
    remove extra copy function
    
    clean up
    
    connect paste to reducer
    
    integration test test
    
    integration test
    
    testing dashboard w integ test
    
    testing dashboard w integ test
    
    testing dashboard w integ test
    
    revert back to original after testing
    
    Change name to dashboard and dashboard-internal
    
    Change name to dashboard and dashboard-internal
    
    Edit .spec
    
    Fix unit tests
    
    fix tests
    
    fix tests
    
    fix names
    
    fix tests
    
    create iot-dashboard.css

--- packages/dashboard/src/dashboard-actions/dashboardReducer.spec.ts
+++ packages/dashboard/src/dashboard-actions/dashboardReducer.spec.ts
@@ -1,5 +1,6 @@
 import { dashboardReducer } from './dashboardReducer';
 import { onMoveAction } from './actions';
+import { onDeleteAction, onResizeAction, onPasteAction } from '../types';
 
 it('returns empty dashboard configuration when provided an empty dashboard', () => {
   expect(
@@ -13,4 +14,122 @@ it('returns empty dashboard configuration when provided an empty dashboard', () 
       })
     )
   ).toEqual([]);
+});
+
+it('applies move', () => {
+  expect(
+    dashboardReducer(
+      [{ x: 1, y: 1, width: 1, height: 1, id: 'some-id', widget: 'line-chart' }],
+      onMoveAction({
+        widgetIds: ['some-id'],
+        cellSize: 10,
+        position: { x: 20, y: 10 },
+        prevPosition: { x: 10, y: 10 },
+      })
+    )
+  ).toEqual([{ x: 2, y: 1, width: 1, height: 1, id: 'some-id', widget: 'line-chart' }]);
+});
+
+it('applies resize', () => {
+  expect(
+    dashboardReducer(
+      [
+        {
+          x: 2,
+          y: 2,
+          height: 1,
+          width: 1,
+          id: 'some-id',
+          widget: 'line-chart',
+        },
+      ],
+      onResizeAction({
+        widgetIds: ['some-id'],
+        cellSize: 10,
+        changeInPosition: { x: -10, y: -10 },
+        anchor: 'top-left',
+      })
+    )
+  ).toEqual([
+    {
+      x: 1,
+      y: 1,
+      height: 2,
+      width: 2,
+      id: 'some-id',
+      widget: 'line-chart',
+    },
+  ]);
+});
+
+it('applies delete', () => {
+  expect(
+    dashboardReducer(
+      [
+        {
+          x: 2,
+          y: 2,
+          height: 1,
+          width: 1,
+          id: 'some-id',
+          widget: 'line-chart',
+        },
+      ],
+      onDeleteAction({
+        widgetIds: ['some-id'],
+      })
+    )
+  ).toEqual([]);
+});
+
+it('applies paste', () => {
+  expect(
+    dashboardReducer(
+      [
+        {
+          x: 2,
+          y: 2,
+          height: 1,
+          width: 1,
+          id: 'some-id',
+          widget: 'line-chart',
+        },
+      ],
+      onPasteAction({
+        copyGroup: [
+          {
+            x: 2,
+            y: 2,
+            height: 1,
+            width: 1,
+            id: 'some-id',
+            widget: 'line-chart',
+          },
+        ],
+        numTimesCopyGroupHasBeenPasted: 0,
+      })
+    )
+  ).toEqual([
+    {
+      x: 2,
+      y: 2,
+      height: 1,
+      width: 1,
+      id: 'some-id',
+      widget: 'line-chart',
+    },
+    {
+      ...{
+        x: 2,
+        y: 2,
+        height: 1,
+        width: 1,
+        id: 'some-id',
+        widget: 'line-chart',
+      },
+      id: expect.any(String),
+      x: 3,
+      y: 3,
+    },
+  ]);
 });
--- packages/dashboard/src/dashboard-actions/undo.ts
+++ packages/dashboard/src/dashboard-actions/undo.ts
@@ -2,41 +2,16 @@ import { DashboardConfiguration, MoveAction, ResizeAction } from '../types';
 import { getMovedDashboardConfiguration } from './move';
 import { DashboardAction } from './actions';
 import { resize } from './resize';
+import { reverseMove } from './reverse-actions/reverseMove';
+import { reverseResize } from './reverse-actions/reverseResize';
 
-export const reverseAction = (dashAction: DashboardAction): DashboardAction | MoveAction | ResizeAction => {
-  switch (dashAction.type) {
-    case 'MOVE':
-      //create new move action to return later
-      const newMoveAction: MoveAction = dashAction;
-      if (typeof dashAction.payload.prevPosition != 'undefined') {
-        //flip previous and current positions
-        const tempPosition = dashAction.payload.position;
-        newMoveAction.payload.position = dashAction.payload.prevPosition;
-        newMoveAction.payload.prevPosition = tempPosition;
-        return newMoveAction;
-      }
-
-      return newMoveAction;
-
-    case 'RESIZE':
-      const newResizeAction: ResizeAction = dashAction;
-      //invert changeInPosition values
-      newResizeAction.payload.changeInPosition.x = dashAction.payload.changeInPosition.x * -1;
-      newResizeAction.payload.changeInPosition.y = dashAction.payload.changeInPosition.y * -1;
-      return newResizeAction;
-
-    default:
-      return dashAction;
-  }
-};
-
-export const applyReverseAction = (
+export const undo = (
   dashAction: DashboardAction | MoveAction | ResizeAction,
   dashboardConfiguration: DashboardConfiguration
 ): DashboardConfiguration => {
   switch (dashAction.type) {
     case 'MOVE':
-      let newMoveAction: DashboardAction = reverseAction(dashAction);
+      let newMoveAction: DashboardAction = reverseMove(dashAction);
       if (newMoveAction.type == 'MOVE') {
         return getMovedDashboardConfiguration({
           dashboardConfiguration: dashboardConfiguration,
@@ -49,7 +24,7 @@ export const applyReverseAction = (
       return dashboardConfiguration;
 
     case 'RESIZE':
-      let newResizeAction: DashboardAction = reverseAction(dashAction);
+      let newResizeAction: DashboardAction = reverseResize(dashAction);
       if (newResizeAction.type == 'RESIZE') {
         return resize({
           anchor: newResizeAction.payload.anchor,
