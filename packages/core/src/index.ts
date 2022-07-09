e634c3c0146f17babaebb074ae0ff2f353668b66
Jane <qianweiz@amazon.com>
2022-07-08 17:04:32 -0700

    fix: add a global audioAlertPlayer (#154)

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -7,10 +7,8 @@ export * from './common/types';
 export * from './common/viewport';
 export * from './common/time';
 export * from './common/combineProviders';
+export * from './common/audioAlert';
 
 export * from './data-module/IotAppKitDataModule';
 
 export * from './mockWidgetProperties';
-
-export * from './common/audioAlertPlayer';
-export * from './common/audioAlert';
