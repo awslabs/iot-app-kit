54d16c314dc1c2651fae3de804bb1ea507877644
Jason Buss <jmbuss@amazon.com>
2022-07-15 20:17:34 +0000

    feat(dashboard): add dynamic widget to render app-kit charts

--- packages/dashboard/src/testing/testing-ground/testing-ground.tsx
+++ packages/dashboard/src/testing/testing-ground/testing-ground.tsx
@@ -1,10 +1,17 @@
 import { Component, State, h } from '@stencil/core';
 import { DashboardConfiguration } from '../../types';
-import { dashboardConfig } from '../mocks';
+import { concatWidgets } from '../../util/dashboardConfiguration';
+import { dashboardConfig, MockWidgetFactory } from '../mocks';
 
 const DEFAULT_CELL_SIZE = 30;
 const DEFAULT_WIDTH = 1000;
 const DEFAULT_STRETCH_TO_FIT = true;
+
+const getRandomInt = (min: number, max: number) => {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+};
 
 @Component({
   tag: 'testing-ground',
@@ -17,18 +24,22 @@ export class TestingGround {
   @State() width = DEFAULT_WIDTH;
 
   addWidget = () => {
-    this.dashboardConfiguration = [
-      ...this.dashboardConfiguration,
-      {
-        x: 1,
-        y: 1,
-        z: 1,
-        width: 4,
-        height: 4,
-        widget: 'line-chart',
-        id: Math.random().toString() + new Date().toISOString(),
-      },
-    ];
+    const n = getRandomInt(1, 3);
+    switch (n) {
+      case 1:
+        this.dashboardConfiguration = concatWidgets(this.dashboardConfiguration, [MockWidgetFactory.getKpiWidget()]);
+        break;
+      case 2:
+        this.dashboardConfiguration = concatWidgets(this.dashboardConfiguration, [
+          MockWidgetFactory.getScatterChartWidget(),
+        ]);
+        break;
+      case 3:
+        this.dashboardConfiguration = concatWidgets(this.dashboardConfiguration, [
+          MockWidgetFactory.getLineChartWidget(),
+        ]);
+        break;
+    }
   };
 
   onCellSizeInput = (e: Event) => {
