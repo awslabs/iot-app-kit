1afe356d197aa5634a6fd7ff653c5c5a5ff1c341
Jason Buss <jmbuss@amazon.com>
2022-07-15 20:17:34 +0000

    feat(dashboard): add dynamic widget to render app-kit charts
    Undo/redo functionality with dynamic widgets
    
    fix

--- packages/dashboard/src/components/iot-dashboard/trimWidgetPosition.spec.ts
+++ packages/dashboard/src/components/iot-dashboard/trimWidgetPosition.spec.ts
@@ -1,15 +1,17 @@
+import { MockWidgetFactory } from '../../testing/mocks';
 import { trimWidgetPosition } from './trimWidgetPosition';
 
 it('rounds the position to the nearest decimal', () => {
   expect(
-    trimWidgetPosition({
-      x: 1.01,
-      y: 1.99,
-      width: 10,
-      height: 20,
-      id: 'some-id',
-      widget: 'some-widget',
-    })
+    trimWidgetPosition(
+      MockWidgetFactory.getKpiWidget({
+        x: 1.01,
+        y: 1.99,
+        width: 10,
+        height: 20,
+        id: 'some-id',
+      })
+    )
   ).toEqual(
     expect.objectContaining({
       x: 1,
@@ -20,14 +22,15 @@ it('rounds the position to the nearest decimal', () => {
 
 it('rounds the width and height to the nearest decimal', () => {
   expect(
-    trimWidgetPosition({
-      x: 1,
-      y: 2,
-      width: 10.49,
-      height: 20.59,
-      id: 'some-id',
-      widget: 'some-widget',
-    })
+    trimWidgetPosition(
+      MockWidgetFactory.getKpiWidget({
+        x: 1,
+        y: 2,
+        width: 10.49,
+        height: 20.59,
+        id: 'some-id',
+      })
+    )
   ).toEqual(
     expect.objectContaining({
       width: 10,
@@ -38,14 +41,15 @@ it('rounds the width and height to the nearest decimal', () => {
 
 it('does nothing to widgets with integer based position and dimensions', () => {
   expect(
-    trimWidgetPosition({
-      x: 1,
-      y: 2,
-      width: 10,
-      height: 20,
-      id: 'some-id',
-      widget: 'some-widget',
-    })
+    trimWidgetPosition(
+      MockWidgetFactory.getKpiWidget({
+        x: 1,
+        y: 2,
+        width: 10,
+        height: 20,
+        id: 'some-id',
+      })
+    )
   ).toEqual(
     expect.objectContaining({
       width: 10,
---
+++ packages/dashboard/src/dashboard-actions/reverse-actions/reverseMove.spec.ts
@@ -0,0 +1,53 @@
+import { reverseMove } from './reverseMove';
+
+it('returns move action where position and prevPosition are switched', () => {
+  expect(
+    reverseMove({
+      type: 'MOVE',
+      payload: {
+        position: { x: 10, y: 10 },
+        prevPosition: { x: 11, y: 10 },
+        widgetIds: ['some-id'],
+        cellSize: 10,
+      },
+    })
+  ).toEqual({
+    payload: { cellSize: 10, position: { x: 11, y: 10 }, prevPosition: { x: 10, y: 10 }, widgetIds: ['some-id'] },
+    type: 'MOVE',
+  });
+});
+
+it('returns same move action when previous position is undefined', () => {
+  expect(
+    reverseMove({
+      type: 'MOVE',
+      payload: {
+        position: { x: 10, y: 10 },
+        prevPosition: undefined,
+        widgetIds: ['some-id'],
+        cellSize: 10,
+      },
+    })
+  ).toEqual({
+    payload: { cellSize: 10, position: { x: 10, y: 10 }, prevPosition: undefined, widgetIds: ['some-id'] },
+    type: 'MOVE',
+  });
+});
+it('returns the original action when reversed twice', () => {
+  expect(
+    reverseMove(
+      reverseMove({
+        type: 'MOVE',
+        payload: {
+          position: { x: 10, y: 10 },
+          prevPosition: { x: 11, y: 10 },
+          widgetIds: ['some-id'],
+          cellSize: 10,
+        },
+      })
+    )
+  ).toEqual({
+    payload: { cellSize: 10, position: { x: 10, y: 10 }, prevPosition: { x: 11, y: 10 }, widgetIds: ['some-id'] },
+    type: 'MOVE',
+  });
+});
