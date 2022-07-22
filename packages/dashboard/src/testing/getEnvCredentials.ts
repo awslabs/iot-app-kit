6d146f8159373c8a4110fd680598f673d4dedec5
sydopy <106272253+sydopy@users.noreply.github.com>
2022-07-22 12:30:42 -0400

    Update getEnvCredentials.ts

--- packages/dashboard/src/testing/getEnvCredentials.ts
+++ packages/dashboard/src/testing/getEnvCredentials.ts
@@ -9,8 +9,5 @@ export const getEnvCredentials = () => {
     );
   }*/
   return {
-    "accessKeyId": "ASIAUSMZLEWGCWVDBR6O",
-		"secretAccessKey": "6tNMNeX9pD5Slm6BMW8aQBizmNvIgjPeZ7+VCW5b",
-		"sessionToken": "IQoJb3JpZ2luX2VjEBYaCXVzLWVhc3QtMSJHMEUCIFikmR8N2KcIzFPHq8AQf28WVL2KyNCSld/StXa4vFG0AiEAjYX8/c6iK1LM1qdYbSXW0pEEjoTZdsMtjW0Jqj+WfMYqnAIIXxAAGgwzMTQzOTEwNzgyODQiDH9K+uuXN5A+ohEPsCr5AVofIs09ev64ocZKi0bd8c9glrws15q3gIh1hArImtL0CDAeCtbNiiaOsNAYRwo8qJkfZeZEEg+FkmTFksvTwzcWnugfjPJ09PIfULlPbR/an1oWVPusWL8KjDcbgsQyyIFin8WOANZBODkAQALm5znOST/Nx8Davopv2Fuq91LtCkJgkNZTpmPAJuq/oo/bSlauGqh5ZDkIThL3jqcarzd8sTBFqFJgHxO9CdbQfsGmrDlBwem1iJXhKQm81bGqh0H/YXHm9hzHNkIFyK14HgGRLgGjbmddMwdRiga8UmmeYlBpyBNkNSFBYco7BbTnUk6E4yWSms8AzDDS1eqWBjqdAexHOoqugGSfO9zPVJgfwbPcWOjsbkE9+wV2OUJmJdtnOpRt6FDX77/Nz8W9W3Cp3GC/yR/JbsTsdjlHxZt2VFTmXGbwI+zx2wCEt557jvhFbuudpOHU4iZ3nyRrSivAfslg4otyfVkHpZusHOclX7IiVHImHDdcdYI5CskLStL4eRs7uMBxjM5k2ZGqJtBpyOhZLbIV9Nx7zmIaPM4=",
   };
 };
