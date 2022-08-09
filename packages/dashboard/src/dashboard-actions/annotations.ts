f1004a3f68914185edfdf8ff96ef35d892cfb847
Raymond <raymfang@amazon.com>
2022-08-09 07:05:05 -0700

    feat(dashboard): add time-point annotations and dashboard controls

---
+++ packages/dashboard/src/dashboard-actions/annotations.ts
@@ -0,0 +1,73 @@
+import { XAnnotation } from '@synchro-charts/core';
+import { DashboardConfiguration } from '../types';
+
+export const addXAnnotation = ({
+  dashboardConfiguration,
+  widgetId,
+  annotation,
+}: {
+  dashboardConfiguration: DashboardConfiguration;
+  widgetId: string;
+  annotation: XAnnotation;
+}) => {
+  return dashboardConfiguration.widgets.map((widget) => {
+    if (widget.id === widgetId) {
+      const currAnnotations = widget.annotations || {};
+      const currXAnnotations = currAnnotations.x || [];
+      return { ...widget, annotations: { ...currAnnotations, x: [...currXAnnotations, annotation] } };
+    }
+    return widget;
+  });
+};
+
+export const deleteXAnnotation = ({
+  dashboardConfiguration,
+  widgetId,
+  annotationIdToDelete,
+}: {
+  dashboardConfiguration: DashboardConfiguration;
+  widgetId: string;
+  annotationIdToDelete: string;
+}) => {
+  return dashboardConfiguration.widgets.map((widget) => {
+    if (widget.id !== widgetId) {
+      return widget;
+    }
+    if (widget.annotations && widget.annotations.x) {
+      widget.annotations = {
+        ...widget.annotations,
+        x: widget.annotations.x.filter((annotation) => annotation.id != annotationIdToDelete),
+      };
+    }
+    return {
+      ...widget,
+      annotations: widget.annotations,
+    };
+  });
+};
+
+export const editXAnnotation = ({
+  dashboardConfiguration,
+  widgetId,
+  oldAnnotationId,
+  newAnnotation,
+}: {
+  dashboardConfiguration: DashboardConfiguration;
+  widgetId: string;
+  oldAnnotationId: string;
+  newAnnotation: XAnnotation;
+}) => {
+  return dashboardConfiguration.widgets.map((widget) => {
+    if (widget.id !== widgetId) {
+      return widget;
+    }
+    if (widget.annotations && widget.annotations.x) {
+      widget.annotations.x = widget.annotations.x.filter((annotation) => annotation.id != oldAnnotationId);
+      widget.annotations = { ...widget.annotations, x: [...widget.annotations.x, newAnnotation] };
+    }
+    return {
+      ...widget,
+      annotations: widget.annotations,
+    };
+  });
+};
--- packages/dashboard/src/testing/testing-ground/testing-ground.tsx
+++ packages/dashboard/src/testing/testing-ground/testing-ground.tsx
@@ -2,10 +2,14 @@ import { Component, State, h } from '@stencil/core';
 import { DashboardConfiguration } from '../../types';
 import { concatWidgets } from '../../util/dashboardConfiguration';
 import { dashboardConfig, MockWidgetFactory } from '../mocks';
+import { XAnnotation } from '@synchro-charts/core';
+import { addXAnnotation, editXAnnotation, deleteXAnnotation } from '../../dashboard-actions/annotations';
+import { viewportEndDate, viewportStartDate } from '@iot-app-kit/core';
 
 const DEFAULT_CELL_SIZE = 30;
 const DEFAULT_WIDTH = 1000;
 const DEFAULT_STRETCH_TO_FIT = true;
+const DEFAULT_LIST_VISIBLE_ANNOTATIONS = false;
 
 const getRandomInt = (min: number, max: number) => {
   min = Math.ceil(min);
@@ -22,6 +26,8 @@ export class TestingGround {
   @State() cellSize = DEFAULT_CELL_SIZE;
   @State() stretchToFit = DEFAULT_STRETCH_TO_FIT;
   @State() width = DEFAULT_WIDTH;
+  @State() selectedWidgetId = this.dashboardConfiguration.widgets[0].id;
+  @State() onlyListVisibleAnnotations = DEFAULT_LIST_VISIBLE_ANNOTATIONS;
 
   addWidget = () => {
     const n = getRandomInt(1, 3);
@@ -52,6 +58,91 @@ export class TestingGround {
 
   onStretchToFit = (e: Event) => {
     this.stretchToFit = (e as any).target.checked;
+  };
+
+  onListVisibleAnnotations = (e: Event) => {
+    this.onlyListVisibleAnnotations = (e as any).target.checked;
+  };
+
+  /**
+   * Modify annotation data based on input from UI
+   */
+  selectWidget = (e: Event) => {
+    e.preventDefault();
+    const widgetId = (e as any).target.value;
+    this.selectedWidgetId = widgetId;
+  };
+
+  addAnnotation = (e: Event) => {
+    e.preventDefault();
+    const labelInput = (e as any).target[0].value;
+    const annotationLabel = { text: labelInput, show: true };
+    const colorInput = (e as any).target[1].value;
+    const dateInput = (e as any).target[2].value;
+    const newDate = new Date(dateInput);
+    const XANNOTATION: XAnnotation = {
+      color: colorInput,
+      value: newDate,
+      id: Date.now().toString(),
+      showValue: true,
+      label: annotationLabel,
+    };
+    const newWidgets = addXAnnotation({
+      dashboardConfiguration: this.dashboardConfiguration,
+      widgetId: this.selectedWidgetId,
+      annotation: XANNOTATION,
+    });
+    this.dashboardConfiguration = { ...this.dashboardConfiguration, widgets: newWidgets };
+  };
+
+  deleteAnnotation = (e: Event) => {
+    if (window.confirm('Delete annotation?')) {
+      e.preventDefault();
+      const stringInput = (e as any).target[0].value;
+      const newWidgets = deleteXAnnotation({
+        dashboardConfiguration: this.dashboardConfiguration,
+        widgetId: this.selectedWidgetId,
+        annotationIdToDelete: stringInput,
+      });
+      this.dashboardConfiguration = { ...this.dashboardConfiguration, widgets: newWidgets };
+    }
+  };
+
+  editAnnotation = (e: Event) => {
+    if (window.confirm('Edit annotation?')) {
+      e.preventDefault();
+      const labelInput = (e as any).target[0].value;
+      const annotationLabel = { text: labelInput, show: true };
+      const colorInput = (e as any).target[1].value;
+      const stringInput = (e as any).target[2].value;
+      const dateInput = (e as any).target[3].value;
+      const newDate = new Date(dateInput);
+      const oldWidget = this.dashboardConfiguration.widgets
+        .find((x) => x.id == this.selectedWidgetId)
+        ?.annotations?.x?.find((annotation) => annotation.id == stringInput);
+      const XANNOTATION: XAnnotation = {
+        color: colorInput,
+        value: newDate,
+        id: Date.now().toString(),
+        showValue: true,
+        label: annotationLabel ? annotationLabel : oldWidget?.label,
+      };
+      const newWidgets = editXAnnotation({
+        dashboardConfiguration: this.dashboardConfiguration,
+        widgetId: this.selectedWidgetId,
+        oldAnnotationId: stringInput,
+        newAnnotation: XANNOTATION,
+      });
+      this.dashboardConfiguration = { ...this.dashboardConfiguration, widgets: newWidgets };
+      console.log(this.dashboardConfiguration);
+    }
+  };
+
+  isVisible = (annotation: XAnnotation) => {
+    const start = viewportStartDate(this.dashboardConfiguration.viewport).getTime();
+    const end = viewportEndDate(this.dashboardConfiguration.viewport).getTime();
+    const annotationTime = annotation.value.getTime();
+    return annotationTime >= start && annotationTime <= end;
   };
 
   render() {
@@ -91,7 +182,79 @@ export class TestingGround {
             />
           </div>
           <div slot="right">
-            <div class="dummy-content">Component pane</div>
+            <div>
+              <label htmlFor="selectWidget"> Select widget:</label>
+              <select id="selectWidget" onChange={this.selectWidget}>
+                {this.dashboardConfiguration.widgets.map((widget) => (
+                  <option value={widget.id}>{widget.id.toString() + ', ' + widget.componentTag.toString()}</option>
+                ))}
+              </select>
+            </div>
+            <div>
+              <label>Only list visible annotations</label>
+              <input
+                type="checkbox"
+                checked={this.onlyListVisibleAnnotations}
+                onChange={this.onListVisibleAnnotations}
+              />
+            </div>
+            <br />
+            <br />
+            <div>
+              <form onSubmit={this.addAnnotation}>
+                <label htmlFor="addAnnotation"> Add annotation -</label>
+                <label htmlFor="addLabel"> Label:</label>
+                <input type="text" id="addLabel"></input>
+                <input type="color" value="#FF0000"></input>
+                <input type="datetime-local" step="1"></input>
+                <input type="submit" value="Add" id="addAnnotation" onSubmit={this.addAnnotation}></input>
+              </form>
+            </div>
+            <br />
+            <br />
+            <div>
+              <form onSubmit={this.deleteAnnotation}>
+                <label htmlFor="deleteAnnotation"> Delete annotation:</label>
+                <select id="deleteAnnotation">
+                  {this.onlyListVisibleAnnotations
+                    ? this.dashboardConfiguration.widgets
+                        .find((x) => x.id == this.selectedWidgetId)
+                        ?.annotations?.x?.filter((annotation) => this.isVisible(annotation))
+                        .map((annotation) => <option value={annotation.id}>{annotation.value.toString()}</option>)
+                    : this.dashboardConfiguration.widgets
+                        .find((x) => x.id == this.selectedWidgetId)
+                        ?.annotations?.x?.map((annotation) => (
+                          <option value={annotation.id}>{annotation.value.toString()}</option>
+                        ))}
+                </select>
+                <input type="submit" value="Delete"></input>
+              </form>
+            </div>
+            <br />
+            <br />
+            <div>
+              <form onSubmit={this.editAnnotation}>
+                <label htmlFor="editAnnotation"> Edit annotation - </label>
+                <label htmlFor="addLabel"> Label:</label>
+                <input type="text" id="addLabel"></input>
+                <input type="color" value="#FF0000"></input>
+                <br />
+                <select id="editAnnotation">
+                  {this.onlyListVisibleAnnotations
+                    ? this.dashboardConfiguration.widgets
+                        .find((x) => x.id == this.selectedWidgetId)
+                        ?.annotations?.x?.filter((annotation) => this.isVisible(annotation))
+                        .map((annotation) => <option value={annotation.id}>{annotation.value.toString()}</option>)
+                    : this.dashboardConfiguration.widgets
+                        .find((x) => x.id == this.selectedWidgetId)
+                        ?.annotations?.x?.map((annotation) => (
+                          <option value={annotation.id}>{annotation.value.toString()}</option>
+                        ))}
+                </select>
+                <input type="datetime-local" step="1"></input>
+                <input type="submit" value="Edit"></input>
+              </form>
+            </div>
           </div>
         </iot-resizable-panes>
       </div>
