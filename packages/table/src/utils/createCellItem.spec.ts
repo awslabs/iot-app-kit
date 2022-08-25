6d44c00daa3d067ae3d4e5afcddf6358008e8a60
Xiao(Bill) Li <limxiao@amazon.com>
2022-08-25 14:23:09 -0400

    feat(table): support messageOverrides (#211)

--- packages/table/src/utils/createCellItem.spec.ts
+++ packages/table/src/utils/createCellItem.spec.ts
@@ -1,25 +1,33 @@
 import { createCellItem } from './createCellItem';
 import { CellItem } from './types';
+import { DefaultTableMessages, TableMessages } from './messages';
 
-describe('createCellItem', () => {
-  it('creates CellItem', () => {
-    const props = { value: 10, error: undefined, isLoading: false, threshold: undefined };
-    const item: CellItem = createCellItem(props);
-    expect(item).toMatchObject({ value: 10 });
-    expect(item.valueOf()).toEqual(10);
-  });
+const messageOverride = { tableCell: { loading: 'Override loading text' } } as TableMessages;
 
-  it('creates CellItem that returns error message on error', () => {
-    const props = { value: 10, error: { msg: 'Some error' }, isLoading: false, threshold: undefined };
-    const item: CellItem = createCellItem(props);
+it('creates CellItem', () => {
+  const props = { value: 10, error: undefined, isLoading: false, threshold: undefined };
+  const item: CellItem = createCellItem(props, DefaultTableMessages);
+  expect(item).toMatchObject({ value: 10 });
+  expect(item.valueOf()).toEqual(10);
+});
 
-    expect(`${item}`).toBe('Some error');
-  });
+it('creates CellItem that returns error message on error', () => {
+  const props = { value: 10, error: { msg: 'Some error' }, isLoading: false, threshold: undefined };
+  const item: CellItem = createCellItem(props, DefaultTableMessages);
 
-  it('creates CellItem that returns loading message on loading', () => {
-    const props = { value: 10, isLoading: true, threshold: undefined };
-    const item: CellItem = createCellItem(props);
+  expect(`${item}`).toBe('Some error');
+});
 
-    expect(`${item}`).toBe('Loading');
-  });
+it('creates CellItem that returns loading message on loading', () => {
+  const props = { value: 10, isLoading: true, threshold: undefined };
+  const item: CellItem = createCellItem(props, DefaultTableMessages);
+
+  expect(`${item}`).toBe('Loading');
+});
+
+it('accepts messageOverrides for loading message', () => {
+  const props = { value: 10, isLoading: true, threshold: undefined };
+  const item: CellItem = createCellItem(props, messageOverride);
+
+  expect(`${item}`).toBe(messageOverride!.tableCell!.loading);
 });
