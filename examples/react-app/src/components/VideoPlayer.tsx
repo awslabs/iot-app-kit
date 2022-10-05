7cd9165702581b2ecefae4fd39e7e942e41d74cf
Bowei Han <boweih@amazon.com>
2022-10-05 13:54:09 -0400

    chore(examples): easier linking for iot-app-kit packages (#265)

--- examples/react-app/src/components/VideoPlayer.tsx
+++ examples/react-app/src/components/VideoPlayer.tsx
@@ -1,20 +1,19 @@
-import React from 'react';
-
 import { VideoPlayer } from '@iot-app-kit/react-components';
 import { dataSource } from '../dataSource';
 import { kvsStreamName, videoEntityId, videoComponentName, videoViewport } from '../configs';
 
-const videoData = dataSource.videoData({ kvsStreamName: kvsStreamName, entityId: videoEntityId, componentName: videoComponentName });
+const videoData = dataSource.videoData({
+  kvsStreamName: kvsStreamName,
+  entityId: videoEntityId,
+  componentName: videoComponentName,
+});
 
 const VideoPlayerComponent = () => {
   return (
-    <div className="VideoPlayerComponent" >
-      <VideoPlayer
-        videoData={videoData}
-        viewport={videoViewport}
-      />
+    <div className="VideoPlayerComponent">
+      <VideoPlayer videoData={videoData} viewport={videoViewport} />
     </div>
   );
-}
+};
 
 export default VideoPlayerComponent;
--- examples/react-app/src/components/componentsTab.tsx
+++ examples/react-app/src/components/componentsTab.tsx
@@ -1,33 +1,37 @@
-import React from "react";
-import Paper from "@material-ui/core/Paper";
-import Tab from "@material-ui/core/Tab";
-import Tabs from "@material-ui/core/Tabs";
-import SceneViewer from "./SceneViewer";
+import React from 'react';
+import Paper from '@material-ui/core/Paper';
+import Tab from '@material-ui/core/Tab';
+import Tabs from '@material-ui/core/Tabs';
+import SceneViewer from './SceneViewer';
 import VideoPlayerComponent from './VideoPlayer';
 
 const ComponentsTab = () => {
-    const [value, setValue] = React.useState(0);
+  const [value, setValue] = React.useState(0);
 
-    return (
-        <div>
-            <h3>Sample IoT AppKit Components</h3>
-            <Paper square>
-                <Tabs
-                value={value}
-                textColor="primary"
-                indicatorColor="primary"
-                onChange={(event: any, newValue: React.SetStateAction<number>) => {
-                    setValue(newValue);
-                }}
-                >
-                <Tab label="Scene Viewer" />
-                <Tab label="Video Player" />
-                </Tabs>
-                {value === 0 && <SceneViewer />}
-                {value === 1 && <div><VideoPlayerComponent /></div>}
-            </Paper>
-        </div>
-    );
+  return (
+    <div>
+      <h3>Sample IoT AppKit Components</h3>
+      <Paper square>
+        <Tabs
+          value={value}
+          textColor="primary"
+          indicatorColor="primary"
+          onChange={(event: any, newValue: React.SetStateAction<number>) => {
+            setValue(newValue);
+          }}
+        >
+          <Tab label="Scene Viewer" />
+          <Tab label="Video Player" />
+        </Tabs>
+        {value === 0 && <SceneViewer />}
+        {value === 1 && (
+          <div>
+            <VideoPlayerComponent />
+          </div>
+        )}
+      </Paper>
+    </div>
+  );
 };
 
 export default ComponentsTab;
