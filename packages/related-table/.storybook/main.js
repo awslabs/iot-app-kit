09ce04abe78e8836eb8c4e8c8ef4f30b6d9cef70
Jason Buss <jmbuss@amazon.com>
2022-10-11 12:11:48 -0500

    chore(related-table): updated build system to use rollup

--- packages/related-table/.storybook/main.js
+++ packages/related-table/.storybook/main.js
@@ -2,6 +2,10 @@ module.exports = {
   stories: ['../stories/**/*.stories.@(ts|tsx|js|jsx)'],
   addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/preset-scss'],
   // https://storybook.js.org/docs/react/configure/typescript#mainjs-configuration
+  "framework": "@storybook/react",
+  "core": {
+    "builder": "@storybook/builder-webpack5"
+  },
   typescript: {
     check: true, // type-check stories during Storybook build
   },
---
+++ packages/related-table/rollup.config.js
@@ -0,0 +1,33 @@
+import { nodeResolve } from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import typescript from '@rollup/plugin-typescript';
+import dts from 'rollup-plugin-dts';
+import { terser } from 'rollup-plugin-terser';
+import peerDepsExternal from 'rollup-plugin-peer-deps-external';
+
+const packageJson = require('./package.json'); // eslint-disable-line
+
+export default [
+  {
+    input: 'src/index.ts',
+    output: [
+      {
+        file: packageJson.main,
+        format: 'cjs',
+        sourcemap: true,
+      },
+      {
+        file: packageJson.module,
+        format: 'esm',
+        sourcemap: true,
+      },
+    ],
+    plugins: [peerDepsExternal(), nodeResolve(), commonjs(), typescript({ tsconfig: './tsconfig.json' }), terser()],
+    external: ['react', 'react-dom'],
+  },
+  {
+    input: 'dist/esm/index.d.ts',
+    output: [{ file: 'dist/index.d.ts', format: 'esm' }],
+    plugins: [dts()],
+  },
+];
