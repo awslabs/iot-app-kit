8f462eed09f15ad517cbb33f67dd634393aaeca6
Xinyi Xu <sheilaxxy@gmail.com>
2022-10-14 09:18:40 -0700

    fix(composer): use arrow data as texture to avoid public path issue (#276)

--- examples/react-app/config-overrides.js
+++ examples/react-app/config-overrides.js
@@ -5,25 +5,6 @@ module.exports = function override(config) {
     new NodePolyfillPlugin({excludeAliases: ['console']})
    ]);
 
-   // Override the default svg rules from create-react-app to set absolute path as localhost
-   config.module.rules.find((rule => !!rule.oneOf))?.['oneOf'].forEach(rule => {
-      let isSvg = false;
-      if (!Array.isArray(rule.test) && rule.test) {
-         isSvg = rule.test.test('.svg');
-      }
-      if (isSvg) {
-         rule.use = [
-            {
-               loader: require.resolve('file-loader'),
-               options: {
-               publicPath: 'http://localhost:3000/',
-               name: 'static/media/[name].[hash].[ext]',
-               },
-            },
-         ]
-      }
-   });
-
    return config; 
 }
 
