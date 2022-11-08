a7cdb24a5939cb7c43078b53f32c7f804ac03b45
Emily Dodds <dodemily@amazon.com>
2022-11-08 17:54:18 -0600

    fix(composer): Removing dashboard package to fix pipeline

--- packages/dashboard/src/util/constrainWidgetPositionToGrid.ts
+++
@@ -1,7 +0,0 @@
-import { Rect, Widget } from '../types';
-
-export const constrainWidgetPositionToGrid = (gridRect: Rect, widget: Widget): Widget => ({
-  ...widget,
-  x: Math.min(gridRect.width - widget.width, Math.max(gridRect.x, widget.x)),
-  y: Math.min(gridRect.height - widget.height, Math.max(gridRect.y, widget.y)),
-});
