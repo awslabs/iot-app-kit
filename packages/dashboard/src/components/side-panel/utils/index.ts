452b659831ab599ca68c6ec38408880b3923fbc8
Bill Li <limxiao@amazon.com>
2022-11-17 17:32:06 -0500

    feat(dashboard-side-panel): create `useInput` hook

---
+++ packages/dashboard/src/components/side-panel/utils/index.ts
@@ -0,0 +1,17 @@
+import { useEffect, Dispatch, useState } from 'react';
+// import { useDispatch } from 'react-redux';
+import { get, set, cloneDeep } from 'lodash';
+
+export const useInput: <T>(path: string) => [T, Dispatch<T>] = (path) => {
+  const selectedWidget = {}; // TODO: get selected widget from state manager or context
+  const [value, updateValue] = useState(get(selectedWidget, path));
+  // const dispatch = useDispatch();
+  useEffect(() => {
+    if (selectedWidget && get(selectedWidget, path) !== value) {
+      const deepClone = cloneDeep(selectedWidget);
+      const newWidget = set(deepClone, path, value);
+      // TODO: dispatch(onUpdateSelectedWidgetAction(newWidget))
+    }
+  }, [value]);
+  return [value, updateValue];
+};
