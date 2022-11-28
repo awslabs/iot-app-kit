407b21deffad05afaae4b57b085f901baa19263b
Thomas Juranek <thjurane@amazon.com>
2022-11-28 17:11:23 -0500

    fix(dashboard-side-panel): fix jest transforms with @swc/jest

---
+++ packages/dashboard/jest.config.ts
@@ -0,0 +1,39 @@
+/*
+ * For a detailed explanation regarding each configuration property and type check, visit:
+ * https://jestjs.io/docs/configuration
+ */
+
+export default {
+  // preset: 'ts-jest',
+  // clearMocks: true,
+  // collectCoverage: true,
+  // coverageDirectory: 'coverage',
+  // coverageProvider: 'v8',
+  moduleFileExtensions: ['js', 'ts', 'jsx', 'tsx'],
+  moduleNameMapper: {
+    '\\.(svg|css|less)$': '<rootDir>/testing/styleMock.js',
+  },
+  testEnvironment: 'jsdom',
+  transform: {
+    '.*\\.(tsx?|jsx?)$': [
+      '@swc/jest',
+      {
+        jsc: {
+          transform: {
+            react: {
+              runtime: 'automatic',
+            },
+          },
+        },
+      },
+    ],
+  },
+  transformIgnorePatterns: [],
+  setupFilesAfterEnv: ['mutationobserver-shim'],
+  //transform: {
+  //   '.+\\.ts$': 'ts-jest',
+  //   '^.+\\.tsx?$': 'ts-jest',
+  //   '^.+\\.(js|jsx)$': 'babel-jest',
+  // },
+  // transformIgnorePatterns: ['node_modules/(?!@awsui/components-react)/'],
+};
