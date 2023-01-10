b664fd05b9883350d8197246ad223b805b52bf67
Bill Li <limxiao@amazon.com>
2023-01-10 15:23:19 -0500

    feat(dashboard): dispatch updateWidget action from side panel

--- packages/dashboard/src/components/sidePanel/utils/index.ts
+++ packages/dashboard/src/components/sidePanel/utils/index.ts
@@ -1,14 +1,15 @@
 import { Dispatch, useEffect, useState } from 'react';
 // import { useDispatch } from 'react-redux';
-import { get } from 'lodash';
-import { useSelector } from 'react-redux';
+import { cloneDeep, get, set } from 'lodash';
+import { useDispatch, useSelector } from 'react-redux';
 import { DashboardState } from '../../../store/state';
+import { onUpdateWidgetsAction } from '../../../store/actions';
 
 export const useInput: <T>(path: string) => [T, Dispatch<T>] = (path) => {
   // TECHDEBT: only support viewing and updating the first selected widget.
-  const [selectedWidget] = useSelector((state: DashboardState) => state.dashboardConfiguration.widgets);
+  const [selectedWidget] = useSelector((state: DashboardState) => state.selectedWidgets);
   const [inputValue, updateInputValue] = useState(get(selectedWidget, path));
-
+  const dispatch = useDispatch();
   const onSelectedWidgetChange = () => {
     const currentWidgetState = get(selectedWidget, path);
     if (inputValue !== currentWidgetState) {
@@ -20,7 +21,12 @@ export const useInput: <T>(path: string) => [T, Dispatch<T>] = (path) => {
   const onInputValueChange = () => {
     const currentWidgetState = get(selectedWidget, path);
     if (inputValue !== currentWidgetState) {
-      // TODO: dispatch an action to update widget
+      const newWidget = set(cloneDeep(selectedWidget), path, inputValue);
+      dispatch(
+        onUpdateWidgetsAction({
+          widgets: [newWidget],
+        })
+      );
     }
   };
   useEffect(onInputValueChange, [inputValue]);
