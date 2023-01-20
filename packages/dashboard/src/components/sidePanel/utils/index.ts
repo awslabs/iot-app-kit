c4327efdc9a050cd551970ec51d0811a9cc4bf22
Xiao (Bill) Li <lixiao.bill@gmail.com>
2023-01-20 15:13:32 -0500

    feat(dashboard): add configuration section for Text component

--- packages/dashboard/src/components/sidePanel/utils/index.ts
+++ packages/dashboard/src/components/sidePanel/utils/index.ts
@@ -4,6 +4,9 @@ import { cloneDeep, get, set } from 'lodash';
 import { useDispatch, useSelector } from 'react-redux';
 import { DashboardState } from '../../../store/state';
 import { onUpdateWidgetsAction } from '../../../store/actions';
+import { TextWidget, Widget } from '../../../types';
+
+export type typedInputHook<T extends Widget = Widget> = <K extends keyof T>(key: K) => [T[K], Dispatch<T[K]>];
 
 export const useInput: <T>(path: string) => [T, Dispatch<T>] = (path) => {
   // TECHDEBT: only support viewing and updating the first selected widget.
@@ -38,3 +41,6 @@ export function capitalizeFirstLetter(string: string) {
   const lowerCase = string.toLowerCase();
   return lowerCase.charAt(0).toUpperCase() + lowerCase.slice(1);
 }
+
+// this helps Typescript finding correct types given attribute names
+export const useTextWidgetInput: typedInputHook<TextWidget> = useInput;
