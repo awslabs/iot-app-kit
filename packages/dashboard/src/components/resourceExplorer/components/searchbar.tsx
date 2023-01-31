897a0a93a4e4a7e5d6f3a96e21e7a83462fd7972
Matt Popovich <mattpopovich@outlook.com>
2023-01-31 11:29:02 -1000

    feat(dashboard): initial resource explorer search (#519)

--- packages/dashboard/src/components/resourceExplorer/components/searchbar.tsx
+++ packages/dashboard/src/components/resourceExplorer/components/searchbar.tsx
@@ -1,33 +1,112 @@
-import React, { useState, Dispatch, SetStateAction } from 'react';
+import React, { useState, Dispatch, SetStateAction, useEffect } from 'react';
 import SpaceBetween from '@cloudscape-design/components/space-between';
 import Autosuggest from '@cloudscape-design/components/autosuggest';
 import Checkbox from '@cloudscape-design/components/checkbox';
+import { BaseChangeDetail } from '@cloudscape-design/components/input/interfaces';
+import { MaybeSiteWiseAssetTreeSessionInterface } from '../types';
+import { AssetPropertiesCache } from '../useAssetProperties';
+import { ExtendedPanelAssetSummary } from '..';
+import { DashboardMessages } from '../../../messages';
 
-const checkboxOnChange = (setShowFn: Dispatch<SetStateAction<boolean>>) => {
-  return ({ detail }: { detail: { checked: boolean } }) => setShowFn(detail.checked);
-};
+interface SearchOption {
+  id: string;
+  value: string;
+}
 
-export const IotResourceExplorerSearchbar = () => {
+interface IotResourceExplorerSearchbarProps {
+  provider: MaybeSiteWiseAssetTreeSessionInterface;
+  assetPropertiesCache: AssetPropertiesCache;
+  setCrumbsToSearch: () => void;
+  setPanelItems: (panelItems: ExtendedPanelAssetSummary[]) => void;
+  messageOverrides: DashboardMessages;
+}
+
+export const IotResourceExplorerSearchbar: React.FC<IotResourceExplorerSearchbarProps> = ({
+  provider,
+  assetPropertiesCache,
+  setCrumbsToSearch,
+  setPanelItems,
+  messageOverrides,
+}) => {
   const [autosuggestValue, setAutosuggestValue] = useState('');
   const [showAssets, setShowAssets] = useState(true);
-  const [showProperties, setShowProperties] = useState(false);
-  const [showAlarms, setShowAlarms] = useState(false);
+  const [showProperties, setShowProperties] = useState(true);
+  const [showAlarms, setShowAlarms] = useState(true);
+  const [searchOptions = [], setSearchOptions] = useState<SearchOption[]>([]);
+
+  const checkboxOnChange = (setShowFn: Dispatch<SetStateAction<boolean>>) => {
+    return ({ detail }: { detail: { checked: boolean } }) => {
+      if (setPanelItems.length > 0) {
+        setPanelItems([]);
+      }
+      setShowFn(detail.checked);
+    };
+  };
+
+  useEffect(() => {
+    if (!provider?.assetNodes || !assetPropertiesCache) return;
+
+    let allAssetsSearchOptions: SearchOption[] = [];
+    if (showAssets) {
+      allAssetsSearchOptions = Object.entries(provider?.assetNodes).map(([id, asset]) => ({
+        ...asset,
+        value: asset?.asset?.name || '',
+        id,
+      }));
+    }
+
+    let allPropertiesSearchOptions: SearchOption[] = [];
+    const reduceResult: SearchOption[] = [];
+    if (showProperties) {
+      allPropertiesSearchOptions = Object.entries(assetPropertiesCache).reduce((acc, [id, properties]) => {
+        if (properties && properties.length) {
+          for (const property of properties) {
+            const nextProperty = { ...property, id, value: property.name || '' };
+            acc.push(nextProperty);
+          }
+        }
+        return acc;
+      }, reduceResult);
+    }
+
+    const allSearchOptions = [...allAssetsSearchOptions, ...allPropertiesSearchOptions];
+
+    setSearchOptions(allSearchOptions);
+  }, [
+    JSON.stringify(provider?.assetNodes),
+    JSON.stringify(assetPropertiesCache),
+    showProperties,
+    showAssets,
+    showAlarms,
+  ]);
+
+  const onSearchChange = ({ detail }: { detail: BaseChangeDetail }) => {
+    setCrumbsToSearch();
+    setAutosuggestValue(detail.value);
+    let nextPanelItems: ExtendedPanelAssetSummary[] = [];
+    try {
+      const reduceResult: SearchOption[] = [];
+      nextPanelItems = searchOptions.reduce((acc, option) => {
+        if (!option.value.includes(detail.value)) return acc;
+        const nextPanelItem = { ...option, name: option.value };
+        return [...acc, nextPanelItem];
+      }, reduceResult);
+    } catch (e) {
+      console.log(e);
+    }
+    setPanelItems(nextPanelItems);
+  };
 
   return (
     <>
       <Autosuggest
         value={autosuggestValue}
-        onChange={({ detail }) => setAutosuggestValue(detail.value)}
-        options={[
-          { value: 'Suggestion 1' },
-          { value: 'Suggestion 2' },
-          { value: 'Suggestion 3' },
-          { value: 'Suggestion 4' },
-        ]}
+        onChange={onSearchChange}
+        options={searchOptions}
         enteredTextLabel={(value) => `Use: "${value}"`}
-        ariaLabel="Autosuggest example with suggestions"
-        placeholder="Enter value"
-        empty="No matches found"
+        ariaLabel={messageOverrides.resourceExplorer.searchAriaLabel}
+        placeholder={messageOverrides.resourceExplorer.searchPlaceholder}
+        empty={messageOverrides.resourceExplorer.searchEmpty}
       />
 
       <SpaceBetween direction="horizontal" size="xs">
---
+++ packages/dashboard/src/components/resourceExplorer/useAssetProperties.test.tsx
@@ -0,0 +1,26 @@
+import { renderHook, act } from '@testing-library/react-hooks';
+import { useAssetProperties } from './useAssetProperties';
+
+const testAssetId = 'warpcore01';
+const testAssetProperties = [{ id: 'tps', name: 'Teradynes/Second' }];
+
+describe('useAssetProperties', () => {
+  it('returns the starting cache', () => {
+    const { result } = renderHook(() => useAssetProperties());
+    expect(result.current.cache).toEqual({});
+  });
+
+  it('hasKey returns false if a key is missing', () => {
+    const { result } = renderHook(() => useAssetProperties());
+    expect(result.current.hasKey(testAssetId)).toBe(false);
+  });
+
+  it('updates the cache with the right key/values and indicates cache hasKey', () => {
+    const { result } = renderHook(() => useAssetProperties());
+    act(() => {
+      result.current.update(testAssetId, testAssetProperties);
+    });
+    expect(result.current.cache).toEqual({ [testAssetId]: testAssetProperties });
+    expect(result.current.hasKey(testAssetId)).toBe(true);
+  });
+});
