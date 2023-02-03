64aef0666ff42c3885c8f6034b08c4f7021a0336
Brian Diehr <diehbria@amazon.com>
2023-02-02 22:35:36 -0800

    chore(react-components): port over kpi-base and associated helpers

---
+++ packages/react-components/src/kpi/constants.ts
@@ -0,0 +1,6 @@
+/** Font Colors */
+// should be $color-text-form-default https://polaris.a2z.com/fundamentals/foundation/design_tokens/
+export const DEFAULT_FONT_COLOR = '#16191f';
+
+export const FONT_SIZE = 44;
+export const ICON_SIZE = 44 * 0.7;
