52a837694b8490b0cb4a178cf1e8102d2b3ef825
Brian Diehr <diehbria@amazon.com>
2023-02-12 20:46:39 -0800

    chore(dial): port over DialBase from SynchroCharts

---
+++ packages/react-components/src/components/dial/dial.tsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import { DialBase } from './dialBase';
+import { TimeQuery, TimeSeriesData, TimeSeriesDataRequest, StyleSettingsMap, Viewport } from '@iot-app-kit/core';
+import { useTimeSeriesData } from '../../hooks/useTimeSeriesData';
+import { widgetPropertiesFromInputs } from '../../common/widgetPropertiesFromInputs';
+import { Annotations } from '../../common/thresholdTypes';
+import { DialSettings } from './types';
+import { useViewport } from '../../hooks/useViewport';
+
+export const Dial = ({
+  query,
+  viewport: passedInViewport,
+  annotations,
+  styles,
+  settings,
+}: {
+  query: TimeQuery<TimeSeriesData[], TimeSeriesDataRequest>;
+  viewport?: Viewport;
+  annotations?: Annotations;
+  styles?: StyleSettingsMap;
+  settings?: DialSettings;
+}) => {
+  const { dataStreams } = useTimeSeriesData({
+    viewport: passedInViewport,
+    query,
+    settings: { fetchMostRecentBeforeEnd: true },
+    styles,
+  });
+  const { viewport } = useViewport();
+
+  const utilizedViewport = passedInViewport || viewport; // explicitly passed in viewport overrides viewport group
+  const { propertyPoint, alarmPoint, alarmThreshold, propertyThreshold, alarmStream, propertyStream } =
+    widgetPropertiesFromInputs({ dataStreams, annotations, viewport: utilizedViewport });
+
+  const name = propertyStream?.name || alarmStream?.name;
+  const unit = propertyStream?.unit || alarmStream?.unit;
+  const color = alarmThreshold?.color || propertyThreshold?.color || propertyStream?.color;
+
+  return (
+    <DialBase
+      propertyPoint={propertyPoint}
+      alarmPoint={alarmPoint}
+      settings={settings}
+      name={name}
+      unit={unit}
+      color={color}
+    />
+  );
+};
