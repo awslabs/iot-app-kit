9dea96d8b6fb1f59ba2173510ebeb749ebe6233c
tjuranek <thomas@juranek.com>
2023-03-15 15:25:02 -0600

    feat(dashboard): kpi and status widget empty states

--- packages/dashboard/src/customization/widgets/kpi/component.tsx
+++ packages/dashboard/src/customization/widgets/kpi/component.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 import { Kpi } from '@iot-app-kit/react-components';
-
 import { useDataSource } from '../../hooks/useDataSource';
 import { computeQueryConfigKey } from '../utils/computeQueryConfigKey';
 import type { Annotations, YAnnotation } from '@synchro-charts/core';
 import type { DashboardState } from '~/store/state';
 import type { KPIWidget } from '../types';
+import { Box } from '@cloudscape-design/components';
+import './component.css';
 
 const KPIWidgetComponent: React.FC<KPIWidget> = (widget) => {
   const viewport = useSelector((state: DashboardState) => state.dashboardConfiguration.viewport);
@@ -30,8 +31,34 @@ const KPIWidgetComponent: React.FC<KPIWidget> = (widget) => {
     colorDataAcrossThresholds: thresholdSettings?.colorAcrossThresholds,
   };
 
+  const shouldShowEmptyState = queries.length === 0 || !dataSource;
+
+  if (shouldShowEmptyState) {
+    return <KPIWidgetEmptyStateComponent />;
+  }
+
   return (
     <Kpi key={key} queries={queries} viewport={viewport} styleSettings={styleSettings} annotations={annotations} />
+  );
+};
+
+const KPIWidgetEmptyStateComponent: React.FC = () => {
+  return (
+    <div className='kpi-widget-empty-state'>
+      <Box variant='strong' color='text-status-inactive' margin='s'>
+        KPI
+      </Box>
+
+      <div className='kpi-widget-empty-state-message-container'>
+        <Box variant='strong' color='text-status-inactive' margin={{ horizontal: 's' }}>
+          No properties or alarms
+        </Box>
+
+        <Box variant='p' color='text-status-inactive' margin={{ bottom: 's', horizontal: 's' }}>
+          Add a property or alarm to populate KPI chart
+        </Box>
+      </div>
+    </div>
   );
 };
 
