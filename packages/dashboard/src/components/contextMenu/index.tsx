1551867caf948b69ac0d0127c0e26921d7b36f9c
Brian Diehr <diehbria@amazon.com>
2023-03-22 21:17:37 -0700

    style(Dashboard): remove scroll bars when not needed

--- packages/dashboard/src/components/contextMenu/index.tsx
+++ packages/dashboard/src/components/contextMenu/index.tsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from 'react';
-import './index.css';
 import Menu from './menu';
 import ContextMenuSection from './section';
 import ContextMenuOption from './option';
