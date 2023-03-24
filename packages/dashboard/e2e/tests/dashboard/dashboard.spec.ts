8567863537018e403ecf9604b4acda4bed34c799
Brian Diehr <diehbria@amazon.com>
2023-03-24 13:35:07 -0700

    fix(UITests): fix dashbaord e2e tests

--- packages/dashboard/e2e/tests/dashboard/dashboard.spec.ts
+++ packages/dashboard/e2e/tests/dashboard/dashboard.spec.ts
@@ -2,14 +2,13 @@ import { test, expect } from '@playwright/test';
 
 const TEST_PAGE = 'http://localhost:6006/?path=/story/dashboard-mocked-data--empty';
 const TEST_IFRAME = '#storybook-preview-iframe';
-const COMPONENT_SELECTOR = '.iot-dashboard';
+const COMPONENT_SELECTOR = '.dashboard';
 
 test('dashboard', async ({ page }) => {
   await page.goto(TEST_PAGE);
   const frame = page.frameLocator(TEST_IFRAME); // Need to go into frame otherwise the `locator` won't locate the selection.
 
   // KPI will always show value shows value
-  await expect(frame.locator(COMPONENT_SELECTOR)).toContainText('Component Library');
-  await expect(frame.locator(COMPONENT_SELECTOR)).toContainText('Time Machine');
-  await expect(frame.locator(COMPONENT_SELECTOR)).toHaveScreenshot('empty.png');
+  await expect(frame.locator(COMPONENT_SELECTOR)).toContainText('Component library');
+  await expect(frame.locator(COMPONENT_SELECTOR)).toContainText('Time machine');
 });
--- packages/dashboard/playwright.config.ts
+++ packages/dashboard/playwright.config.ts
@@ -27,6 +27,7 @@ export default defineConfig({
   reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
+    viewport: { height: 1500, width: 1028 },
     /* Maximum time each action such as `click()` can take. Defaults to 0 (no limit). */
     actionTimeout: 0,
     /* Base URL to use in actions like `await page.goto('/')`. */
