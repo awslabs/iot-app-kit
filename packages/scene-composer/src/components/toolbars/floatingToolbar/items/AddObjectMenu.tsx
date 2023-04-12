1f817549b2c22d35e6d0d53d415354d3e3ace47f
Emily Dodds <dodemily@amazon.com>
2023-04-12 10:30:06 -0500

    fix(composer): Restores loading indicator for scene composer

--- packages/scene-composer/src/components/toolbars/floatingToolbar/items/AddObjectMenu.tsx
+++ packages/scene-composer/src/components/toolbars/floatingToolbar/items/AddObjectMenu.tsx
@@ -13,10 +13,17 @@ import {
   IModelRefComponent,
   IMotionIndicatorComponent,
   KnownComponentType,
+  KnownSceneProperty,
 } from '../../../../interfaces';
 import { sceneComposerIdContext } from '../../../../common/sceneComposerIdContext';
 import { Component, LightType, ModelType } from '../../../../models/SceneModels';
-import { IColorOverlayComponentInternal, ISceneNodeInternal, useEditorState, useStore } from '../../../../store';
+import {
+  IColorOverlayComponentInternal,
+  ISceneNodeInternal,
+  useEditorState,
+  useStore,
+  useViewOptionState,
+} from '../../../../store';
 import { extractFileNameExtFromUrl, parseS3BucketFromArn } from '../../../../utils/pathUtils';
 import { ToolbarItem } from '../../common/ToolbarItem';
 import { ToolbarItemOptions } from '../../common/types';
@@ -85,7 +92,9 @@ export const AddObjectMenu = () => {
   const enhancedEditingEnabled = getGlobalSettings().featureConfig[COMPOSER_FEATURES.ENHANCED_EDITING];
   const { formatMessage } = useIntl();
   const { activeCameraSettings, mainCameraObject } = useActiveCamera();
-
+  const matterportModelId = useStore(sceneComposerId)((state) =>
+    state.getSceneProperty(KnownSceneProperty.MatterportModelId),
+  );
   const selectedSceneNode = useMemo(() => {
     return getSceneNodeByRef(selectedSceneNodeRef);
   }, [getSceneNodeByRef, selectedSceneNodeRef]);
@@ -138,6 +147,7 @@ export const AddObjectMenu = () => {
         {
           uuid: ObjectTypes.ViewCamera,
           feature: { name: COMPOSER_FEATURES.CameraView },
+          isDisabled: matterportModelId,
         },
         {
           uuid: ObjectTypes.Tag,
@@ -154,7 +164,13 @@ export const AddObjectMenu = () => {
           uuid: ObjectTypes.MotionIndicator,
         },
       ].map(mapToMenuItem),
-    [showAssetBrowserCallback, sceneContainsEnvironmentModel, selectedSceneNodeRef, isEnvironmentNode],
+    [
+      showAssetBrowserCallback,
+      sceneContainsEnvironmentModel,
+      selectedSceneNodeRef,
+      isEnvironmentNode,
+      matterportModelId,
+    ],
   );
 
   const getRefForParenting = useCallback(() => {
