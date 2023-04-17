fe46a4de4c5023582e2c6937fe029af48719256a
Mukesh Sahay <muksahay@amazon.com>
2023-04-17 09:36:58 -0700

    fix(composer): matterport integration bug fixes

--- packages/scene-composer/src/hooks/useMatterportTags.ts
+++ packages/scene-composer/src/hooks/useMatterportTags.ts
@@ -2,9 +2,33 @@ import { useCallback } from 'react';
 
 import { useSceneComposerId } from '../common/sceneComposerIdContext';
 import { IAnchorComponent, ISceneNode, KnownComponentType } from '../interfaces';
-import { IAnchorComponentInternal, ISceneNodeInternal, useStore } from '../store';
+import { IAnchorComponentInternal, IDataOverlayComponentInternal, ISceneNodeInternal, useStore } from '../store';
 import { MattertagItem, TagItem } from '../utils/matterportTagUtils';
 import { RecursivePartial } from '../utils/typeUtils';
+import { Component } from '../models/SceneModels';
+import { generateUUID } from '../utils/mathUtils';
+
+const getContentForOverlayComponent = (label: string, description: string): string => {
+  // Do not change indentation as it affects rendering
+  return `#### **${label}**  
+${description}`;
+};
+
+const getNewDataOverlayComponent = (item: MattertagItem | TagItem): IDataOverlayComponentInternal => {
+  const dataoverlayComponent: IDataOverlayComponentInternal = {
+    ref: generateUUID(),
+    type: KnownComponentType.DataOverlay,
+    subType: Component.DataOverlaySubType.OverlayPanel,
+    valueDataBindings: [],
+    dataRows: [
+      {
+        rowType: Component.DataOverlayRowType.Markdown,
+        content: getContentForOverlayComponent(item.label, item.description),
+      },
+    ],
+  };
+  return dataoverlayComponent;
+};
 
 const addTag = (
   addSceneNode: (node: ISceneNode, parentRef?: string) => Readonly<ISceneNode>,
@@ -15,9 +39,10 @@ const addTag = (
     type: KnownComponentType.Tag,
     offset: [item.stemVector.x, item.stemVector.y, item.stemVector.z],
   };
+  const dataoverlayComponent = getNewDataOverlayComponent(item);
   const node = {
     name: item.label,
-    components: [anchorComponent],
+    components: [anchorComponent, dataoverlayComponent],
     transform: {
       position: [item.anchorPosition.x, item.anchorPosition.y, item.anchorPosition.z],
       rotation: [0, 0, 0],
@@ -27,6 +52,7 @@ const addTag = (
       matterportId: id, //mattertag uses item.sid and tag uses item.id so we just us the collection key for both
     },
   } as ISceneNode;
+
   addSceneNode(node);
 };
 
@@ -44,6 +70,21 @@ const updateTag = (
       ...components[tagIndex],
       offset: [item.stemVector.x, item.stemVector.y, item.stemVector.z],
     } as IAnchorComponentInternal;
+  }
+  const dataOverlayIndex = node.components.findIndex((elem) => elem.type === KnownComponentType.DataOverlay);
+  if (dataOverlayIndex !== -1) {
+    components[dataOverlayIndex] = {
+      ...components[dataOverlayIndex],
+      dataRows: [
+        {
+          rowType: Component.DataOverlayRowType.Markdown,
+          content: getContentForOverlayComponent(item.label, item.description),
+        },
+      ],
+    } as IDataOverlayComponentInternal;
+  } else {
+    const dataoverlayComponent = getNewDataOverlayComponent(item);
+    components.push(dataoverlayComponent);
   }
   updateSceneNode(ref, {
     name: item.label,
