a176c1950c8fe56ecdecadf7e487fe14a6b40612
Mohamed Elmuhtadi <moelmu@amazon.com>
2023-05-04 17:25:47 -0700

    feat(Knowledge Graph Comp): Knowledge Graph DataSource
    feat(Knowledge Graph Comp): Adding queryStatement as a function parameter
    
    feat(Knowledge Graph Comp): Fix maxResults Bug
    
    feat(Knowledge Graph): Knowledge Graph DataSource
    
    feat(Knowledge Graph): TwinMaker Knowledge Graph DataSource

---
+++ packages/source-iottwinmaker/src/knowledgeGraph-module/KGDataModule.ts
@@ -0,0 +1,51 @@
+import {
+  ExecuteQueryCommand,
+  ExecuteQueryCommandOutput,
+  ExecuteQueryCommandInput,
+  IoTTwinMakerClient,
+  ColumnDescription,
+  Row,
+} from '@aws-sdk/client-iottwinmaker';
+import { KGDataModuleInput, executeQueryParams } from './types';
+import { TwinMakerKGQueryDataModule } from '../types';
+
+export class KGDataModule implements TwinMakerKGQueryDataModule {
+  private twinMakerClient: IoTTwinMakerClient;
+  private workspaceId: string;
+
+  constructor(input: KGDataModuleInput) {
+    this.twinMakerClient = input.twinMakerClient;
+    this.workspaceId = input.workspaceId;
+  }
+
+  executeQuery = async ({
+    queryStatement,
+    resultsPerPage,
+    maxPagesCount,
+  }: executeQueryParams): Promise<ExecuteQueryCommandOutput> => {
+    const rows: Row[] = [];
+    let columnDescriptions: ColumnDescription[] = [];
+    let $metadata = {};
+    let pageCounter = 0;
+    const commandInput: ExecuteQueryCommandInput = {
+      workspaceId: this.workspaceId,
+      queryStatement: queryStatement,
+      maxResults: resultsPerPage,
+    };
+    do {
+      const response: ExecuteQueryCommandOutput = await this.twinMakerClient.send(
+        new ExecuteQueryCommand(commandInput)
+      );
+      pageCounter = pageCounter + 1;
+      if (response.rows) {
+        rows.push(...response.rows);
+      }
+      if (response.columnDescriptions && columnDescriptions.length === 0) {
+        columnDescriptions = response.columnDescriptions;
+      }
+      if (response.$metadata && Object.keys($metadata).length === 0) $metadata = response.$metadata;
+      commandInput.nextToken = response.nextToken;
+    } while (maxPagesCount ? commandInput.nextToken && pageCounter < maxPagesCount : commandInput.nextToken);
+    return { rows, columnDescriptions, $metadata };
+  };
+}
---
+++ packages/source-iottwinmaker/src/knowledgeGraph-module/types.ts
@@ -0,0 +1,10 @@
+import { IoTTwinMakerClient } from '@aws-sdk/client-iottwinmaker';
+export type KGDataModuleInput = {
+  workspaceId: string;
+  twinMakerClient: IoTTwinMakerClient;
+};
+export type executeQueryParams = {
+  queryStatement: string;
+  resultsPerPage?: number;
+  maxPagesCount?: number;
+};
