2d38a3da61957daa6e5ca9341032db55b0915e61
Divya Kamath <divyasr@amazon.com>
2023-07-05 11:10:49 -0700

    feat: color picker changes

--- packages/scene-composer/src/components/panels/scene-components/tag-style/ColorPicker/ColorPicker.tsx
+++ packages/scene-composer/src/components/panels/scene-components/tag-style/ColorPicker/ColorPicker.tsx
@@ -15,8 +15,8 @@ interface IColorPickerProps {
 }
 export const ColorPicker = ({ color, onSelectColor, label }: IColorPickerProps) => {
   const [showPicker, setShowPicker] = useState(false);
-  const [currentColor, setCurrentColor] = useState(color);
-  const [hexColor, setHexColor] = useState(currentColor);
+  // const [currentColor, setCurrentColor] = useState(color);
+  // const [hexColor, setHexColor] = useState(currentColor);
   const { formatMessage } = useIntl();
   const handleClick = () => {
     setShowPicker(!showPicker);
@@ -24,15 +24,12 @@ export const ColorPicker = ({ color, onSelectColor, label }: IColorPickerProps) 
 
   const handleColorChange = useCallback(
     (color) => {
-      setCurrentColor(color.hex);
-      setHexColor(color.hex);
       onSelectColor(color.hex);
     },
     [color],
   );
 
   const handleHexCodeChange = useCallback((event: NonCancelableCustomEvent<InputProps.ChangeDetail>) => {
-    setHexColor(event.detail.value);
     onSelectColor(event.detail.value);
   }, []);
 
@@ -46,23 +43,21 @@ export const ColorPicker = ({ color, onSelectColor, label }: IColorPickerProps) 
             size='big'
             svg={
               <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 52 52' width='52px' height='52px'>
-                <g fill={currentColor} fillRule='evenodd' transform='translate(1 1)'>
-                  <circle cx='25' cy='25' r='16' fill={currentColor} />
+                <g fill={color} fillRule='evenodd' transform='translate(1 1)'>
+                  <circle cx='25' cy='25' r='16' fill={color} />
                 </g>
               </svg>
             }
           />
         }
         onClick={() => {
-          setCurrentColor(color);
-          setHexColor(color);
           onSelectColor(color);
           handleClick();
         }}
       />
-        <Input value={hexColor} onChange={handleHexCodeChange} placeholder='Hex code' />
+        <Input value={color} onChange={handleHexCodeChange} placeholder='Hex code' />
       {showPicker && (
-        <CirclePicker aria-label='color' colors={colors} color={currentColor} onChangeComplete={handleColorChange} />
+        <CirclePicker aria-label='color' colors={colors} color={color} onChangeComplete={handleColorChange} />
         )}
     </SpaceBetween>
   );
