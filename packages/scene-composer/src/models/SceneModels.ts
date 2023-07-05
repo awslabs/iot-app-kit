4323b15d4140b2f36e0b651ce0ab40090fc31151
Divya Kamath <divyasr@amazon.com>
2023-07-05 11:54:45 -0700

    feat: color picker changes

--- packages/scene-composer/src/models/SceneModels.ts
+++ packages/scene-composer/src/models/SceneModels.ts
@@ -162,6 +162,7 @@ export namespace Component {
     icon?: string;
     navLink?: NavLink;
     offset?: Vector3;
+    chosenColor?: string;
   }
 
   export interface ModelShader extends IComponent, IDataBindingRuleMap {}
