b8475b38935aabe28ac9a5a5f46109c79d5fa9f6
Weston Harper <haweston@amazon.com>
2023-07-10 18:53:27 -0500

    chore(scene): add DataQuery field to storybook for testing options

---
+++ packages/scene-composer/stories/components/hooks/useDatasource.ts
@@ -0,0 +1,24 @@
+import { useMemo } from 'react';
+import { initialize } from '@iot-app-kit/source-iottwinmaker';
+import { CredentialProvider, Credentials } from '@aws-sdk/types';
+
+const region = 'us-east-1';
+const rociEndpoint = 'https://iottwinmaker.us-east-1.amazonaws.com';
+
+const useDataSource = (
+  awsCredentials: Credentials | CredentialProvider | undefined,
+  workspaceId: string | undefined,
+) => {
+  const datasource = useMemo(() => {
+    const datasource = initialize(workspaceId!, {
+      awsCredentials: awsCredentials!,
+      awsRegion: region,
+      tmEndpoint: rociEndpoint,
+    });
+    return datasource;
+  }, [awsCredentials, workspaceId]);
+
+  return datasource;
+};
+
+export default useDataSource;
