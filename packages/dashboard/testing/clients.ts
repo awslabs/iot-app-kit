c62e68bc9181ad2d6995097fb60638f41dc168ab
Tracy French <67283114+tracy-french@users.noreply.github.com>
2023-07-11 15:05:01 -0600

    fix: use REGION env variable in dashboard testing environment

--- packages/dashboard/testing/clients.ts
+++ packages/dashboard/testing/clients.ts
@@ -2,15 +2,14 @@ import { IoTSiteWiseClient } from '@aws-sdk/client-iotsitewise';
 import { IoTEventsClient } from '@aws-sdk/client-iot-events';
 
 import { getEnvCredentials } from './getEnvCredentials';
-
-const REGION = 'us-west-2';
+import { TEST_REGION } from './constants';
 
 export const iotSiteWiseClient = new IoTSiteWiseClient({
   credentials: getEnvCredentials(),
-  region: REGION,
+  region: TEST_REGION,
 });
 
 export const iotEventsClient = new IoTEventsClient({
   credentials: getEnvCredentials(),
-  region: REGION,
+  region: TEST_REGION,
 });
---
+++ packages/dashboard/testing/constants.ts
@@ -0,0 +1,6 @@
+/**
+ * Region used for testing.
+ *
+ * Configure the value within `.env` at the root of the dashboard package.
+ */
+export const TEST_REGION = process.env.REGION ?? 'us-west-2';
