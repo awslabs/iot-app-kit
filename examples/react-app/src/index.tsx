7b5a4e452b1e4cee2ec13220b0b2c828906f27db
Mohamed Elmuhtadi <moelmu@amazon.com>
2023-08-01 14:22:05 -0700

    fix(reactExample): fix css in Knowledge Graph Component

--- examples/react-app/src/index.tsx
+++ examples/react-app/src/index.tsx
@@ -7,6 +7,8 @@ import { StrictMode } from 'react';
 import router from './router';
 
 import { Density, Mode, applyDensity, applyMode } from '@cloudscape-design/global-styles';
+import '@cloudscape-design/global-styles/index.css';
+import '@iot-app-kit/components/styles.css';
 
 applyDensity(Density.Comfortable);
 applyMode(Mode.Dark);
