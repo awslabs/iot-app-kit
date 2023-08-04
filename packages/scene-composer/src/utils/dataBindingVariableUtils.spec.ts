3c4820cf923dacfaa749614739b9a28b2b8802e0
Xinyi Xu <sheilaxxy@gmail.com>
2023-08-04 09:40:35 -0700

    fix(composer): fix issue displaying 0 in overlay

--- packages/scene-composer/src/utils/dataBindingVariableUtils.spec.ts
+++ packages/scene-composer/src/utils/dataBindingVariableUtils.spec.ts
@@ -1,7 +1,9 @@
 import { replaceBindingVariables } from './dataBindingVariableUtils';
 
 describe('replaceBindingVariables', () => {
-  const bindingValuesMap: Record<string, string> = {
+  const bindingValuesMap: Record<string, unknown> = {
+    zero: 0,
+    false: false,
     'binding a': '11',
     'binding~!@#$%^&*()': '22',
     '<随机>': '33',
@@ -10,6 +12,22 @@ describe('replaceBindingVariables', () => {
   it('should replace variable with value correctly', () => {
     const original = 'abc ${binding a} def,.';
     const expected = 'abc 11 def,.';
+
+    const result = replaceBindingVariables(original, bindingValuesMap);
+    expect(result).toEqual(expected);
+  });
+
+  it('should replace variable with value 0 correctly', () => {
+    const original = 'abc ${zero} def,.';
+    const expected = 'abc 0 def,.';
+
+    const result = replaceBindingVariables(original, bindingValuesMap);
+    expect(result).toEqual(expected);
+  });
+
+  it('should replace variable with value false correctly', () => {
+    const original = 'abc ${false} def,.';
+    const expected = 'abc false def,.';
 
     const result = replaceBindingVariables(original, bindingValuesMap);
     expect(result).toEqual(expected);
