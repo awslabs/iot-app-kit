ef2ad21f8ec525093ff704492bded9e99364a046
Nischay Maridevegowda <mnischay@amazon.com>
2023-08-04 15:36:13 -0400

    chore(react-components): add per chart context menu test cases

--- packages/react-components/src/components/chart/contextMenu/ChartContextMenu.spec.tsx
+++ packages/react-components/src/components/chart/contextMenu/ChartContextMenu.spec.tsx
@@ -1,20 +1,92 @@
-import { describe, expect } from '@jest/globals';
-import { render } from '@testing-library/react';
+import { beforeEach, describe, expect } from '@jest/globals';
+import { fireEvent, render, RenderResult } from '@testing-library/react';
 import React from 'react';
 import ChartContextMenu from './ChartContextMenu';
+import { InternalGraphicComponentGroupOption } from '../types';
 
 describe('ChartContextMenu', () => {
   const menuOptionClickHandler = jest.fn();
   const onOutSideClickHandler = jest.fn();
-  const component = render(
-    <ChartContextMenu
-      menuOptionClickHandler={menuOptionClickHandler}
-      onOutSideClickHandler={onOutSideClickHandler}
-      position={{ x: 100, y: 100 }}
-      trendCursors={[]}
-    />
-  );
+  let component: RenderResult;
+  beforeEach(() => {
+    component = render(
+      <ChartContextMenu
+        menuOptionClickHandler={menuOptionClickHandler}
+        onOutSideClickHandler={onOutSideClickHandler}
+        position={{ x: 100, y: 100 }}
+        trendCursors={[{} as InternalGraphicComponentGroupOption]}
+      />
+    );
+  });
+
   it('renders', () => {
     expect(component).not.toBeNull();
+  });
+
+  it('menuOptionClickHandler should be called with add when "Add trend cursor" is clicked', () => {
+    const { getByText } = component;
+    const clickIndicator = getByText('Add trend cursor');
+    fireEvent.click(clickIndicator);
+    expect(menuOptionClickHandler).toBeCalledWith(
+      expect.objectContaining({
+        action: 'add',
+      })
+    );
+  });
+
+  it('menuOptionClickHandler should be called with delete when "Delete trend cursor" is clicked', () => {
+    const { getByText } = component;
+    const clickIndicator = getByText('Delete trend cursor');
+    fireEvent.click(clickIndicator);
+    expect(menuOptionClickHandler).toBeCalledWith(
+      expect.objectContaining({
+        action: 'delete',
+      })
+    );
+  });
+
+  it('menuOptionClickHandler should be called with copy when "Copy to clipboard" is clicked', () => {
+    const { getByText } = component;
+    const clickIndicator = getByText('Copy to clipboard');
+    fireEvent.click(clickIndicator);
+    expect(menuOptionClickHandler).toBeCalledWith(
+      expect.objectContaining({
+        action: 'copy',
+      })
+    );
+  });
+
+  it('menuOptionClickHandler cannot be called with delete when graphic.length  is <= 0', () => {
+    const { getAllByText } = render(
+      <ChartContextMenu
+        menuOptionClickHandler={menuOptionClickHandler}
+        onOutSideClickHandler={onOutSideClickHandler}
+        position={{ x: 100, y: 100 }}
+        trendCursors={[]}
+      />
+    );
+    const clickIndicator = getAllByText('Delete trend cursor')[0];
+    fireEvent.click(clickIndicator);
+    expect(menuOptionClickHandler).not.toBeCalledWith();
+  });
+
+  it('menuOptionClickHandler cannot be called with add when graphic.length > 5', () => {
+    const { getAllByText } = render(
+      <ChartContextMenu
+        menuOptionClickHandler={menuOptionClickHandler}
+        onOutSideClickHandler={onOutSideClickHandler}
+        position={{ x: 100, y: 100 }}
+        trendCursors={[
+          {} as InternalGraphicComponentGroupOption,
+          {} as InternalGraphicComponentGroupOption,
+          {} as InternalGraphicComponentGroupOption,
+          {} as InternalGraphicComponentGroupOption,
+          {} as InternalGraphicComponentGroupOption,
+        ]}
+      />
+    );
+    const clickIndicator = getAllByText('Add trend cursor')[0];
+    fireEvent.click(clickIndicator);
+    expect(menuOptionClickHandler).not.toBeCalledWith();
   });
 });
