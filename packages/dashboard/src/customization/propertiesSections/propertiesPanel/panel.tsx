149545160551174af83ce3ddb2a61f1a61977c94
Harry Wandersman <hwanders@amazon.com>
2023-09-13 12:14:22 -0400

    feat(dashboard): update collapsible side panels

--- packages/dashboard/src/customization/propertiesSections/propertiesPanel/panel.tsx
+++ packages/dashboard/src/customization/propertiesSections/propertiesPanel/panel.tsx
@@ -1,7 +1,5 @@
 import React from 'react';
 
-import { colorBorderDividerDefault } from '@cloudscape-design/design-tokens';
-import Header from '@cloudscape-design/components/header';
 import Box from '@cloudscape-design/components/box';
 import Tabs from '@cloudscape-design/components/tabs';
 
@@ -10,7 +8,6 @@ import { useSelection } from '../../propertiesSection';
 import { PropertiesPanelEmpty } from './emptyPanel';
 import { StylesSection } from './styleTab';
 
-import './panel.css';
 import SpaceBetween from '@cloudscape-design/components/space-between';
 import { PropertiesAndAlarmsSettingsConfiguration } from '../propertiesAndAlarmsSettings';
 import { ThresholdSettingsConfiguration } from '../thresholdSettings';
@@ -19,47 +16,37 @@ import { ThresholdSettingsConfiguration } from '../thresholdSettings';
 export const PropertiesPanel = () => {
   const selection = useSelection();
 
-  return (
-    <div className='properties-panel'>
-      <div className='properties-panel-header-container' style={{ borderColor: colorBorderDividerDefault }}>
-        <Box padding='m'>
-          <Header variant='h3'>Configuration</Header>
-        </Box>
-      </div>
-
-      {selection ? (
-        <Box padding={{ horizontal: 'm', bottom: 'l' }}>
-          <Tabs
-            tabs={[
-              {
-                label: 'Style',
-                id: 'style',
-                content: <StylesSection />,
-              },
-              {
-                label: 'Properties & Alarms',
-                id: 'propertiesAndAlarms',
-                content: (
-                  <SpaceBetween size='xs' direction='vertical'>
-                    <PropertiesAndAlarmsSettingsConfiguration />
-                  </SpaceBetween>
-                ),
-              },
-              {
-                label: 'Thresholds',
-                id: 'thresholds',
-                content: (
-                  <SpaceBetween size='xs' direction='vertical'>
-                    <ThresholdSettingsConfiguration />
-                  </SpaceBetween>
-                ),
-              },
-            ]}
-          />
-        </Box>
-      ) : (
-        <PropertiesPanelEmpty />
-      )}
-    </div>
+  return selection ? (
+    <Box padding={{ horizontal: 'm', bottom: 'l' }}>
+      <Tabs
+        tabs={[
+          {
+            label: 'Style',
+            id: 'style',
+            content: <StylesSection />,
+          },
+          {
+            label: 'Properties & Alarms',
+            id: 'propertiesAndAlarms',
+            content: (
+              <SpaceBetween size='xs' direction='vertical'>
+                <PropertiesAndAlarmsSettingsConfiguration />
+              </SpaceBetween>
+            ),
+          },
+          {
+            label: 'Thresholds',
+            id: 'thresholds',
+            content: (
+              <SpaceBetween size='xs' direction='vertical'>
+                <ThresholdSettingsConfiguration />
+              </SpaceBetween>
+            ),
+          },
+        ]}
+      />
+    </Box>
+  ) : (
+    <PropertiesPanelEmpty />
   );
 };
