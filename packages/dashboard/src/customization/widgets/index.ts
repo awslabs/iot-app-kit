076f15129b4a1e61b4ef57467031210365ef58cb
Jason Buss <jmbuss@amazon.com>
2023-09-28 14:32:00 -0600

    feat(dashboard): add echart line-scatter-widget
    BREAKING CHANGE: remove line and scatter widgets

--- packages/dashboard/src/customization/widgets/index.ts
+++ packages/dashboard/src/customization/widgets/index.ts
@@ -1,7 +1,5 @@
 export * from './barChart/plugin';
 export * from './kpi/plugin';
-export * from './lineChart/plugin';
-export * from './scatterChart/plugin';
 export * from './status-timeline/statusTimelinePlugin';
 export * from './table/plugin';
 export * from './text/plugin';
