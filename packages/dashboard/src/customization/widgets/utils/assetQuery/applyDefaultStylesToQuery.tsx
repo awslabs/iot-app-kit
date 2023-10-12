254f59c3e2dc49ca732273d0b4329d0b716f3c32
Sahil Jagad <sahiljag@amazon.com>
2023-10-12 14:46:30 -0400

    fix: line charts colors too quickly choose similar colors

--- packages/dashboard/src/customization/widgets/utils/assetQuery/applyDefaultStylesToQuery.tsx
+++ packages/dashboard/src/customization/widgets/utils/assetQuery/applyDefaultStylesToQuery.tsx
@@ -1,10 +1,13 @@
 import { assignDefaultColor } from '@iot-app-kit/core-util';
 import type { IoTSiteWiseDataStreamQuery } from '~/types';
 
-export const applyDefaultStylesToQuery = ({ assets = [], properties = [] }: IoTSiteWiseDataStreamQuery) => ({
-  assets: assets.map(({ properties, ...others }, assetIndex) => ({
-    ...others,
-    properties: properties.map((propertyQuery, propIndex) => assignDefaultColor(propertyQuery, assetIndex + propIndex)),
-  })),
-  properties: properties.map((property, propertyIndex) => assignDefaultColor(property, propertyIndex)),
-});
+export const applyDefaultStylesToQuery = ({ assets = [], properties = [] }: IoTSiteWiseDataStreamQuery) => {
+  let offset = 0;
+  return {
+    assets: assets.map(({ properties, ...others }) => ({
+      ...others,
+      properties: properties.map((propertyQuery) => assignDefaultColor(propertyQuery, offset++)),
+    })),
+    properties: properties.map((property, propertyIndex) => assignDefaultColor(property, propertyIndex + offset++)),
+  };
+};
