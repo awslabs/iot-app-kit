a8cfdd0bd23bca975d158682711794928a4957c5
Priyanshu44 <priyanshu@happiestminds.com>
2023-10-18 09:55:17 +0530

    feat: added widget text in the widget panel

--- packages/dashboard/src/components/palette/index.tsx
+++ packages/dashboard/src/components/palette/index.tsx
@@ -1,20 +1,42 @@
 import React, { memo } from 'react';
 import SpaceBetween from '@cloudscape-design/components/space-between';
-import { spaceFieldHorizontal } from '@cloudscape-design/design-tokens';
+import {
+  colorBorderDividerDefault,
+  colorTextBodyDefault,
+  spaceFieldHorizontal,
+  spaceScaledS,
+  spaceScaledXxxs,
+  spaceStaticXxl,
+} from '@cloudscape-design/design-tokens';
 import {
   ComponentLibraryComponentMap,
   ComponentLibraryComponentOrdering,
 } from '~/customization/componentLibraryComponentMap';
 import PaletteComponent from './component';
+import './index.css';
 
 /* added left padding as per UX and top padding for positioning Icons Vertically centre */
 const palettePadding = {
   padding: `${spaceFieldHorizontal} ${0} ${0} ${spaceFieldHorizontal}`,
 };
 
+const widgetFont = {
+  color: colorTextBodyDefault,
+};
+
+const divider = {
+  borderLeft: `solid ${spaceScaledXxxs} ${colorBorderDividerDefault}`,
+  height: spaceStaticXxl,
+  margin: `0 ${spaceScaledS}`,
+};
+
+const Divider = () => <div style={divider} />;
+
 const Palette = () => {
   return (
-    <div style={palettePadding}>
+    <div className='widget-panel' style={palettePadding}>
+      <h4 style={widgetFont}>Widgets</h4>
+      <Divider />
       <SpaceBetween size='xxxs' direction='horizontal'>
         {ComponentLibraryComponentOrdering.map((widgetType) => {
           const [name, iconComponent] = ComponentLibraryComponentMap[widgetType];
