e8a6f5d39b0aa49c1d8f9fc4938649305b27825a
Tracy French <67283114+tracy-french@users.noreply.github.com>
2023-10-24 16:07:45 -0600

    fix: do not override selected data stream preferences

---
+++ packages/dashboard/src/components/queryEditor/iotSiteWiseQueryEditor/queryExtender/queryExtender.spec.ts
@@ -0,0 +1,164 @@
+import { ModeledDataStream } from '../modeledDataStreamQueryEditor/modeledDataStreamExplorer/types';
+import { UnmodeledDataStream } from '../unmodeledDataStreamExplorer/types';
+import { QueryExtender } from './queryExtender';
+
+describe(QueryExtender.name, () => {
+  describe('extendAssetQueries', () => {
+    it('should extend the current query with new asset queries', () => {
+      const currentQuery = {
+        assets: [
+          {
+            assetId: 'asset-1',
+            properties: [{ propertyId: 'property-1' }],
+          },
+        ],
+        properties: [{ propertyAlias: 'property-1' }],
+      };
+      const queryExtender = new QueryExtender(currentQuery);
+      const modeledDataStreams = [
+        {
+          assetId: 'asset-2',
+          propertyId: 'property-2',
+        },
+      ] as ModeledDataStream[];
+
+      const extendedQuery = queryExtender.extendAssetQueries(modeledDataStreams);
+
+      expect(extendedQuery).toEqual({
+        assets: [
+          {
+            assetId: 'asset-1',
+            properties: [{ propertyId: 'property-1' }],
+          },
+          {
+            assetId: 'asset-2',
+            properties: [{ propertyId: 'property-2' }],
+          },
+        ],
+        properties: [{ propertyAlias: 'property-1' }],
+      });
+    });
+
+    it('should dedupe asset queries with properties', () => {
+      const currentQuery = {
+        assets: [
+          {
+            assetId: 'asset-1',
+            properties: [{ propertyId: 'property-1' }],
+          },
+        ],
+      };
+      const queryExtender = new QueryExtender(currentQuery);
+      const modeledDataStreams = [
+        {
+          assetId: 'asset-1',
+          propertyId: 'property-1',
+        },
+        {
+          assetId: 'asset-1',
+          propertyId: 'property-2',
+        },
+      ] as ModeledDataStream[];
+
+      const extendedQuery = queryExtender.extendAssetQueries(modeledDataStreams);
+
+      expect(extendedQuery).toEqual({
+        assets: [
+          {
+            assetId: 'asset-1',
+            properties: [{ propertyId: 'property-1' }, { propertyId: 'property-2' }],
+          },
+        ],
+      });
+    });
+
+    it('should initialize with an empty query when no argument is passed', () => {
+      const queryExtender = new QueryExtender();
+      const extendedQuery = queryExtender.extendAssetQueries([]);
+
+      expect(extendedQuery).toEqual({ assets: [] });
+    });
+
+    it('should return the current query when an empty array is passed to extendAssetQueries', () => {
+      const currentQuery = {
+        assets: [
+          {
+            assetId: 'asset-1',
+            properties: [{ propertyId: 'property-1' }],
+          },
+        ],
+        properties: [{ propertyAlias: 'property-1' }],
+      };
+      const queryExtender = new QueryExtender(currentQuery);
+      const extendedQuery = queryExtender.extendAssetQueries([]);
+
+      expect(extendedQuery).toEqual(currentQuery);
+    });
+  });
+
+  describe('extendPropertyAliasQueries', () => {
+    it('should extend the current query with new property alias queries', () => {
+      const currentQuery = {
+        assets: [
+          {
+            assetId: 'asset-1',
+            properties: [{ propertyId: 'property-1' }],
+          },
+        ],
+        properties: [{ propertyAlias: 'property-1' }],
+      };
+      const queryExtender = new QueryExtender(currentQuery);
+      const unmodeledDataStreams = [{ propertyAlias: 'property-2' }] as UnmodeledDataStream[];
+
+      const extendedQuery = queryExtender.extendPropertyAliasQueries(unmodeledDataStreams);
+
+      expect(extendedQuery).toEqual({
+        assets: [
+          {
+            assetId: 'asset-1',
+            properties: [{ propertyId: 'property-1' }],
+          },
+        ],
+        properties: [{ propertyAlias: 'property-1' }, { propertyAlias: 'property-2' }],
+      });
+    });
+
+    it('should dedupe property alias queries', () => {
+      const currentQuery = {
+        properties: [{ propertyAlias: 'property-1' }],
+      };
+      const queryExtender = new QueryExtender(currentQuery);
+      const unmodeledDataStreams = [
+        { propertyAlias: 'property-1' },
+        { propertyAlias: 'property-2' },
+      ] as UnmodeledDataStream[];
+
+      const extendedQuery = queryExtender.extendPropertyAliasQueries(unmodeledDataStreams);
+
+      expect(extendedQuery.properties).toEqual([{ propertyAlias: 'property-1' }, { propertyAlias: 'property-2' }]);
+    });
+  });
+
+  it('should initialize with an empty query when no argument is passed', () => {
+    const queryExtender = new QueryExtender();
+    const extendedQuery = queryExtender.extendPropertyAliasQueries([]);
+
+    expect(extendedQuery).toEqual({ properties: [] });
+  });
+
+  it('should return the current query when an empty array is passed to extendAssetQueries', () => {
+    const currentQuery = {
+      assets: [
+        {
+          assetId: 'asset-1',
+          properties: [{ propertyId: 'property-1' }],
+        },
+      ],
+      properties: [{ propertyAlias: 'property-1' }],
+    };
+    const queryExtender = new QueryExtender(currentQuery);
+    const extendedQuery = queryExtender.extendPropertyAliasQueries([]);
+
+    expect(extendedQuery).toEqual(currentQuery);
+  });
+});
