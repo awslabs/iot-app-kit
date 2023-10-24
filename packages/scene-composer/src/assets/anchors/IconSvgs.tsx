0cecac14f2ff201f3f5eb16446b7aab43acf10ce
Mohamed Elmuhtadi <moelmu@amazon.com>
2023-10-24 00:58:29 -0700

    fix(composer): custom tag rendered slightly off the center

--- packages/scene-composer/src/assets/anchors/IconSvgs.tsx
+++ packages/scene-composer/src/assets/anchors/IconSvgs.tsx
@@ -66,10 +66,10 @@ export const SelectedIconSvgString = `
 
 export const CustomIconSvgString = `
   <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 52 52' width='256' height='256'>
-    <g fill='none' fillRule='evenodd' transform='translate(2 2)'>
-      <ellipse cx='26' cy='26' rx='21' ry='21' stroke='${colors.customBlue}' stroke-width='2' />
-      <circle cx='26' cy='26' r='16' fill='${colors.customBlue}'/>
-      <g transform='translate(26,26) scale(0.03) translate(${iconWidth}, ${iconHeight})' fill=''>
+    <g fill='none' fillRule='evenodd' transform='translate(1 1)'>
+      <ellipse cx='25' cy='25' rx='21' ry='21' stroke='${colors.customBlue}' stroke-width='2' />
+      <circle cx='25' cy='25' r='16' fill='${colors.customBlue}'/>
+      <g transform='translate(25,25) scale(0.03) translate(${iconWidth}, ${iconHeight})' fill=''>
         <path d='M22 80a48 48 0 1 1 96 0A48 48 0 1 1 48 80zM 224c0-17.7 14.3-32 32-32H96c17.7 0 32 14.3 32 32V448h32c17.7 0 32 14.3 32 32s-14.3 32-32 32H32c-17.7 0-32-14.3-32-32s14.3-32 32-32H64V256H32c-17.7 0-32-14.3-32-32z' fill=''/>
       </g>
     </g>
