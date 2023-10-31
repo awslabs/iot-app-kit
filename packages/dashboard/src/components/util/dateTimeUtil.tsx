d5e9a1c0adc2a0ed836bbb32a10a3a335c126570
Chandru_HM <chandrashekhara.n@happiestminds.com>
2023-10-31 14:55:55 +0530

    feat: format latest value time in resource explorer #2140

--- packages/dashboard/src/components/util/dateTimeUtil.tsx
+++ packages/dashboard/src/components/util/dateTimeUtil.tsx
@@ -1,7 +1,17 @@
 // Format Date object to get date and time to display MM/DD/YY HH:MM:SS format
-export const getFormattedDateTime = (rawDate: Date) => {
+const formatDateTime = (rawDate: Date) => {
   const date = rawDate.getMonth() + 1 + '/' + rawDate.getDate() + '/' + rawDate.getFullYear().toString().slice(-2);
   const time = rawDate.toTimeString().split(' ')[0];
   const dateTime = date + ' ' + time;
   return dateTime;
+};
+
+export const getFormattedDateTime = (rawDate: Date) => {
+  return formatDateTime(rawDate);
+};
+
+export const getFormattedDateTimeFromEpoch = (epochSeconds: number | undefined) => {
+  if (!epochSeconds) return '';
+  const rawDate = new Date(epochSeconds * 1000);
+  return formatDateTime(rawDate);
 };
