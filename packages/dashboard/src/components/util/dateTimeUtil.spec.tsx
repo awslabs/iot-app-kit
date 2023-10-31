40864b451a46be65ecc5a2badb96adc76f98d26d
Chandru_HM <chandrashekhara.n@happiestminds.com>
2023-10-31 14:55:55 +0530

    feat: format latest value time in resource explorer #2140

--- packages/dashboard/src/components/util/dateTimeUtil.spec.tsx
+++ packages/dashboard/src/components/util/dateTimeUtil.spec.tsx
@@ -1,6 +1,12 @@
-import { getFormattedDateTime } from './dateTimeUtil';
+import { getFormattedDateTime, getFormattedDateTimeFromEpoch } from './dateTimeUtil';
 
 it('should format the Date to full Date and Time value', () => {
   const rawDate = new Date(1665583620000 + new Date().getTimezoneOffset() * 60000);
   expect(getFormattedDateTime(rawDate)).toEqual(`10/12/22 14:07:00`);
+});
+
+it('should format the epoch seconds to Date and Time value', () => {
+  const rawDate = 1698641538;
+  const time = new Date(rawDate * 1000).toTimeString().split(' ')[0];
+  expect(getFormattedDateTimeFromEpoch(rawDate)).toEqual(`10/30/23 ${time}`);
 });
