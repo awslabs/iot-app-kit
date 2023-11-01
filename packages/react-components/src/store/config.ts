b39b1bc187ca497d70d163c0ea0f1bde2164bebb
Luciana Corteggiano <luciana.corteggiano@gmail.com>
2023-11-01 13:53:45 -0400

    feat(dashboard): gated CSV download button

--- packages/react-components/src/store/config.ts
+++ packages/react-components/src/store/config.ts
@@ -1,11 +1,12 @@
 import { StateCreator } from 'zustand/esm';
 
-export type Flags = 'useEcharts';
+export type Flags = 'useEcharts' | 'useCSVDownload';
 export interface ConfigState {
   config: Record<Flags, boolean>;
 }
 export const createConfigSlice: StateCreator<ConfigState> = () => ({
   config: {
     useEcharts: !!localStorage?.getItem('USE_ECHARTS'),
+    useCSVDownload: !!localStorage?.getItem('USE_CSV_DOWNLOAD'),
   },
 });
