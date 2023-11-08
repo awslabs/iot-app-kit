b3d87cb9bb4d82c3c5bdfc5c40feb614024c9e7f
Jason Buss <jmbuss@amazon.com>
2023-11-08 12:16:55 -0700

    feat(dashboard): add e2e tests

---
+++ packages/dashboard/e2e/tests/constants.ts
@@ -0,0 +1,13 @@
+export const TEST_PAGE = '/iframe.html?id=dashboard-mocked-data--empty';
+export const TEST_IFRAME = '#root';
+export const COMPONENT_SELECTOR = '.dashboard';
+
+export const TOOLBAR_FRAME = '.dashboard-toolbar';
+
+export const RESOURCE_EXPLORER_FRAME = '.collapsible-panel-left';
+export const RESOURCE_EXPLORER_ICON = '[data-testid="collapsed-left-panel-icon"]';
+export const MODELED_TAB = '[data-testid="explore-modeled-tab"]';
+export const UNMODELED_TAB = '[data-testid="explore-unmodeled-tab"]';
+export const ASSET_MODEL_TAB = '[data-testid="explore-asset-model-tab"]';
+
+export const WIDGET_EMPTY_STATE_TEXT = 'Browse and select to add your asset properties in your line widget.';
---
+++ packages/dashboard/e2e/tests/test.ts
@@ -0,0 +1,18 @@
+import { test as base, expect } from '@playwright/test';
+import { rest } from 'msw';
+import type { MockServiceWorker, Config } from 'playwright-msw';
+import { createWorkerFixture } from 'playwright-msw';
+import { handlers } from '../../src/msw/handlers';
+
+const testFactory = (config?: Config) =>
+  base.extend<{
+    worker: MockServiceWorker;
+    rest: typeof rest;
+  }>({
+    worker: createWorkerFixture(handlers, config),
+    rest,
+  });
+
+const test = testFactory();
+
+export { testFactory, test, expect };
