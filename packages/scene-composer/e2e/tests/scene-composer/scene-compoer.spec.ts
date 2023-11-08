1dc9936ab31b2ecd76e510bcb4348a747b0a8d21
Emily Dodds <dodemily@amazon.com>
2023-11-08 13:28:05 -0600

    breaking because of useState shenanigans

--- packages/scene-composer/e2e/tests/scene-composer/scene-compoer.spec.ts
+++ packages/scene-composer/e2e/tests/scene-composer/scene-compoer.spec.ts
@@ -1,9 +1,85 @@
-import { test, expect } from '@playwright/test';
+// import { test, expect } from '@playwright/test';
+import { test, expect } from '@playwright/experimental-ct-react';
+import R3FTestHarness from '../utils/testUtils';
+import { useStore } from '../../../src/store';
+// const { useStore } = require('../../../src/store/Store');
 
-const TEST_PAGE = '/?path=/story/developer-scene-composer--local-scene';
+const TEST_IFRAME = '#root';
+const LOCAL_SCENE = '/iframe.html?args=&id=developer-scene-composer--local-scene';
+const CANVAS = '#tm-scene-unselectable-canvas';
 
-test('dashboard', async ({ page }) => {
-  const scenePage = await page.goto(TEST_PAGE);
-
-  await expect(scenePage).toBeTruthy()
+test('load scene', async ({ page }) => {
+  const scenePage = await page.goto(LOCAL_SCENE);
+  expect(scenePage).toBeDefined()
 });
+
+test('visual regression', async({page}) => {
+  await page.goto(LOCAL_SCENE);
+  const frame = page.locator(TEST_IFRAME);
+  expect(await frame.locator(CANVAS).screenshot()).toMatchSnapshot('local-scene-canvas.png'); 
+}); 
+
+test.describe('scene composer', () => {
+  test.beforeEach(async ({ page }) => {
+    await page.goto(LOCAL_SCENE);
+    const testStore = useStore('test1'); // current problem
+    console.log('something: ', testStore)
+  });
+
+  // test('get scene', async({page}) => {
+  //   // const scene = useStore('10'); // fails tests
+  //   // await expect(scene).toBeDefined() 
+  // }); 
+
+// test('get object', async({page}) => {
+
+// }); 
+
+// test('delete object', async({page}) => {
+
+// }); 
+
+// test('scale object', async({page}) => {
+
+// }); 
+
+// test('rotate object', async({page}) => {
+
+// }); 
+
+// test('move object', async({page}) => {
+
+// }); 
+
+// test('move object', async({page}) => {
+
+// }); 
+
+// test('select object', async({page}) => {
+
+// }); 
+
+// test('click object', async({page}) => {
+
+// }); 
+
+// test('add tag', async({page}) => {
+
+// }); 
+
+// test('place object', async({page}) => {
+
+// }); 
+
+// test('undo edit', async({page}) => {
+
+// }); 
+
+// test('redo edit', async({page}) => {
+
+// }); 
+
+  
+});
+
+
---
+++ packages/scene-composer/e2e/tests/utils/testUtils.ts
@@ -0,0 +1,134 @@
+import { useThree } from "@react-three/fiber";
+import { sceneComposerIdContext } from "../../../src/common/sceneComposerIdContext";
+import { Object3D, Vector3 } from "three";
+
+export default class R3FTestHarness {
+  sceneComposerId;
+
+  constructor(id) {
+    this.sceneComposerId = id;
+  }
+
+  // TODO: fill in function typescript helpers
+
+  // Get Scene
+  // get getScene() {
+  //   return this.testScene;
+  // }
+
+  // Get Object
+  /*
+  * Returns scene object
+  * `name` as string 
+  */
+  // getObjectByName(name: string) {
+  //   return this.testScene.getObjectByName(name)
+  // }
+
+  // Look At Object
+  /* 
+  
+  
+  */
+  // lookAtRef(ref: string) {
+  //   const setCameraTarget = useStore(this.sceneComposerId).getState().setCameraTarget;
+  //   setCameraTarget(ref, 'teleport');
+  // };
+
+  // Delete Object
+  /* 
+    * Remove object from scene
+  */
+//   deleteObject(obj: Object3D) {
+//     // return this.testScene.remove(obj)
+// };
+
+  // Scale Object
+  /* 
+    * Modify scale of object on x, y, z props
+    * Takes `scale` prop as Vector3 object  
+  */
+  //   scaleObject(scale: THREE.Vector3) {
+  //     console.log('implement scaleObject')
+  //     return this.testScene.scale()
+  // };
+
+  // Rotate Object
+  /* 
+  
+  
+  */
+//   rotateObjec(ref: string) {
+//     console.log('implement rotateObjec')
+// };
+
+  // Move Object
+  /* 
+  
+  
+  */
+//   moveObject(ref: string) {
+//     console.log('implement moveObject')
+// };
+
+// Select / Click Object
+  /* 
+  
+  
+  */
+//   selectObject(ref: string) {
+//     console.log('implement selectObject')
+// };
+
+  // Add Tag
+  /* 
+  
+  
+  */
+//   addTag(ref: string) {
+//     console.log('implement addTag')
+// };
+
+  // Place Object
+  /* 
+  
+  
+  */
+//   placeObject(ref: string) {
+//     console.log('implement placeObject')
+// };
+
+// Undo Edit
+  /* 
+  
+  
+  */
+//   undoEdit(ref: string) {
+//     console.log('implement undoEdit')
+// };
+
+// Redo Edit
+  /* 
+  
+  
+  */
+//   redoEdit(ref: string) {
+//     console.log('implement redoEdit')
+// };
+
+  // window['TMGetObject'] = (typeof window === 'undefined') ? undefined : getObjectByName;
+  // window['TMlookAt'] = (typeof window === 'undefined') ? undefined : lookAtRef;
+  // window['TwinMakerScene'] = (typeof window === 'undefined') ? undefined : scene;
+
+}
+
+/*
+
+jackwindow[0].TMGetObject("PalletJ )
+part1window[0].TMGetObject("Scene49 )
+
+window[0].TMlookAt(jack.userData.componentRef)
+
+window[0].TwinMakerScene
+
+*/
