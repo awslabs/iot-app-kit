ec11c82c1b2932a5f7f28d9394f469cac6d68f97
Luciana Corteggiano <luciana.corteggiano@gmail.com>
2023-11-10 12:24:10 -0700

    fix: ungate CSV download feature

--- packages/react-components/src/store/config.ts
+++ packages/react-components/src/store/config.ts
@@ -1,13 +1,11 @@
 import { StateCreator } from 'zustand/esm';
 
-export type Flags = 'useEcharts' | 'useCSVDownload' | 'useModelBasedQuery';
+export type Flags = 'useModelBasedQuery';
 export interface ConfigState {
   config: Record<Flags, boolean>;
 }
 export const createConfigSlice: StateCreator<ConfigState> = () => ({
   config: {
-    useEcharts: !!localStorage?.getItem('USE_ECHARTS'),
-    useCSVDownload: !!localStorage?.getItem('USE_CSV_DOWNLOAD'),
     useModelBasedQuery: !!localStorage?.getItem('USE_MODEL_BASED_QUERY'),
   },
 });
