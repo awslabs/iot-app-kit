3a879ecc8fcf2269daa5fed088354de03900a99a
Chandru_HM <chandrashekhara.n@happiestminds.com>
2023-11-17 11:15:49 +0530

    fix: panel resizing issue fix #2256

--- packages/dashboard/src/components/resizablePanes/constants.ts
+++ packages/dashboard/src/components/resizablePanes/constants.ts
@@ -1,7 +1,5 @@
 export const LEFT_WIDTH_PERCENT = 0.2;
 
-export const RIGHT_WIDTH_PERCENT = 0.2;
-
 export const MINIMUM_CENTER_PANE_WIDTH = 100;
 
 export const MINIMUM_SIDE_PANE_WIDTH = 100;
@@ -17,5 +15,3 @@ export const DEFAULT_COLLAPSED_SIDE_PANE_WIDTH = 80;
 export const MAXIMUM_PANES_PROPORTION = 0.8;
 
 export const LEFT_WIDTH_PERCENT_STORAGE_KEY = 'iot-dashboard-pane-left-width';
-
-export const RIGHT_WIDTH_PERCENT_STORAGE_KEY = 'iot-dashboard-pane-right-width';
