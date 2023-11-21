f9d269824d7ddaf6fe2f328cb00ad8b3fbe82fd2
Tracy French <tracfren@amazon.com>
2023-11-21 09:44:17 -0700

    fix: remove widget panel from text widget

--- packages/dashboard/src/customization/widgets/text/plugin.tsx
+++ packages/dashboard/src/customization/widgets/text/plugin.tsx
@@ -3,17 +3,12 @@ import TextWidgetComponent from './component';
 import TextIcon from './icon';
 import type { DashboardPlugin } from '~/customization/api';
 import type { TextWidget } from '../types';
-import WidgetTile from '~/components/widgets/tile/tile';
 import { TEXT_WIDGET_INITIAL_HEIGHT, TEXT_WIDGET_INITIAL_WIDTH } from '../constants';
 
 export const textPlugin: DashboardPlugin = {
   install: ({ registerWidget }) => {
     registerWidget<TextWidget>('text', {
-      render: (widget) => (
-        <WidgetTile widget={widget} removeable>
-          <TextWidgetComponent {...widget} />
-        </WidgetTile>
-      ),
+      render: (widget) => <TextWidgetComponent {...widget} />,
       componentLibrary: {
         name: 'Text',
         icon: TextIcon,
