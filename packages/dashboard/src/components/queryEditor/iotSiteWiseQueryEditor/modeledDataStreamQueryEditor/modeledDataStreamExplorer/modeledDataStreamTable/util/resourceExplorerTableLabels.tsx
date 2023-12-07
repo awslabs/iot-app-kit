931db41a1363ebc20d4b9859a4dab4dd058881bb
Chandru_HM <chandrashekhara.n@happiestminds.com>
2023-12-07 15:41:52 +0530

    feat: xy-plot & bar-chart allow only numerical datatypes #1952

---
+++ packages/dashboard/src/components/queryEditor/iotSiteWiseQueryEditor/modeledDataStreamQueryEditor/modeledDataStreamExplorer/modeledDataStreamTable/util/resourceExplorerTableLabels.tsx
@@ -0,0 +1,13 @@
+import { ModeledDataStream } from '../../types';
+
+export const isInValidProperty = (dataType: ModeledDataStream['dataType'], widgetType?: string) => {
+  if (!widgetType) return false;
+
+  const isNumericDataType = dataType === 'DOUBLE' || dataType === 'INTEGER';
+
+  if (widgetType === 'xy-plot' || widgetType === 'bar-chart') {
+    return !isNumericDataType;
+  }
+
+  return false;
+};
