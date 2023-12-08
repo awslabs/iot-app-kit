dde04116758ede97fc70020e35042c7aa34fb026
Chandru_HM <chandrashekhara.n@happiestminds.com>
2023-12-08 13:24:09 +0530

    fix: dashboard settings to set correct columns and rows #2313

--- packages/dashboard/src/store/actions/changeDashboardGrid/changeHeight.spec.ts
+++ packages/dashboard/src/store/actions/changeDashboardGrid/changeHeight.spec.ts
@@ -1,5 +1,6 @@
 import { changeDashboardHeight, onChangeDashboardHeightAction } from './changeHeight';
 import { initialState } from '../../state';
+import { MOCK_LINE_CHART_WIDGET } from '../../../../testing/mocks';
 
 it('can change the height of the dashboard', () => {
   expect(
@@ -10,6 +11,32 @@ it('can change the height of the dashboard', () => {
       })
     ).grid.height
   ).toEqual(10);
+});
+
+it('can update the widget selection with changed height of the dashboard', () => {
+  const store = {
+    ...initialState,
+    dashboardConfiguration: {
+      ...initialState.dashboardConfiguration,
+      widgets: [{ ...MOCK_LINE_CHART_WIDGET }],
+    },
+    selectedWidgets: [{ ...MOCK_LINE_CHART_WIDGET }],
+  };
+  const updatedStore = changeDashboardHeight(
+    store,
+    onChangeDashboardHeightAction({
+      height: 10,
+    })
+  );
+  expect(updatedStore.selectedWidgets[0].height).toEqual(10);
+  expect(
+    changeDashboardHeight(
+      updatedStore,
+      onChangeDashboardHeightAction({
+        height: 100,
+      })
+    ).selectedWidgets[0].height
+  ).toEqual(20);
 });
 
 it('does not allow negative heights', () => {
--- packages/dashboard/src/store/actions/changeDashboardGrid/updateGrid.ts
+++ packages/dashboard/src/store/actions/changeDashboardGrid/updateGrid.ts
@@ -1,5 +1,7 @@
 import { constrainWidgetPositionToGrid } from '~/util/constrainWidgetPositionToGrid';
 import type { DashboardState } from '../../state';
+import { WidgetPropertiesGeneratorMap } from '~/customization/widgetPropertiesGeneratorMap';
+import { WIDGET_INITIAL_HEIGHT, WIDGET_INITIAL_WIDTH } from '~/customization/widgets/constants';
 
 type GridProperties = keyof DashboardState['grid'];
 type GridValues = DashboardState['grid'][GridProperties];
@@ -14,7 +16,23 @@ export const changeGridProperty = (
     [property]: value,
   };
   const widgets = state.dashboardConfiguration.widgets.map((w) => {
-    return constrainWidgetPositionToGrid({ x: 0, y: 0, width: grid.width, height: grid.height }, w);
+    const { initialSize } = WidgetPropertiesGeneratorMap[w.type] || {};
+
+    const { width: widgetPixelWidth, height: widgetPixelHeight } = initialSize || {
+      height: WIDGET_INITIAL_HEIGHT,
+      width: WIDGET_INITIAL_WIDTH,
+    };
+    const width = Math.min(Math.ceil(widgetPixelWidth / state.grid.cellSize), grid.width);
+    const height = Math.min(Math.ceil(widgetPixelHeight / state.grid.cellSize), grid.height);
+
+    return constrainWidgetPositionToGrid(
+      { x: 0, y: 0, width: grid.width, height: grid.height },
+      { ...w, ...(property !== 'cellSize' && { width: width, height: height }) }
+    );
+  });
+
+  const updatedSelectedWidgets = state.selectedWidgets.map((w) => {
+    return widgets.find((k) => k.id === w.id) || w;
   });
 
   return {
@@ -24,5 +42,6 @@ export const changeGridProperty = (
       widgets,
     },
     grid,
+    selectedWidgets: [...updatedSelectedWidgets],
   };
 };
