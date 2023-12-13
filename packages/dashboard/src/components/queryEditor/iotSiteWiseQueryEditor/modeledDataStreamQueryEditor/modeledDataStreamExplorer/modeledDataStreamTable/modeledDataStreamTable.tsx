7cc95769d7fea353cd53dea8d732d1e7991d5105
Brian Diehr <diehbria@amazon.com>
2023-12-13 13:47:50 -0800

    Revert "feat(dashboard): add metrics for query editor usage"
    This reverts commit 6dc3cc416d2d86c9d8e1d85145b0c9083fd3f260.

--- packages/dashboard/src/components/queryEditor/iotSiteWiseQueryEditor/modeledDataStreamQueryEditor/modeledDataStreamExplorer/modeledDataStreamTable/modeledDataStreamTable.tsx
+++ packages/dashboard/src/components/queryEditor/iotSiteWiseQueryEditor/modeledDataStreamQueryEditor/modeledDataStreamExplorer/modeledDataStreamTable/modeledDataStreamTable.tsx
@@ -21,7 +21,6 @@ import { DashboardState } from '~/store/state';
 import { ResourceExplorerFooter } from '../../../footer/footer';
 import { SelectedAsset } from '../../types';
 import { ResourceExplorerErrorState } from '../../components/resourceExplorerErrorState';
-import { getPlugin } from '@iot-app-kit/core';
 import { isInValidProperty } from './util/resourceExplorerTableLabels';
 
 export interface ModeledDataStreamTableProps {
@@ -47,7 +46,6 @@ export function ModeledDataStreamTable({
   hasNextPage,
   modeledDataStreamsTitle,
 }: ModeledDataStreamTableProps) {
-  const metricsRecorder = getPlugin('metricsRecorder');
   const significantDigits = useSelector((state: DashboardState) => state.significantDigits);
   const selectedWidgets = useSelector((state: DashboardState) => state.selectedWidgets);
 
@@ -161,10 +159,6 @@ export function ModeledDataStreamTable({
           addDisabled={collectionProps.selectedItems?.length === 0 || selectedWidgets.length !== 1}
           onAdd={() => {
             onClickAddModeledDataStreams(collectionProps.selectedItems as unknown as ModeledDataStream[]);
-            metricsRecorder?.record({
-              metricName: 'ModeledDataStreamAdd',
-              metricValue: 1,
-            });
           }}
         />
       }
