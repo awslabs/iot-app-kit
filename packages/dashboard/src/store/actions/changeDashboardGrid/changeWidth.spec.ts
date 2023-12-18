d23e9dd9fd893820a4a190188fb206400a4cc235
Nischay Maridevegowda <mnischay@amazon.com>
2023-12-18 12:44:06 -0500

    Revert "fix: dashboard settings to set correct columns and rows #2313"
    This reverts commit 5eb72e7f92b07e6678227d332031642baa8332ff.

--- packages/dashboard/src/store/actions/changeDashboardGrid/changeWidth.spec.ts
+++ packages/dashboard/src/store/actions/changeDashboardGrid/changeWidth.spec.ts
@@ -1,6 +1,5 @@
 import { changeDashboardWidth, onChangeDashboardWidthAction } from './changeWidth';
 import { initialState } from '../../state';
-import { MOCK_LINE_CHART_WIDGET } from '../../../../testing/mocks';
 
 it('can change the width of the dashboard', () => {
   expect(
@@ -11,32 +10,6 @@ it('can change the width of the dashboard', () => {
       })
     ).grid.width
   ).toEqual(10);
-});
-
-it('can update the widget selection with changed width of the dashboard', () => {
-  const store = {
-    ...initialState,
-    dashboardConfiguration: {
-      ...initialState.dashboardConfiguration,
-      widgets: [{ ...MOCK_LINE_CHART_WIDGET }],
-    },
-    selectedWidgets: [{ ...MOCK_LINE_CHART_WIDGET }],
-  };
-  const updatedStore = changeDashboardWidth(
-    store,
-    onChangeDashboardWidthAction({
-      width: 10,
-    })
-  );
-  expect(updatedStore.selectedWidgets[0].width).toEqual(10);
-  expect(
-    changeDashboardWidth(
-      updatedStore,
-      onChangeDashboardWidthAction({
-        width: 100,
-      })
-    ).selectedWidgets[0].width
-  ).toEqual(33);
 });
 
 it('does not allow negative widths', () => {
