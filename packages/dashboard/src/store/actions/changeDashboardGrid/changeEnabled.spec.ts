43a24350b275568315f39600e24a2fda325d2e80
Chandru_HM <chandrashekhara.n@happiestminds.com>
2023-12-19 11:53:35 +0530

    fix: dashboard settings to set correct columns and rows #2313

--- packages/dashboard/src/store/actions/changeDashboardGrid/changeEnabled.spec.ts
+++ packages/dashboard/src/store/actions/changeDashboardGrid/changeEnabled.spec.ts
@@ -1,22 +1,28 @@
 import { initialState } from '../../state';
 import { changeDashboardGridDragEnabled, onChangeDashboardGridEnabledAction } from './changeEnabled';
+import { MOCK_LINE_CHART_WIDGET } from '../../../../testing/mocks';
 
 it('can change grid enabled', () => {
-  expect(
-    changeDashboardGridDragEnabled(
-      initialState,
-      onChangeDashboardGridEnabledAction({
-        enabled: false,
-      })
-    ).grid.enabled
-  ).toEqual(false);
+  const store = {
+    ...initialState,
+    dashboardConfiguration: {
+      ...initialState.dashboardConfiguration,
+      widgets: [{ ...MOCK_LINE_CHART_WIDGET }],
+    },
+    selectedWidgets: [{ ...MOCK_LINE_CHART_WIDGET }],
+  };
 
-  expect(
-    changeDashboardGridDragEnabled(
-      initialState,
+  const gridEnabled = [false, true];
+
+  gridEnabled.forEach((enabled) => {
+    const updatedStore = changeDashboardGridDragEnabled(
+      store,
       onChangeDashboardGridEnabledAction({
-        enabled: true,
+        enabled: enabled,
       })
-    ).grid.enabled
-  ).toEqual(true);
+    );
+    expect(updatedStore.grid.enabled).toEqual(enabled);
+    expect(updatedStore.selectedWidgets[0].height).toEqual(MOCK_LINE_CHART_WIDGET.height);
+    expect(updatedStore.selectedWidgets[0].width).toEqual(MOCK_LINE_CHART_WIDGET.width);
+  });
 });
