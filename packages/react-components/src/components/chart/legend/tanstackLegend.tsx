f3210b5220ccfd29d8cdb95ec085fb5a8feb1701
Chandru_HM <chandrashekhara.n@happiestminds.com>
2023-12-20 20:42:14 +0530

    feat: legend table is implemeted using tanstack table

---
+++ packages/react-components/src/components/chart/legend/tanstackLegend.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { DataStream } from '@iot-app-kit/core';
+import { ColumnDef } from '@tanstack/react-table';
+
+import { SeriesOption } from 'echarts';
+import useChartsLegendTanstack from './useChartsLegendTanstack';
+
+import { InternalGraphicComponentGroupOption } from '../trendCursor/types';
+import { TanstackTable } from './tanstackTable';
+
+type legendOptions = {
+  datastreams: DataStream[];
+  series: SeriesOption[];
+  graphic: InternalGraphicComponentGroupOption[];
+  width: number;
+};
+
+const TanstackLegend = (legendOptions: legendOptions) => {
+  const { items: allItemsTanstack, columnDefinitions: columnDefinitionsTanstack } =
+    useChartsLegendTanstack(legendOptions);
+
+  return (
+    <TanstackTable
+      data={allItemsTanstack}
+      columnDefinitions={columnDefinitionsTanstack as ColumnDef<object, string>[]}
+      stickyColumns={{ first: 1 }}
+      stickyHeader
+    />
+  );
+};
+
+export default TanstackLegend;
