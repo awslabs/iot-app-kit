010343112ba7ef7fbf6ad0416e2525a84d3d7e88
Nischay Maridevegowda <mnischay@amazon.com>
2023-12-21 13:32:13 -0500

    chore(testing-util): adding data mock utilities for doc-site

--- packages/testing-util/src/index.ts
+++ packages/testing-util/src/index.ts
@@ -1,3 +1,11 @@
-export { mockTimeSeriesDataQuery } from './mockTimeSeriesDataQuery';
+export {
+  mockTimeSeriesDataQuery,
+  mockTimeSeriesDataQueryLoading,
+  mockTimeSeriesDataQueryWithError,
+} from './data/mockTimeSeriesDataQuery';
+export {
+  mockTimeSeriesDataQueryLiveStream,
+  mockTimeSeriesDataQueryLiveStreamAggregated,
+} from './data/mockTimeSeriesDataRealTime';
 export { createMockSiteWiseSDK } from './sdks/mockIoTSiteWise';
 export { createMockIoTEventsSDK } from './sdks/mockIoTEvents';
