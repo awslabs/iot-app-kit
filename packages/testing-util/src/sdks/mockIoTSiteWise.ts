b165679ce6d73c5d7caaa755f62feebfc80f0dd1
Nischay Maridevegowda <mnischay@amazon.com>
2023-12-21 13:32:13 -0500

    chore(testing-util): adding data mock utilities for doc-site

--- packages/testing-util/src/sdks/mockIoTSiteWise.ts
+++ packages/testing-util/src/sdks/mockIoTSiteWise.ts
@@ -1,7 +1,32 @@
-import {
-  IoTSiteWiseClient
+import { IoTSiteWiseClient } from '@aws-sdk/client-iotsitewise';
+import type {
+  DescribeAssetCommandInput,
+  DescribeAssetModelCommandInput,
+  DescribeAssetModelResponse,
+  DescribeAssetResponse,
+  GetAssetPropertyAggregatesCommandInput,
+  GetAssetPropertyAggregatesResponse,
+  GetAssetPropertyValueCommandInput,
+  GetAssetPropertyValueHistoryCommandInput,
+  GetAssetPropertyValueHistoryResponse,
+  GetAssetPropertyValueResponse,
+  GetInterpolatedAssetPropertyValuesCommandInput,
+  GetInterpolatedAssetPropertyValuesResponse,
+  BatchGetAssetPropertyValueHistoryCommandInput,
+  BatchGetAssetPropertyValueHistoryResponse,
+  BatchGetAssetPropertyAggregatesCommandInput,
+  BatchGetAssetPropertyAggregatesResponse,
+  BatchGetAssetPropertyValueCommandInput,
+  BatchGetAssetPropertyValueResponse,
+  ListAssetsCommandInput,
+  ListAssociatedAssetsCommandInput,
+  ListAssociatedAssetsResponse,
+  ListAssetsResponse,
+  ListAssetPropertiesCommandInput,
+  ListAssetPropertiesResponse,
+  ListAssetModelPropertiesCommandInput,
+  ListAssetModelPropertiesResponse,
 } from '@aws-sdk/client-iotsitewise';
-import type { DescribeAssetCommandInput, DescribeAssetModelCommandInput, DescribeAssetModelResponse, DescribeAssetResponse, GetAssetPropertyAggregatesCommandInput, GetAssetPropertyAggregatesResponse, GetAssetPropertyValueCommandInput, GetAssetPropertyValueHistoryCommandInput, GetAssetPropertyValueHistoryResponse, GetAssetPropertyValueResponse, GetInterpolatedAssetPropertyValuesCommandInput, GetInterpolatedAssetPropertyValuesResponse, BatchGetAssetPropertyValueHistoryCommandInput, BatchGetAssetPropertyValueHistoryResponse, BatchGetAssetPropertyAggregatesCommandInput, BatchGetAssetPropertyAggregatesResponse, BatchGetAssetPropertyValueCommandInput, BatchGetAssetPropertyValueResponse, ListAssetsCommandInput, ListAssociatedAssetsCommandInput, ListAssociatedAssetsResponse, ListAssetsResponse, ListAssetPropertiesCommandInput, ListAssetPropertiesResponse, ListAssetModelPropertiesCommandInput, ListAssetModelPropertiesResponse } from "@aws-sdk/client-iotsitewise";
 
 const nonOverriddenMock = () => Promise.reject(new Error('Mock method not override.'));
 
@@ -46,43 +71,43 @@ export const createMockSiteWiseSDK = ({
     input: BatchGetAssetPropertyValueCommandInput
   ) => Promise<BatchGetAssetPropertyValueResponse>;
 } = {}) =>
-({
-  send: (command: { input: any }) => {
-    // Mocks out the process of a sending a command within the JS AWS-SDK v3, learn more at
-    // https://docs.aws.amazon.com/AWSJavaScriptSDK/v3/latest/index.html#high-level-concepts
-    const commandName = command.constructor.name;
+  ({
+    send: (command: { input: never }) => {
+      // Mocks out the process of a sending a command within the JS AWS-SDK v3, learn more at
+      // https://docs.aws.amazon.com/AWSJavaScriptSDK/v3/latest/index.html#high-level-concepts
+      const commandName = command.constructor.name;
 
-    switch (commandName) {
-      case 'ListAssetsCommand':
-        return listAssets(command.input);
-      case 'ListAssetPropertiesCommand':
-        return listAssetProperties(command.input);
-      case 'ListAssetModelPropertiesCommand':
-        return listAssetModelProperties(command.input);
-      case 'ListAssociatedAssetsCommand':
-        return listAssociatedAssets(command.input);
-      case 'DescribeAssetCommand':
-        return describeAsset(command.input);
-      case 'DescribeAssetModelCommand':
-        return describeAssetModel(command.input);
-      case 'GetAssetPropertyValueCommand':
-        return getAssetPropertyValue(command.input);
-      case 'GetAssetPropertyAggregatesCommand':
-        return getAssetPropertyAggregates(command.input);
-      case 'GetAssetPropertyValueHistoryCommand':
-        return getAssetPropertyValueHistory(command.input);
-      case 'GetInterpolatedAssetPropertyValuesCommand':
-        return getInterpolatedAssetPropertyValues(command.input);
-      case 'BatchGetAssetPropertyValueHistoryCommand':
-        return batchGetAssetPropertyValueHistory(command.input);
-      case 'BatchGetAssetPropertyAggregatesCommand':
-        return batchGetAssetPropertyAggregates(command.input);
-      case 'BatchGetAssetPropertyValueCommand':
-        return batchGetAssetPropertyValue(command.input);
-      default:
-        throw new Error(
-          `missing mock implementation for command name ${commandName}. Add a new command within the mock SiteWise SDK.`
-        );
-    }
-  },
-} as unknown as IoTSiteWiseClient);
+      switch (commandName) {
+        case 'ListAssetsCommand':
+          return listAssets(command.input);
+        case 'ListAssetPropertiesCommand':
+          return listAssetProperties(command.input);
+        case 'ListAssetModelPropertiesCommand':
+          return listAssetModelProperties(command.input);
+        case 'ListAssociatedAssetsCommand':
+          return listAssociatedAssets(command.input);
+        case 'DescribeAssetCommand':
+          return describeAsset(command.input);
+        case 'DescribeAssetModelCommand':
+          return describeAssetModel(command.input);
+        case 'GetAssetPropertyValueCommand':
+          return getAssetPropertyValue(command.input);
+        case 'GetAssetPropertyAggregatesCommand':
+          return getAssetPropertyAggregates(command.input);
+        case 'GetAssetPropertyValueHistoryCommand':
+          return getAssetPropertyValueHistory(command.input);
+        case 'GetInterpolatedAssetPropertyValuesCommand':
+          return getInterpolatedAssetPropertyValues(command.input);
+        case 'BatchGetAssetPropertyValueHistoryCommand':
+          return batchGetAssetPropertyValueHistory(command.input);
+        case 'BatchGetAssetPropertyAggregatesCommand':
+          return batchGetAssetPropertyAggregates(command.input);
+        case 'BatchGetAssetPropertyValueCommand':
+          return batchGetAssetPropertyValue(command.input);
+        default:
+          throw new Error(
+            `missing mock implementation for command name ${commandName}. Add a new command within the mock SiteWise SDK.`
+          );
+      }
+    },
+  } as unknown as IoTSiteWiseClient);
