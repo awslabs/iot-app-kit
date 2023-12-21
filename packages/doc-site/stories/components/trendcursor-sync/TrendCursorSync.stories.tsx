246560d2ccb6ec6804e56560526358eb691d9812
Nischay Maridevegowda <mnischay@amazon.com>
2023-12-21 17:32:32 -0500

    feat(doc-site): adding doc-site

---
+++ packages/doc-site/stories/components/trendcursor-sync/TrendCursorSync.stories.tsx
@@ -0,0 +1,48 @@
+import { Meta, StoryObj } from '@storybook/react';
+import { Chart, TimeSync, TrendCursorSync } from '@iot-app-kit/react-components';
+import { mockSinWaveData } from '../../mockSinWaveData';
+
+const meta: Meta<typeof TrendCursorSync> = {
+  title: 'Components/TrendCursorSync',
+  component: TrendCursorSync,
+};
+
+export default meta;
+
+type Story = StoryObj<typeof TrendCursorSync>;
+
+export const Standard: Story = {
+  render: (props) => (
+    <div style={{ height: '500px', width: '800px' }}>
+      <TimeSync>
+        <TrendCursorSync {...props}>
+          <div style={{ display: 'flex', flexDirection: 'row' }}>
+            <Chart
+              {...{
+                ...{
+                  queries: [mockSinWaveData()],
+                  viewport: { duration: '10s' },
+                },
+                legend: {},
+                size: { height: 500, width: 450 },
+              }}
+            />
+            <Chart
+              {...{
+                ...{
+                  queries: [mockSinWaveData()],
+                  viewport: { duration: '10s' },
+                },
+                legend: {},
+                size: { height: 500, width: 450 },
+              }}
+            />
+          </div>
+        </TrendCursorSync>
+      </TimeSync>
+    </div>
+  ),
+  args: {
+    groupId: 'test',
+  },
+};
