7aebaa6501b8806646dcc4e1cde732d5f77f1143
Tracy French <tracfren@amazon.com>
2024-01-02 16:01:06 -0700

    perf: increase batch entry and result size

--- packages/source-iotsitewise/src/time-series-data/client/batch.spec.ts
+++ packages/source-iotsitewise/src/time-series-data/client/batch.spec.ts
@@ -1,15 +1,127 @@
 import {
-  createEntryBatches,
-  calculateNextBatchSize,
+  createRawLatestEntryBatches,
+  createRawHistoricalEntryBatches,
+  createAggregateEntryBatches,
+  calculateNextRawHistoricalBatchSize,
+  calculateNextAggregatedBatchSize,
   shouldFetchNextBatch,
-  MAX_BATCH_RESULTS,
-  MAX_BATCH_ENTRIES,
   NO_LIMIT_BATCH,
 } from './batch';
+import {
+  MAX_AGGREGATED_DATA_POINTS,
+  MAX_RAW_HISTORICAL_DATA_POINTS,
+  MAX_RAW_LATEST_REQUEST_ENTRIES,
+  MAX_RAW_HISTORICAL_REQUEST_ENTRIES,
+  MAX_AGGREGATED_REQUEST_ENTRIES,
+} from './constants';
 
-describe('createEntryBatches', () => {
+describe('createRawLatestEntryBatches', () => {
   it('buckets entries by maxResults for a given batch', () => {
-    const batches = createEntryBatches([
+    const batches = createRawLatestEntryBatches([
+      {
+        id: '1',
+        maxResults: undefined,
+      },
+      {
+        id: '2',
+        maxResults: 2000,
+      },
+      {
+        id: '3',
+        maxResults: 2000,
+      },
+      {
+        id: '4',
+        maxResults: 10,
+      },
+      {
+        id: '5',
+        maxResults: 2000,
+      },
+    ]);
+
+    expect(batches).toEqual(
+      expect.arrayContaining([
+        [
+          [
+            {
+              id: '1',
+              maxResults: undefined,
+            },
+          ],
+          NO_LIMIT_BATCH,
+        ],
+        [
+          [
+            {
+              id: '2',
+              maxResults: 2000,
+            },
+            {
+              id: '3',
+              maxResults: 2000,
+            },
+            {
+              id: '5',
+              maxResults: 2000,
+            },
+          ],
+          6000,
+        ],
+        [
+          [
+            {
+              id: '4',
+              maxResults: 10,
+            },
+          ],
+          10,
+        ],
+      ])
+    );
+  });
+
+  it('chunks batches that exceed max entry size (128)', () => {
+    const entrySize = 2000;
+
+    const entries = [
+      ...[...Array(MAX_RAW_LATEST_REQUEST_ENTRIES * 3)].map((_args, index) => ({
+        id: String(index),
+        maxResults: entrySize,
+      })),
+      {
+        id: 'abc',
+        maxResults: 10,
+      },
+    ];
+
+    const batches = createRawLatestEntryBatches(entries);
+
+    expect(batches).toEqual(
+      expect.arrayContaining([
+        [entries.slice(0, MAX_RAW_LATEST_REQUEST_ENTRIES), MAX_RAW_LATEST_REQUEST_ENTRIES * entrySize],
+        [
+          entries.slice(MAX_RAW_LATEST_REQUEST_ENTRIES, 2 * MAX_RAW_LATEST_REQUEST_ENTRIES),
+          MAX_RAW_LATEST_REQUEST_ENTRIES * entrySize,
+        ],
+        [
+          entries.slice(MAX_RAW_LATEST_REQUEST_ENTRIES * 2, MAX_RAW_LATEST_REQUEST_ENTRIES * 3),
+          MAX_RAW_LATEST_REQUEST_ENTRIES * entrySize,
+        ],
+        [[entries[MAX_RAW_LATEST_REQUEST_ENTRIES * 3]], 10],
+      ])
+    );
+  });
+
+  it('handles empty input', () => {
+    const batches = createRawLatestEntryBatches([]);
+    expect(batches).toEqual([]);
+  });
+});
+
+describe('createRawHistoricalEntryBatches', () => {
+  it('buckets entries by maxResults for a given batch', () => {
+    const batches = createRawHistoricalEntryBatches([
       {
         id: '1',
         maxResults: undefined,
@@ -77,7 +189,7 @@ describe('createEntryBatches', () => {
     const entrySize = 2000;
 
     const entries = [
-      ...[...Array(MAX_BATCH_ENTRIES * 3)].map((_args, index) => ({
+      ...[...Array(MAX_RAW_HISTORICAL_REQUEST_ENTRIES * 3)].map((_args, index) => ({
         id: String(index),
         maxResults: entrySize,
       })),
@@ -87,41 +199,189 @@ describe('createEntryBatches', () => {
       },
     ];
 
-    const batches = createEntryBatches(entries);
+    const batches = createRawHistoricalEntryBatches(entries);
 
     expect(batches).toEqual(
       expect.arrayContaining([
-        [entries.slice(0, MAX_BATCH_ENTRIES), MAX_BATCH_ENTRIES * entrySize],
-        [entries.slice(MAX_BATCH_ENTRIES, 2 * MAX_BATCH_ENTRIES), MAX_BATCH_ENTRIES * entrySize],
-        [entries.slice(MAX_BATCH_ENTRIES * 2, MAX_BATCH_ENTRIES * 3), MAX_BATCH_ENTRIES * entrySize],
-        [[entries[MAX_BATCH_ENTRIES * 3]], 10],
+        [entries.slice(0, MAX_RAW_HISTORICAL_REQUEST_ENTRIES), MAX_RAW_HISTORICAL_REQUEST_ENTRIES * entrySize],
+        [
+          entries.slice(MAX_RAW_HISTORICAL_REQUEST_ENTRIES, 2 * MAX_RAW_HISTORICAL_REQUEST_ENTRIES),
+          MAX_RAW_HISTORICAL_REQUEST_ENTRIES * entrySize,
+        ],
+        [
+          entries.slice(MAX_RAW_HISTORICAL_REQUEST_ENTRIES * 2, MAX_RAW_HISTORICAL_REQUEST_ENTRIES * 3),
+          MAX_RAW_HISTORICAL_REQUEST_ENTRIES * entrySize,
+        ],
+        [[entries[MAX_RAW_HISTORICAL_REQUEST_ENTRIES * 3]], 10],
       ])
     );
   });
 
   it('handles empty input', () => {
-    const batches = createEntryBatches([]);
+    const batches = createRawHistoricalEntryBatches([]);
     expect(batches).toEqual([]);
   });
 });
 
-describe('calculateNextBatchSize', () => {
+describe('createAggregateEntryBatches', () => {
+  it('buckets entries by maxResults for a given batch', () => {
+    const batches = createAggregateEntryBatches([
+      {
+        id: '1',
+        maxResults: undefined,
+      },
+      {
+        id: '2',
+        maxResults: 2000,
+      },
+      {
+        id: '3',
+        maxResults: 2000,
+      },
+      {
+        id: '4',
+        maxResults: 10,
+      },
+      {
+        id: '5',
+        maxResults: 2000,
+      },
+    ]);
+
+    expect(batches).toEqual(
+      expect.arrayContaining([
+        [
+          [
+            {
+              id: '1',
+              maxResults: undefined,
+            },
+          ],
+          NO_LIMIT_BATCH,
+        ],
+        [
+          [
+            {
+              id: '2',
+              maxResults: 2000,
+            },
+            {
+              id: '3',
+              maxResults: 2000,
+            },
+            {
+              id: '5',
+              maxResults: 2000,
+            },
+          ],
+          6000,
+        ],
+        [
+          [
+            {
+              id: '4',
+              maxResults: 10,
+            },
+          ],
+          10,
+        ],
+      ])
+    );
+  });
+
+  it('chunks batches that exceed max entry size (16)', () => {
+    const entrySize = 2000;
+
+    const entries = [
+      ...[...Array(MAX_AGGREGATED_REQUEST_ENTRIES * 3)].map((_args, index) => ({
+        id: String(index),
+        maxResults: entrySize,
+      })),
+      {
+        id: 'abc',
+        maxResults: 10,
+      },
+    ];
+
+    const batches = createAggregateEntryBatches(entries);
+
+    expect(batches).toEqual(
+      expect.arrayContaining([
+        [entries.slice(0, MAX_AGGREGATED_REQUEST_ENTRIES), MAX_AGGREGATED_REQUEST_ENTRIES * entrySize],
+        [
+          entries.slice(MAX_AGGREGATED_REQUEST_ENTRIES, 2 * MAX_AGGREGATED_REQUEST_ENTRIES),
+          MAX_AGGREGATED_REQUEST_ENTRIES * entrySize,
+        ],
+        [
+          entries.slice(MAX_AGGREGATED_REQUEST_ENTRIES * 2, MAX_AGGREGATED_REQUEST_ENTRIES * 3),
+          MAX_AGGREGATED_REQUEST_ENTRIES * entrySize,
+        ],
+        [[entries[MAX_AGGREGATED_REQUEST_ENTRIES * 3]], 10],
+      ])
+    );
+  });
+
+  it('handles empty input', () => {
+    const batches = createAggregateEntryBatches([]);
+    expect(batches).toEqual([]);
+  });
+});
+
+describe('calculateNextRawHistoricalBatchSize', () => {
   it('returns the correct max batch size for no limit batches', () => {
-    expect(calculateNextBatchSize({ maxResults: NO_LIMIT_BATCH, dataPointsFetched: 0 })).toBe(MAX_BATCH_RESULTS);
-    expect(calculateNextBatchSize({ maxResults: NO_LIMIT_BATCH, dataPointsFetched: 100000 })).toBe(MAX_BATCH_RESULTS);
+    expect(calculateNextRawHistoricalBatchSize({ maxResults: NO_LIMIT_BATCH, dataPointsFetched: 0 })).toBe(
+      MAX_RAW_HISTORICAL_DATA_POINTS
+    );
+    expect(calculateNextRawHistoricalBatchSize({ maxResults: NO_LIMIT_BATCH, dataPointsFetched: 100000 })).toBe(
+      MAX_RAW_HISTORICAL_DATA_POINTS
+    );
   });
 
-  it('returns the correct max batch size when specified and need to fetch more than MAX_BATCH_SIZE', () => {
-    expect(calculateNextBatchSize({ maxResults: MAX_BATCH_RESULTS * 3, dataPointsFetched: 0 })).toBe(MAX_BATCH_RESULTS);
+  it('returns the correct max batch size when specified and need to fetch more than MAX_RAW_HISTORICAL_DATA_POINTS', () => {
+    expect(
+      calculateNextRawHistoricalBatchSize({ maxResults: MAX_RAW_HISTORICAL_DATA_POINTS * 3, dataPointsFetched: 0 })
+    ).toBe(MAX_RAW_HISTORICAL_DATA_POINTS);
   });
 
-  it('returns the correct max batch size when specified and need to fetch less than MAX_BATCH SIZE', () => {
-    expect(calculateNextBatchSize({ maxResults: MAX_BATCH_RESULTS / 2, dataPointsFetched: 0 })).toBe(
-      MAX_BATCH_RESULTS / 2
+  it('returns the correct max batch size when specified and need to fetch less than MAX_RAW_HISTORICAL_DATA_POINTS', () => {
+    expect(
+      calculateNextRawHistoricalBatchSize({ maxResults: MAX_RAW_HISTORICAL_DATA_POINTS / 2, dataPointsFetched: 0 })
+    ).toBe(MAX_RAW_HISTORICAL_DATA_POINTS / 2);
+    expect(
+      calculateNextRawHistoricalBatchSize({
+        maxResults: MAX_RAW_HISTORICAL_DATA_POINTS,
+        dataPointsFetched: MAX_RAW_HISTORICAL_DATA_POINTS / 2,
+      })
+    ).toBe(MAX_RAW_HISTORICAL_DATA_POINTS / 2);
+  });
+});
+
+describe('calculateNextAggregatedBatchSize', () => {
+  it('returns the correct max batch size for no limit batches', () => {
+    expect(calculateNextAggregatedBatchSize({ maxResults: NO_LIMIT_BATCH, dataPointsFetched: 0 })).toBe(
+      MAX_AGGREGATED_DATA_POINTS
     );
-    expect(calculateNextBatchSize({ maxResults: MAX_BATCH_RESULTS, dataPointsFetched: MAX_BATCH_RESULTS / 2 })).toBe(
-      MAX_BATCH_RESULTS / 2
+    expect(calculateNextAggregatedBatchSize({ maxResults: NO_LIMIT_BATCH, dataPointsFetched: 100000 })).toBe(
+      MAX_AGGREGATED_DATA_POINTS
     );
+  });
+
+  it('returns the correct max batch size when specified and need to fetch more than MAX_AGGREGATED_DATA_POINTS', () => {
+    expect(calculateNextAggregatedBatchSize({ maxResults: MAX_AGGREGATED_DATA_POINTS * 3, dataPointsFetched: 0 })).toBe(
+      MAX_AGGREGATED_DATA_POINTS
+    );
+  });
+
+  it('returns the correct max batch size when specified and need to fetch less than MAX_AGGREGATED_DATA_POINTS', () => {
+    expect(calculateNextAggregatedBatchSize({ maxResults: MAX_AGGREGATED_DATA_POINTS / 2, dataPointsFetched: 0 })).toBe(
+      MAX_AGGREGATED_DATA_POINTS / 2
+    );
+    expect(
+      calculateNextAggregatedBatchSize({
+        maxResults: MAX_AGGREGATED_DATA_POINTS,
+        dataPointsFetched: MAX_AGGREGATED_DATA_POINTS / 2,
+      })
+    ).toBe(MAX_AGGREGATED_DATA_POINTS / 2);
   });
 });
 
