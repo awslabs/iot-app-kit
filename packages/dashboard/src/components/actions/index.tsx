8c2dc0300bf3af0ec2bf1809abc29d8892824105
Jason Buss <jmbuss@amazon.com>
2024-01-03 12:00:36 -0700

    fix: fix data-zoom behavior for base chart

--- packages/dashboard/src/components/actions/index.tsx
+++ packages/dashboard/src/components/actions/index.tsx
@@ -34,7 +34,7 @@ const Actions: React.FC<ActionsProps> = ({
   const [dashboardSettingsVisible, setDashboardSettingsVisible] = useState(false);
   const dispatch = useDispatch();
 
-  const { viewport, setViewport } = useViewport();
+  const { viewport } = useViewport();
 
   const metricsRecorder = getPlugin('metricsRecorder');
 
@@ -58,7 +58,6 @@ const Actions: React.FC<ActionsProps> = ({
   };
 
   const handleOnReadOnly = () => {
-    if (viewport) setViewport(viewport); // hack to ensure that the viewport lastupdatedby is always wiped between modes
     dispatch(onToggleReadOnly());
     dispatch(
       onSelectWidgetsAction({
