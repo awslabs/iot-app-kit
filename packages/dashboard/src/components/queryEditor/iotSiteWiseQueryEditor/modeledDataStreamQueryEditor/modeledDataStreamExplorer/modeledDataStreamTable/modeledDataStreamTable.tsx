c91995f19a1dfd86342a1274ff32e902c5700000
Nischay Maridevegowda <mnischay@amazon.com>
2024-01-08 13:02:34 -0500

    fix(dashboard): for kpi/status disable add of RE when an property is already added

--- packages/dashboard/src/components/queryEditor/iotSiteWiseQueryEditor/modeledDataStreamQueryEditor/modeledDataStreamExplorer/modeledDataStreamTable/modeledDataStreamTable.tsx
+++ packages/dashboard/src/components/queryEditor/iotSiteWiseQueryEditor/modeledDataStreamQueryEditor/modeledDataStreamExplorer/modeledDataStreamTable/modeledDataStreamTable.tsx
@@ -23,6 +23,7 @@ import { SelectedAsset } from '../../types';
 import { ResourceExplorerErrorState } from '../../components/resourceExplorerErrorState';
 import { getPlugin } from '@iot-app-kit/core';
 import { isInValidProperty } from './util/resourceExplorerTableLabels';
+import { disableAdd } from '~/components/queryEditor/iotSiteWiseQueryEditor/footer/disableAdd';
 
 export interface ModeledDataStreamTableProps {
   onClickAddModeledDataStreams: (
@@ -195,10 +196,10 @@ export function ModeledDataStreamTable({
         <ResourceExplorerFooter
           resetDisabled={collectionProps.selectedItems?.length === 0}
           onReset={() => actions.setSelectedItems([])}
-          addDisabled={
-            collectionProps.selectedItems?.length === 0 ||
-            selectedWidgets.length !== 1
-          }
+          addDisabled={disableAdd(
+            selectedWidgets,
+            collectionProps?.selectedItems?.length
+          )}
           onAdd={() => {
             onClickAddModeledDataStreams(
               collectionProps.selectedItems as unknown as ModeledDataStream[]
