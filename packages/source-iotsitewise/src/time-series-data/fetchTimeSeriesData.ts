d7cbd9d8bb5a5f56804a7ebce2c87e944bcb6f2b
Luciana Corteggiano <luciana.corteggiano@gmail.com>
2024-01-11 13:39:37 -0700

    fix: add request settings to fetchTimeSeriesData

--- packages/source-iotsitewise/src/time-series-data/fetchTimeSeriesData.ts
+++ packages/source-iotsitewise/src/time-series-data/fetchTimeSeriesData.ts
@@ -1,4 +1,9 @@
-import { TimeSeriesDataModule, Viewport, DataStream } from '@iot-app-kit/core';
+import {
+  TimeSeriesDataModule,
+  Viewport,
+  DataStream,
+  TimeSeriesDataRequestSettings,
+} from '@iot-app-kit/core';
 import { SiteWiseDataStreamQuery } from './types';
 
 // creates async function which requests cachedDataStreams until all the data for viewport is present
@@ -7,14 +12,17 @@ export const fetchTimeSeriesData =
   async ({
     query,
     viewport,
+    settings,
   }: {
     query: SiteWiseDataStreamQuery;
     viewport: Viewport;
+    settings?: TimeSeriesDataRequestSettings;
   }): Promise<DataStream[]> => {
     return new Promise((resolve) => {
       siteWiseTimeSeriesModule.getCachedDataStreams({
         queries: [query],
         viewport,
+        settings,
         emitDataStreams: (ds: DataStream[]) => resolve(ds),
       });
     });
