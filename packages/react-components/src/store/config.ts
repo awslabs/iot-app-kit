c8be85d919faac44441f4b00aa81ac7dbf215599
Chandru_HM <chandrashekhara.n@happiestminds.com>
2024-01-17 11:19:23 +0530

    fix: removed tanstack table related code

--- packages/react-components/src/store/config.ts
+++ packages/react-components/src/store/config.ts
@@ -1,13 +1,11 @@
 import { StateCreator } from 'zustand/esm';
 
-export type Flags = 'useModelBasedQuery' | 'showTanstackTable';
+export type Flags = 'useModelBasedQuery';
 export interface ConfigState {
   config: Record<Flags, boolean>;
 }
 export const createConfigSlice: StateCreator<ConfigState> = () => ({
   config: {
     useModelBasedQuery: !!localStorage?.getItem('USE_MODEL_BASED_QUERY'),
-    showTanstackTable:
-      localStorage?.getItem('SHOW_LEGEND_TANSTACK_TABLE') === 'true',
   },
 });
