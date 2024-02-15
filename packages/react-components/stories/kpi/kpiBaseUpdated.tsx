31ea2f371676be9b6412073772b9110b01c42786
Luciana Corteggiano <luciana.corteggiano@gmail.com>
2024-02-15 13:15:03 -0700

    feat: updated KPI style (gated)

---
+++ packages/react-components/stories/kpi/kpiBaseUpdated.tsx
@@ -0,0 +1,101 @@
+import React from 'react';
+import { STATUS_ICON_TYPE } from '@iot-app-kit/core';
+import { ComponentMeta, ComponentStory } from '@storybook/react';
+import { KpiBase } from '../../src/components/kpi/kpiBase';
+import { KPISettings } from '../../src/components/kpi/types';
+import type { DataPoint } from '@iot-app-kit/core';
+import type { FC } from 'react';
+import { UpdatedKpiBase } from '../../src/components/kpi/updatedKpiBase';
+
+export default {
+  title: 'Widgets/KPI/Updated KPI Base',
+  component: UpdatedKpiBase,
+  argTypes: {
+    name: { control: { type: 'text' }, defaultValue: 'Windmill' },
+    error: { control: { type: 'text' } },
+    color: { control: { type: 'color' } },
+    unit: { control: { type: 'text' } },
+    icon: {
+      control: { type: 'radio' },
+      options: [...Object.values(STATUS_ICON_TYPE), undefined],
+      defaultValue: undefined,
+    },
+    showName: { control: { type: 'boolean' } },
+    showTimestamp: { control: { type: 'boolean' } },
+    showUnit: { control: { type: 'boolean' } },
+    isLoading: { control: { type: 'boolean' } },
+    propertyPoint: {
+      control: { type: 'object' },
+      defaultValue: { x: 123123213, y: 100 },
+    },
+    alarmPoint: { control: { type: 'object' } },
+    fontSize: { control: { type: 'number' } },
+    secondaryFontSize: { control: { type: 'number' } },
+  },
+  parameters: {
+    layout: 'fullscreen',
+  },
+} as ComponentMeta<typeof KpiBase>;
+
+type StoryInputs = KPISettings & {
+  alarmPoint?: DataPoint;
+  propertyPoint?: DataPoint;
+};
+
+export const Main: ComponentStory<FC<StoryInputs>> = ({
+  showName,
+  showUnit,
+  showTimestamp,
+  fontSize,
+  secondaryFontSize,
+  ...args
+}) => (
+  <div style={{ background: 'grey', padding: '25px' }}>
+    <div style={{ margin: '5px 0' }}>Current KPI:</div>
+    <div
+      style={{
+        background: 'white',
+        width: '250px',
+        height: '150px',
+        marginBottom: '24px',
+      }}
+    >
+      <KpiBase
+        {...args}
+        unit='MPH'
+        name='Wind speed'
+        settings={{
+          showName,
+          showUnit,
+          showTimestamp,
+          fontSize,
+          secondaryFontSize,
+        }}
+      />
+    </div>
+    <div style={{ margin: '5px 0' }}>
+      New (gated under localStorage 'USE_UPDATED_KPI'):
+    </div>
+    <div
+      style={{
+        background: 'white',
+        width: '250px',
+        height: '150px',
+        marginBottom: '24px',
+      }}
+    >
+      <UpdatedKpiBase
+        {...args}
+        unit='MPH'
+        name='Wind speed'
+        settings={{
+          showName,
+          showUnit,
+          showTimestamp,
+          fontSize,
+          secondaryFontSize,
+        }}
+      />
+    </div>
+  </div>
+);
