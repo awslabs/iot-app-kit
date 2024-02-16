1937d7cd774da396067ed90de32c71cfac3754c8
Murali <gmuralidhar.reddy@happiestminds.com>
2024-02-17 03:56:55 +0530

    feat: widget tool box on mouse hover and selection state

--- packages/dashboard/src/customization/widgets/barChart/component.tsx
+++ packages/dashboard/src/customization/widgets/barChart/component.tsx
@@ -41,13 +41,10 @@ const BarChartWidgetComponent: React.FC<BarChartWidget> = (widget) => {
   const aggregation = getAggregation(widget);
   const significantDigits =
     widgetSignificantDigits ?? dashboardSignificantDigits;
-  // there may be better ways to fix this, i.e. not have -44 and let the chart container  take its parent height,
-  // the problem is that the Resizable component needs a "height" to be provided,
-  // so not entirely sure if we can have a mechanism where the container auto adjusts the height
-  // the 44 is from the widget tile header's height
-  const size = { width: chartSize.width, height: chartSize.height - 44 };
+
+  const size = { width: chartSize.width, height: chartSize.height };
   return (
-    <WidgetTile widget={widget} removeable title={title}>
+    <WidgetTile widget={widget} title={title}>
       <BarChart
         chartSize={size}
         key={key}
