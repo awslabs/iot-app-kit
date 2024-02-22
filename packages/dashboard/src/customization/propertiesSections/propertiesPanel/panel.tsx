c5eae05acac16c0e5d2d874bea0a6692028c8fdf
Murali <gmuralidhar.reddy@happiestminds.com>
2024-02-22 19:13:47 +0530

    fix: default Style tab upon widget selection

--- packages/dashboard/src/customization/propertiesSections/propertiesPanel/panel.tsx
+++ packages/dashboard/src/customization/propertiesSections/propertiesPanel/panel.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useLayoutEffect, useState } from 'react';
 import Box from '@cloudscape-design/components/box';
 import Tabs from '@cloudscape-design/components/tabs';
 import SpaceBetween from '@cloudscape-design/components/space-between';
@@ -8,14 +8,24 @@ import { StylesSection } from './styleTab';
 import { PropertiesAndAlarmsSettingsConfiguration } from '../propertiesAndAlarmsSettings';
 import { ThresholdSettingsConfiguration } from '../thresholdSettings';
 import { isJust } from '~/util/maybe';
+import { useSelectedWidgets } from '~/hooks/useSelectedWidgets';
 
 /** Panel element responsible for rendering chart configuration sections. */
 export const PropertiesPanel = () => {
   const selection = useSelection();
+  const selectedWidgets = useSelectedWidgets();
+  const selectedWidgetId = selectedWidgets[0]?.id;
+  const [activeTabId, setActiveTabId] = useState('style');
+
+  useLayoutEffect(() => {
+    setActiveTabId('style'); // Default "Style" tab upon widget selection
+  }, [selectedWidgetId]);
 
   return selection ? (
     <Box>
       <Tabs
+        onChange={({ detail }) => setActiveTabId(detail.activeTabId)}
+        activeTabId={activeTabId}
         disableContentPaddings
         tabs={[
           {
