9f6b707b0ba78981d6ee65536260acf0e1a3bb38
Jimmy Cheung <50635800+chejimmy@users.noreply.github.com>
2024-02-27 14:10:16 -0800

    revert: "feat(dashboard): adding new property panel layout"
    This reverts commit 07ece7bf83b57e4dbe2dafd5de2c92ad20f8410e.

--- packages/dashboard/e2e/tests/test.ts
+++ packages/dashboard/e2e/tests/test.ts
@@ -14,7 +14,6 @@ interface Fixtures {
   dashboardWithKPIWidget: DashboardPage;
   dashboardWithStatusWidget: DashboardPage;
   dashboardWithTableWidget: DashboardPage;
-  dashboardWithPropertyPanel: DashboardPage;
   resourceExplorer: ResourceExplorer;
   configPanel: ConfigPanel;
 }
@@ -71,12 +70,6 @@ export const test = base.extend<Fixtures>({
 
     return use(dashboardWithKPIWidget);
   },
-  dashboardWithPropertyPanel: async ({ page, browser }, use) => {
-    const dashboardWithLineWidgetPropertyPanel =
-      await createNewDashboardWithPropertyPanel(page, browser);
-
-    return use(dashboardWithLineWidgetPropertyPanel);
-  },
   configPanel: async ({ page }, use) => {
     const configPanel = new ConfigPanel({ page });
     await use(configPanel);
@@ -98,20 +91,5 @@ const createNewDashboardWithWidget = async (
   if (resourceExplorer) {
     await resourceExplorer.addModeledProperties([MODELED_PROPERTY_NAME]);
   }
-  return dashboard;
-};
-
-const createNewDashboardWithPropertyPanel = async (
-  page: Page,
-  browser: Browser
-) => {
-  const dashboard = new DashboardPage({ page, browser });
-  await dashboard.goto();
-  await page.evaluate(() =>
-    localStorage.setItem('SHOW_DASHBOARD_LAYOUT', 'true')
-  );
-  await page.reload();
-  await dashboard.makeViewportToAbsolute();
-
   return dashboard;
 };
