1ffead0805048445b677f4cd63a31af7d5912095
chandrashekhara.n <chandrashekhara.n@HMECMB00249.local>
2024-03-21 13:22:41 +0530

    fix: updated theming support for buttons #2668

--- packages/react-components/src/hooks/useTheming/applyTokens.ts
+++ packages/react-components/src/hooks/useTheming/applyTokens.ts
@@ -7,10 +7,31 @@ export const applyTokens = (tokens?: Tokens) => {
     borderRadiusContainer: tokens?.borderRadiusContainer,
     borderRadiusDropdown: tokens?.borderRadiusDropdown,
     borderRadiusInput: tokens?.borderRadiusInput,
+    colorBackgroundButtonNormalActive:
+      tokens?.colorBackgroundButtonNormalActive,
+    colorBackgroundButtonNormalDefault:
+      tokens?.colorBackgroundButtonNormalDefault,
+    colorBackgroundButtonNormalDisabled:
+      tokens?.colorBackgroundButtonNormalDisabled,
+    colorBackgroundButtonNormalHover: tokens?.colorBackgroundButtonNormalHover,
+    colorBackgroundButtonPrimaryActive:
+      tokens?.colorBackgroundButtonPrimaryActive,
+    colorBackgroundButtonPrimaryDefault:
+      tokens?.colorBackgroundButtonPrimaryDefault,
+    colorBackgroundButtonPrimaryDisabled:
+      tokens?.colorBackgroundButtonPrimaryDisabled,
+    colorBackgroundButtonPrimaryHover:
+      tokens?.colorBackgroundButtonPrimaryHover,
     colorBorderButtonNormalActive: tokens?.colorBorderButtonNormalActive,
     colorBorderButtonNormalDefault: tokens?.colorBorderButtonNormalDefault,
     colorBorderButtonNormalDisabled: tokens?.colorBorderButtonNormalDisabled,
     colorBorderButtonNormalHover: tokens?.colorBorderButtonNormalHover,
     colorBorderButtonPrimaryDisabled: tokens?.colorBorderButtonPrimaryDisabled,
+    colorTextButtonNormalActive: tokens?.colorTextButtonNormalActive,
+    colorTextButtonNormalDefault: tokens?.colorTextButtonNormalDefault,
+    colorTextButtonNormalHover: tokens?.colorTextButtonNormalHover,
+    colorTextButtonPrimaryActive: tokens?.colorTextButtonPrimaryActive,
+    colorTextButtonPrimaryDefault: tokens?.colorTextButtonPrimaryDefault,
+    colorTextButtonPrimaryHover: tokens?.colorTextButtonPrimaryHover,
   };
 };
