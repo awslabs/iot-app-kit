66e6680fbea3f005aba7239c3c47bf13e5184462
Kausar-HM <kausar.shaikh@happiestminds.com>
2024-03-21 16:48:20 +0530

    feat: added support for border theming options #2668

--- packages/react-components/src/hooks/useTheming/applyTokens.ts
+++ packages/react-components/src/hooks/useTheming/applyTokens.ts
@@ -3,5 +3,14 @@ import { Tokens } from './types';
 export const applyTokens = (tokens?: Tokens) => {
   return {
     fontFamilyBase: tokens?.fontFamilyBase,
+    borderRadiusButton: tokens?.borderRadiusButton,
+    borderRadiusContainer: tokens?.borderRadiusContainer,
+    borderRadiusDropdown: tokens?.borderRadiusDropdown,
+    borderRadiusInput: tokens?.borderRadiusInput,
+    colorBorderButtonNormalActive: tokens?.colorBorderButtonNormalActive,
+    colorBorderButtonNormalDefault: tokens?.colorBorderButtonNormalDefault,
+    colorBorderButtonNormalDisabled: tokens?.colorBorderButtonNormalDisabled,
+    colorBorderButtonNormalHover: tokens?.colorBorderButtonNormalHover,
+    colorBorderButtonPrimaryDisabled: tokens?.colorBorderButtonPrimaryDisabled,
   };
 };
