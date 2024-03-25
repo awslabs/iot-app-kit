57bcb3ddd4d1b0d88d301838f736fbf601ba48d5
Kausar-HM <kausar.shaikh@happiestminds.com>
2024-03-25 14:52:40 +0530

    fix: added validation for the decimal places input #2567

--- packages/dashboard/src/customization/propertiesSections/propertiesPanel/styleTab.tsx
+++ packages/dashboard/src/customization/propertiesSections/propertiesPanel/styleTab.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 import { AggregationsSettingsConfiguration } from '../aggregationSettings';
 import { AxisSettingsConfiguration } from '../axisSettings';
-import { SettingsConfiguration } from '../settings';
+import { FormatDataConfiguration } from '../formatDataSettings';
 import { TextSettingsConfiguration } from '../textSettings';
 import { LineAndScatterStyleSettingsSection } from '../lineAndScatterStyleSettings/section';
 import { WidgetTitle } from '../widgetTitle';
@@ -11,11 +11,11 @@ import { DisplaySettingsSection } from '../displaySettingsSection';
 export const StylesSection = () => (
   <div>
     <WidgetTitle />
+    <AggregationsSettingsConfiguration />
+    <FormatDataConfiguration />
     <LineAndScatterStyleSettingsSection />
     <DisplaySettingsSection />
-    <AggregationsSettingsConfiguration />
     <AxisSettingsConfiguration />
-    <SettingsConfiguration />
     <TextSettingsConfiguration />
   </div>
 );
