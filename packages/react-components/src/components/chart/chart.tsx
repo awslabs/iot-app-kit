7ab024c74310e4e492022232f67e80179f938a18
Luciana Corteggiano <luciana.corteggiano@gmail.com>
2024-04-11 13:16:38 -0600

    fix: add default settings to charts

--- packages/react-components/src/components/chart/chart.tsx
+++ packages/react-components/src/components/chart/chart.tsx
@@ -3,12 +3,15 @@ import { ChartOptions } from './types';
 import { ChartStoreProvider } from './store';
 import BaseChart from './baseChart';
 import { useChartId } from './hooks/useChartId';
+import { DEFAULT_CHART_SETTINGS } from './eChartsConstants';
 
 export const Chart: React.FC<ChartOptions> = (options) => {
   const chartId = useChartId(options.id);
+  const chartOptions = { ...DEFAULT_CHART_SETTINGS, ...options };
+
   return (
     <ChartStoreProvider id={chartId}>
-      <BaseChart {...options} id={chartId} />
+      <BaseChart {...chartOptions} id={chartId} />
     </ChartStoreProvider>
   );
 };
