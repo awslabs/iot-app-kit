c56522e05570157552f0df1184d219c14bcdc955
Weston Harper <haweston@amazon.com>
2024-05-23 11:59:44 -0500

    chore(scene): detect dynamic scenes missing their root entity

--- packages/scene-composer/src/components/SceneLayers.tsx
+++ packages/scene-composer/src/components/SceneLayers.tsx
@@ -1,12 +1,16 @@
-import React, { useContext, useEffect } from 'react';
+import React, { useCallback, useContext, useEffect } from 'react';
 import { useQuery } from '@tanstack/react-query';
+import { useIntl } from 'react-intl';
 
+import { getGlobalSettings } from '../common/GlobalSettings';
 import { sceneComposerIdContext } from '../common/sceneComposerIdContext';
 import { accessStore } from '../store';
-import { fetchSceneNodes } from '../utils/entityModelUtils/sceneUtils';
+import { checkIfEntityExists, fetchSceneNodes } from '../utils/entityModelUtils/sceneUtils';
 import { KnownSceneProperty } from '../interfaces';
+import { DisplayMessageCategory } from '../store/internalInterfaces';
 
 export const SceneLayers: React.FC = () => {
+  const { formatMessage } = useIntl();
   const sceneComposerId = useContext(sceneComposerIdContext);
   const isViewing = accessStore(sceneComposerId)((state) => state.isViewing());
   const autoUpdateInterval = accessStore(sceneComposerId)(
@@ -18,6 +22,7 @@ export const SceneLayers: React.FC = () => {
   const sceneRootEntityId = accessStore(sceneComposerId)((state) =>
     state.getSceneProperty(KnownSceneProperty.SceneRootEntityId),
   ) as string;
+  const addMessages = accessStore(sceneComposerId)((state) => state.addMessages);
 
   const nodes = useQuery({
     enabled: !!sceneRootEntityId,
@@ -31,11 +36,32 @@ export const SceneLayers: React.FC = () => {
     refetchOnWindowFocus: false,
   });
 
+  const checkForSceneRootEntity = useCallback(async () => {
+    const sceneMetadataModule = getGlobalSettings().twinMakerSceneMetadataModule;
+    if (sceneMetadataModule) {
+      if (!(await checkIfEntityExists(sceneRootEntityId, sceneMetadataModule))) {
+        addMessages([
+          {
+            category: DisplayMessageCategory.Error,
+            messageText: formatMessage({
+              description: 'Scene Entity Error',
+              defaultMessage: 'Dynamic Scene is missing root scene entity',
+            }),
+          },
+        ]);
+      }
+    }
+  }, [sceneRootEntityId, addMessages, formatMessage]);
+
   useEffect(() => {
     if (nodes.data) {
-      renderSceneNodes(nodes.data);
+      if (nodes.data.length > 0) {
+        renderSceneNodes(nodes.data);
+      } else {
+        checkForSceneRootEntity();
+      }
     }
-  }, [nodes.data, renderSceneNodes]);
+  }, [nodes.data, renderSceneNodes, checkForSceneRootEntity]);
 
   return <></>;
 };
