f526333dfb81164d3b9451ba98705a01e7d4ae97
Sahil Jagad <sahiljag@amazon.com>
2024-06-05 12:11:42 -0700

    feat(dashboard): migration logic now supports avoiding collisions that lead to overlap

--- packages/dashboard/src/migration/constants.ts
+++ packages/dashboard/src/migration/constants.ts
@@ -1,3 +1,5 @@
+import { MonitorWidgetType, MonitorMetric, MonitorAnnotations } from './types';
+
 export const defaultDisplaySettings = {
   numRows: 100,
   numColumns: 100,
@@ -79,5 +81,95 @@ export const timelineProperties = {
   axis: {
     showY: true,
     showX: true,
+  },
+};
+
+export const createMonitorChartWidget = (
+  widgetType: MonitorWidgetType,
+  metrics: MonitorMetric[],
+  annotations?: MonitorAnnotations,
+  title?: string,
+  x?: number,
+  y?: number
+) => {
+  return {
+    type: widgetType,
+    title: title ?? 'test',
+    x: x ?? 0,
+    y: y ?? 0,
+    height: 3,
+    width: 3,
+    metrics,
+    alarms: [],
+    properties: {
+      colorDataAcrossThresholds: true,
+    },
+    annotations,
+  };
+};
+
+export const createApplicationChartDefinition = (
+  widgetType: string,
+  properties: object,
+  height = 23.5,
+  width = 41.5,
+  x = 0,
+  y = 0,
+  z = 0
+) => {
+  return {
+    type: widgetType,
+    x,
+    y,
+    z,
+    width,
+    height,
+    properties,
+  };
+};
+
+export const metrics = [
+  {
+    type: 'iotsitewise',
+    label: 'Total Average Power (Demo Wind Farm Asset)',
+    assetId: '3d196ab5-85db-4c90-854f-4e29d579b898',
+    propertyId: 'c07c2fa5-265e-4ed4-bbf0-e94fe01e4d54',
+    dataType: 'DOUBLE',
+  },
+];
+
+export const expectedProperties = {
+  title: 'test',
+  symbol: {
+    style: 'filled-circle',
+  },
+  axis: {
+    yVisible: true,
+    xVisible: true,
+  },
+  line: {
+    connectionStyle: 'linear',
+    style: 'solid',
+  },
+  legend: {
+    visible: true,
+  },
+  queryConfig: {
+    source: 'iotsitewise',
+    query: {
+      properties: [],
+      assets: [
+        {
+          assetId: '3d196ab5-85db-4c90-854f-4e29d579b898',
+          properties: [
+            {
+              aggregationType: 'AVERAGE',
+              propertyId: 'c07c2fa5-265e-4ed4-bbf0-e94fe01e4d54',
+              resolution: '1m',
+            },
+          ],
+        },
+      ],
+    },
   },
 };
