8ff2281ddf4142744af7c08676baf0a1b302fe9b
jmbuss <bussterjason@gmail.com>
2024-06-06 14:36:31 -0600

    fix(react-components): ensure chart uses initial passed in viewport

--- packages/react-components/src/hooks/useViewport/useUtilizedViewport.ts
+++ packages/react-components/src/hooks/useViewport/useUtilizedViewport.ts
@@ -1,8 +1,10 @@
 import { Viewport } from '@iot-app-kit/core';
 import { useViewport } from './useViewport';
 import { useCallback, useState } from 'react';
-import { useCustomCompareEffect } from 'react-use';
+import { useCustomCompareEffect, useEffectOnce } from 'react-use';
 import { isEqual } from 'lodash';
+
+type UtilizedViewportType = 'passed-in' | 'injected' | 'default' | 'none';
 
 const resolveVieport = ({
   passedInViewport,
@@ -12,7 +14,25 @@ const resolveVieport = ({
   passedInViewport?: Viewport;
   injectedViewport?: Viewport;
   defaultViewport?: Viewport;
-}) => passedInViewport ?? injectedViewport ?? defaultViewport;
+}): { viewport?: Viewport; type: UtilizedViewportType } => {
+  let viewport = undefined;
+  let type: UtilizedViewportType = 'none';
+  if (passedInViewport != null) {
+    viewport = passedInViewport;
+    type = 'passed-in';
+  } else if (injectedViewport != null) {
+    viewport = injectedViewport;
+    type = 'injected';
+  } else if (defaultViewport != null) {
+    viewport = defaultViewport;
+    type = 'default';
+  }
+
+  return {
+    viewport,
+    type,
+  };
+};
 
 type UseUtilizedViewportOptions = {
   passedInViewport?: Viewport;
@@ -28,7 +48,30 @@ export const useUtilizedViewport = ({
 
   const [viewport, setViewport] = useState(
     resolveVieport({ passedInViewport, injectedViewport, defaultViewport })
+      .viewport
   );
+  const [viewportType, setViewportType] = useState(
+    resolveVieport({ passedInViewport, injectedViewport, defaultViewport }).type
+  );
+
+  /**
+   * The viewportManager will emit an event with the default viewport (10m)
+   * if the component is not actually in a time sync provider.
+   * This is because the manager wants to ensure that anything subscribed
+   * to it gets the value when it subscribes. If we set the value
+   * here, we ensure this does not happen.
+   */
+  useEffectOnce(() => {
+    const { viewport: initialViewport, type: initialType } = resolveVieport({
+      passedInViewport,
+      injectedViewport,
+      defaultViewport,
+    });
+    if (initialViewport) {
+      updateViewportGroup(initialViewport);
+      setViewportType(initialType);
+    }
+  });
 
   const updateViewport = useCallback(
     (updatedViewport: Viewport, updatedBy?: string) => {
@@ -40,13 +83,18 @@ export const useUtilizedViewport = ({
 
   useCustomCompareEffect(
     () => {
-      const resolvedViewport = resolveVieport({
-        passedInViewport,
-        injectedViewport,
-        defaultViewport,
-      });
+      const { viewport: resolvedViewport, type: resolvedType } = resolveVieport(
+        {
+          passedInViewport,
+          injectedViewport,
+          defaultViewport,
+        }
+      );
       if (!isEqual(viewport, resolvedViewport)) {
         setViewport(resolvedViewport);
+      }
+      if (!isEqual(viewportType, resolvedType)) {
+        setViewportType(resolvedType);
       }
     },
     [passedInViewport, injectedViewport, defaultViewport],
@@ -56,5 +104,6 @@ export const useUtilizedViewport = ({
   return {
     viewport,
     setViewport: updateViewport,
+    viewportType,
   };
 };
