02b61aea03e340ba53652ff0159b1a3ecd1f0f66
Fernando Pauer <pauerf@amazon.com>
2024-06-25 11:33:27 -0700

    feat: useAssistant hook

--- packages/react-components/src/hooks/useAssistant/useAssistant.ts
+++ packages/react-components/src/hooks/useAssistant/useAssistant.ts
@@ -1,42 +1,94 @@
-import messages from '@cloudscape-design/components/i18n/messages/all.all';
-import type { IoTSitewiseAssistantClient } from '@iot-app-kit/core-util';
-import type { IMessageParser, BaseStateManager } from './types';
+import { useState } from 'react';
+import type {
+  AssistantClientInvocationResponse,
+  AssistantClientSummarizationProperties,
+} from '@iot-app-kit/core-util';
+import { IoTSitewiseAssistantClient } from '@iot-app-kit/core-util';
+import type { IMessageParser, BaseStateManager, IMessage } from './types';
+import { SenderType } from './types';
 import { MessageParser } from './messageParser';
 import { StateManager } from './stateManager';
 
 export interface IUseAssistant {
-  assistantClient: IoTSitewiseAssistantClient,
-  /** required to call invokeAssistant */ 
-  assistantName: string; 
-  
+  assistantClient: IoTSitewiseAssistantClient;
+
   /** optional but a default implementation will be provided */
-  messageParser?: IMessageParser; 
-  stateManager?: BaseStateManager;
-      
-  // Defines an object that will be injected into the chatbot state
-  // initialState?: Record<string, any>;
+  messageParser?: IMessageParser & MessageParser;
+  stateManager?: BaseStateManager & StateManager<any>;
 }
 
-const createDefaultMessageParger = () => {
-  return new MessageParser( new StateManager() );
-}
+const internalStateManager = new StateManager(() => {});
+const internalMessageParser = new MessageParser();
 
 export const useAssistant = ({
   assistantClient,
-  assistantName,
   messageParser,
   stateManager,
 }: IUseAssistant) => {
+  const [messages, setMessages] = useState<IMessage[]>([]);
 
-  const invokeAssistant = () => {
-    assistantClient.
+  let currentStateManager = internalStateManager;
+  let currentMessageParser = internalMessageParser;
+
+  if (stateManager) {
+    currentStateManager = stateManager;
+    currentMessageParser.setStateManager(stateManager);
   }
-  
+
+  if (messageParser) {
+    currentMessageParser = messageParser;
+    currentMessageParser.setStateManager(currentStateManager);
+  }
+
+  const onResponse = (response: AssistantClientInvocationResponse) => {
+    if (response.statusCode === 200) {
+      currentMessageParser.parse(response.body);
+      setMessages(currentStateManager.getState().messages);
+    }
+  };
+
+  const onComplete = (response: AssistantClientInvocationResponse) => {
+    if (response.statusCode === 200) {
+      currentMessageParser.parse(response.body);
+      setMessages(currentStateManager.getState().messages);
+    }
+  };
+
+  assistantClient.setRequestHandlers(
+    assistantClient.onResponse ? assistantClient.onResponse : onResponse,
+    assistantClient.onComplete ? assistantClient.onComplete : onComplete
+  );
+
+  const invokeAssistant = (
+    conversationId: string,
+    utterance: string,
+    context?: string
+  ) => {
+    currentStateManager.addText(utterance, SenderType.USER);
+    assistantClient.invoke(conversationId, utterance, context);
+  };
+
+  const generateSummary = (
+    conversationId: string,
+    sitewiseProperties: AssistantClientSummarizationProperties,
+    summaryUtterance?: string,
+    summaryInstructions?: string
+  ) => {
+    if (summaryUtterance) {
+      currentStateManager.addText(summaryUtterance, SenderType.USER);
+    }
+    assistantClient.generateSummary(
+      conversationId,
+      sitewiseProperties,
+      summaryUtterance,
+      summaryInstructions
+    );
+  };
 
   return {
-    messages ??,
+    messages,
     invokeAssistant,
-    endConversation,
-    generateSummary
-  }
-}
+    generateSummary,
+    endConversation: assistantClient.endConversation,
+  };
+};
