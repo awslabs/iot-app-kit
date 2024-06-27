05445c3e12db17be69def0b449e8adca7f44ff3f
Luciana Corteggiano <luciana.corteggiano@gmail.com>
2024-06-27 09:35:30 -0600

    fix(react-components): anomaly chart xaxis formatting

--- packages/react-components/src/components/anomaly-chart/converters/convertXAxis.ts
+++ packages/react-components/src/components/anomaly-chart/converters/convertXAxis.ts
@@ -6,5 +6,16 @@ export const convertXAxis = ({ axis }: Pick<ConfigurationOptions, 'axis'>) =>
   merge({}, ANOMALY_X_AXIS.xAxis, {
     axisLabel: {
       show: axis?.showX ?? true,
+      align: 'center',
+      formatter: {
+        year: `{yyyy}`,
+        month: `{MMM}`,
+        day: '{MMM} {d}',
+        hour: '{MMM} {d}\n{HH}:{mm}',
+        minute: '{MMM} {d}\n{HH}:{mm}',
+        second: '{MMM} {d}\n{HH}:{mm}:{ss}',
+        millisecond: '{MMM} {d}\n{hh}:{mm}:{ss} {SSS}',
+        none: `{yyyy}-{MM}={dd} {hh}:{mm}:{ss} {SSS}`,
+      },
     },
   });
