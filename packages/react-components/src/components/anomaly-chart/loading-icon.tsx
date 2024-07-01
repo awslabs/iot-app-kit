581a3c57734ba460ced0e32e232ed38583f232ba
Luciana Corteggiano <luciana.corteggiano@gmail.com>
2024-07-01 12:42:08 -0600

    fix(react-components): anomaly chart move loading state

--- packages/react-components/src/components/anomaly-chart/loading-icon.tsx
+++ packages/react-components/src/components/anomaly-chart/loading-icon.tsx
@@ -1,14 +1,34 @@
-import Button from '@cloudscape-design/components/button';
+import { Box, Popover, Spinner } from '@awsui/components-react';
+import {
+  colorTextBodyDefault,
+  fontFamilyBase,
+} from '@cloudscape-design/design-tokens';
 import React from 'react';
+import './anomalyChartStyles.css';
 
 export const LoadingIcon = ({ loading }: { loading?: boolean }) => {
   if (!loading) return null;
 
   return (
-    <div style={{ position: 'absolute', right: 8, bottom: 2, zIndex: 1 }}>
-      <Button loading variant='icon' loadingText='Loading'>
-        Loading
-      </Button>
+    <div
+      className='anomaly-chart-loading-icon'
+      style={{
+        fontFamily: `${fontFamilyBase}`,
+        color: colorTextBodyDefault,
+      }}
+    >
+      <Box color='text-label'>
+        <Popover
+          size='small'
+          content='Data may be inaccurate until loading completes.'
+          dismissButton={false}
+        >
+          <div style={{ display: 'flex', alignItems: 'center' }}>
+            <Spinner />
+            <span style={{ marginLeft: '5px' }}>Loading</span>
+          </div>
+        </Popover>
+      </Box>
     </div>
   );
 };
