2ca7e6caf771bd929b993d79f91f6e7d3ce21350
Harry Wandersman <hwanders@amazon.com>
2024-09-04 16:21:51 -0400

    feat(react-components): implement request functions and hooks to build clients

--- packages/react-components/src/queries/useAssetPropertyValues/requestExecution/getAssetPropertyAggregates.ts
+++ packages/react-components/src/queries/useAssetPropertyValues/requestExecution/getAssetPropertyAggregates.ts
@@ -5,7 +5,7 @@ import {
   GetAssetPropertyAggregatesRequestFunction,
 } from '../types';
 import { SendOptions } from '../../useTimeSeriesData/requestExecution/types';
-import { RequestResponse } from '../../request-fn';
+import { RequestResponse } from '@iot-app-kit/core';
 import {
   ExecuteRequestStrategy,
   OnRequestSuccessCallback,
