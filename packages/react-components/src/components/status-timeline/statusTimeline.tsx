8727939dc97553a49570fa375ac6272b842f18b7
Luciana Corteggiano <luciana.corteggiano@gmail.com>
2024-09-27 08:29:10 -0600

    feat(alarms): integrate status timeline with alarms

--- packages/react-components/src/components/status-timeline/statusTimeline.tsx
+++ packages/react-components/src/components/status-timeline/statusTimeline.tsx
@@ -1,10 +1,6 @@
-import React from 'react';
-import {
-  StyleSettingsMap,
-  Threshold,
-  TimeSeriesDataQuery,
-  Viewport,
-} from '@iot-app-kit/core';
+/* eslint-disable react-hooks/exhaustive-deps */
+import React, { useMemo } from 'react';
+import { StyleSettingsMap, Threshold, Viewport } from '@iot-app-kit/core';
 import {
   StatusTimeline as StatusTimelineBaseWrongType,
   LineChart,
@@ -21,6 +17,10 @@ import {
   ECHARTS_GESTURE,
 } from '../../common/constants';
 import type { ComponentQuery } from '../../common/chartTypes';
+import { useAlarms } from '../../hooks/useAlarms';
+import { getAlarmQueries, getTimeSeriesQueries } from '../../utils/queries';
+import { transformAlarmsToThreshold } from '../../utils/transformAlarmsToThreshold';
+import { convertAlarmQueryToAlarmRequest } from '../../queries/utils/convertAlarmQueryToAlarmRequest';
 
 // TODO: Remove this type assertion - iot-app-kit/charts has the wrong type for StatusTimeline
 const StatusTimelineBase: typeof LineChart =
@@ -45,9 +45,35 @@ export const StatusTimeline = ({
   gestures?: boolean;
   significantDigits?: number;
 }) => {
+  const { viewport, setViewport, group, lastUpdatedBy } = useViewport();
+  // if using echarts then echarts gesture overrides passed in viewport
+  // else explicitly passed in viewport overrides viewport group
+  const utilizedViewport =
+    (lastUpdatedBy === ECHARTS_GESTURE
+      ? viewport
+      : passedInViewport || viewport) ?? DEFAULT_VIEWPORT;
+
+  const alarmQueries = getAlarmQueries(queries);
+  const timeSeriesQueries = getTimeSeriesQueries(queries);
+
+  const transformedAlarms = useAlarms({
+    iotSiteWiseClient: alarmQueries.at(0)?.iotSiteWiseClient,
+    iotEventsClient: alarmQueries.at(0)?.iotEventsClient,
+    requests: alarmQueries.flatMap((query) =>
+      convertAlarmQueryToAlarmRequest(query)
+    ),
+    viewport: utilizedViewport,
+    settings: {
+      fetchThresholds: true,
+      refreshRate: alarmQueries.at(0)?.query.requestSettings?.refreshRate,
+    },
+    transform: transformAlarmsToThreshold,
+  });
+  const filteredAlarms = transformedAlarms.filter((alarm) => !!alarm);
+
   const { dataStreams, thresholds: queryThresholds } = useTimeSeriesData({
     viewport: passedInViewport,
-    queries: queries.filter((q) => 'build' in q) as TimeSeriesDataQuery[],
+    queries: timeSeriesQueries,
     settings: {
       fetchFromStartToEnd: true,
       fetchMostRecentBeforeStart: true,
@@ -55,15 +81,15 @@ export const StatusTimeline = ({
     },
     styles,
   });
-  const { viewport, setViewport, group, lastUpdatedBy } = useViewport();
-  const allThresholds = [...queryThresholds, ...thresholds];
 
-  // if using echarts then echarts gesture overrides passed in viewport
-  // else explicitly passed in viewport overrides viewport group
-  const utilizedViewport =
-    (lastUpdatedBy === ECHARTS_GESTURE
-      ? viewport
-      : passedInViewport || viewport) ?? DEFAULT_VIEWPORT;
+  const allThresholds = useMemo(
+    () => [...queryThresholds, ...thresholds, ...filteredAlarms],
+    [
+      JSON.stringify(queryThresholds),
+      JSON.stringify(thresholds),
+      JSON.stringify(filteredAlarms),
+    ]
+  );
 
   return (
     <StatusTimelineBase
