b952252dde2ad0b04e66907bdab2631a61d01b47
Tracy French <tracfren@amazon.com>
2024-12-11 11:53:58 -0700

    fix: remove orange button

--- packages/dashboard/src/components/queryEditor/iotSiteWiseQueryEditor/assetModelDataStreamExplorer/assetModelExplorer/assetModelSelection/assetModelSelected.tsx
+++ packages/dashboard/src/components/queryEditor/iotSiteWiseQueryEditor/assetModelDataStreamExplorer/assetModelExplorer/assetModelSelection/assetModelSelected.tsx
@@ -1,8 +1,6 @@
-import Box from '@cloudscape-design/components/box';
-import Button from '@cloudscape-design/components/button';
-
+import CloudscapeBox from '@cloudscape-design/components/box';
+import CloudscapeButton from '@cloudscape-design/components/button';
 import { spaceScaledXxs } from '@cloudscape-design/design-tokens';
-
 import { VerticalDivider } from '../../../../../../components/divider/verticalDivider';
 import { type SelectedAssetModel } from '../../useSelectedAssetModel';
 import { type IoTSiteWise } from '@aws-sdk/client-iotsitewise';
@@ -11,70 +9,72 @@ import {
   type SelectedAsset,
   type UpdateSelectedAsset,
 } from '../../useSelectedAsset';
-
 import { useModalVisibility } from '../../../../../../hooks/useModalVisibility';
 import { ResetAssetModelModal } from './resetAssetModel/resetAssetModelModal';
-
 import { default as assetModelSvg } from './assetModelIcon.svg';
-
 import './assetModelSelected.css';
 import { useAssetModel } from '../../../../../../hooks/useAssetModel/useAssetModel';
+import { memo } from 'react';
 
-type AssetModelSelectedOptions = {
+export interface AssetModelSelectedProps {
   selectedAssetModel: SelectedAssetModel;
   selectedAsset: SelectedAsset;
   setSelectedAsset: UpdateSelectedAsset;
-  onResetSelectedAssetModel?: () => void;
+  onResetSelectedAssetModel: VoidFunction;
   iotSiteWiseClient: IoTSiteWise;
-};
+}
 
-export const AssetModelSelected = ({
-  selectedAsset,
-  selectedAssetModel,
-  setSelectedAsset,
-  onResetSelectedAssetModel,
-  iotSiteWiseClient,
-}: AssetModelSelectedOptions) => {
-  const { visible, onHide, onShow } = useModalVisibility();
-
-  const { assetModel } = useAssetModel({
-    assetModelId: selectedAssetModel?.at(0)?.assetModelId,
+export const AssetModelSelected = memo(
+  ({
+    selectedAsset,
+    selectedAssetModel,
+    setSelectedAsset,
+    onResetSelectedAssetModel,
     iotSiteWiseClient,
-  });
+  }: AssetModelSelectedProps) => {
+    const { visible, onHide, onShow } = useModalVisibility();
 
-  return (
-    <div>
-      <div
-        className='selected-asset-model-conatiner'
-        style={{
-          gap: spaceScaledXxs,
-        }}
-      >
-        <img src={assetModelSvg} alt='Selected asset model icon' />
-        <VerticalDivider
-          classNames={['reset-selected-asset-model-vertical-divider']}
-        />
-        <Box fontWeight='bold' variant='span'>
-          Asset model:
-        </Box>
-        <Box variant='span'>{assetModel?.at(0)?.path?.[0].name}</Box>
-        <div className='reset-selected-asset-model-container'>
-          <Button onClick={onShow}>Reset</Button>
+    const { assetModel } = useAssetModel({
+      assetModelId: selectedAssetModel?.at(0)?.assetModelId,
+      iotSiteWiseClient,
+    });
+
+    return (
+      <div>
+        <div
+          className='selected-asset-model-conatiner'
+          style={{
+            gap: spaceScaledXxs,
+          }}
+        >
+          <img src={assetModelSvg} alt='Selected asset model icon' />
+          <VerticalDivider
+            classNames={['reset-selected-asset-model-vertical-divider']}
+          />
+          <CloudscapeBox fontWeight='bold' variant='span'>
+            Asset model:
+          </CloudscapeBox>
+          <CloudscapeBox variant='span'>
+            {assetModel?.at(0)?.path?.[0].name}
+          </CloudscapeBox>
+          <div className='reset-selected-asset-model-container'>
+            <CloudscapeButton onClick={onShow}>Reset</CloudscapeButton>
+          </div>
         </div>
-      </div>
-      <Box padding={{ top: 's' }}>
-        <AssetForAssetModelSelectForm
-          selectedAssetModel={selectedAssetModel}
-          selectedAsset={selectedAsset}
-          onSelectAsset={setSelectedAsset}
-          iotSiteWiseClient={iotSiteWiseClient}
+        <CloudscapeBox padding={{ top: 's' }}>
+          <AssetForAssetModelSelectForm
+            selectedAssetModel={selectedAssetModel}
+            selectedAsset={selectedAsset}
+            onSelectAsset={setSelectedAsset}
+            iotSiteWiseClient={iotSiteWiseClient}
+          />
+        </CloudscapeBox>
+        <ResetAssetModelModal
+          visible={visible}
+          onHide={onHide}
+          onReset={onResetSelectedAssetModel}
         />
-      </Box>
-      <ResetAssetModelModal
-        visible={visible}
-        onHide={onHide}
-        onReset={onResetSelectedAssetModel}
-      />
-    </div>
-  );
-};
+      </div>
+    );
+  }
+);
