ee8f6fc2ebaf4d290119a57514d0f24f27efe850
Tracy French <tracfren@amazon.com>
2024-12-11 11:53:58 -0700

    fix: remove orange button

---
+++ packages/atoms/.eslintrc.cjs
@@ -0,0 +1,19 @@
+const base = require('@iot-app-kit/eslint-config');
+
+module.exports = {
+  ...base,
+  root: true,
+  settings: {
+    ...base.settings,
+    'import/resolver': {
+      ...base.settings['import/resolver'],
+      typescript: {
+        ...base.settings['import/resolver'].typescript,
+        project: ['packages/atoms/tsconfig.json'],
+      },
+      node: {
+        project: ['packages/atoms/tsconfig.json'],
+      },
+    },
+  },
+};
