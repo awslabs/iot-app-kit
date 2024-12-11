6f15fc3dcd013e54217c9082b2c8a114c3eca761
Tracy French <tracfren@amazon.com>
2024-12-11 11:53:58 -0700

    fix: remove orange button

--- packages/dashboard/src/components/confirmDeleteModal/index.tsx
+++ packages/dashboard/src/components/confirmDeleteModal/index.tsx
@@ -1,60 +1,57 @@
-import { type ReactElement } from 'react';
+import { memo, type ReactElement } from 'react';
+import CloudscapeBox from '@cloudscape-design/components/box';
+import CloudscapeButton from '@cloudscape-design/components/button';
+import CloudscapeModal from '@cloudscape-design/components/modal';
+import CloudscapeSpaceBetween from '@cloudscape-design/components/space-between';
+import { Button } from '@iot-app-kit/atoms';
 
-import {
-  Box,
-  Button,
-  Modal,
-  SpaceBetween,
-} from '@cloudscape-design/components';
-
-import CustomOrangeButton from '../customOrangeButton';
-
-interface ConfirmDeleteModalProps {
+export interface ConfirmDeleteModalProps {
   headerTitle: string;
   cancelTitle?: string;
   submitTitle: string;
   description: ReactElement | string;
   visible: boolean;
-  handleDismiss: () => void;
-  handleCancel: () => void;
-  handleSubmit: () => void;
+  handleDismiss: VoidFunction;
+  handleCancel: VoidFunction;
+  handleSubmit: VoidFunction;
 }
 
-const ConfirmDeleteModal = ({
-  headerTitle,
-  cancelTitle = 'Cancel',
-  submitTitle,
-  description,
-  visible,
-  handleDismiss,
-  handleCancel,
-  handleSubmit,
-}: ConfirmDeleteModalProps) => {
-  return (
-    <Modal
-      visible={visible}
-      onDismiss={handleDismiss}
-      header={headerTitle}
-      data-testid='confirm-modal'
-      footer={
-        <Box float='right'>
-          <SpaceBetween direction='horizontal' size='xs'>
-            {cancelTitle && (
-              <Button variant='link' onClick={handleCancel}>
-                {cancelTitle}
+const ConfirmDeleteModal = memo(
+  ({
+    headerTitle,
+    cancelTitle = 'Cancel',
+    submitTitle,
+    description,
+    visible,
+    handleDismiss,
+    handleCancel,
+    handleSubmit,
+  }: ConfirmDeleteModalProps) => {
+    return (
+      <CloudscapeModal
+        visible={visible}
+        onDismiss={handleDismiss}
+        header={headerTitle}
+        data-testid='confirm-modal'
+        footer={
+          <CloudscapeBox float='right'>
+            <CloudscapeSpaceBetween direction='horizontal' size='xs'>
+              {cancelTitle && (
+                <CloudscapeButton variant='link' onClick={handleCancel}>
+                  {cancelTitle}
+                </CloudscapeButton>
+              )}
+              <Button type='primary' onClick={handleSubmit}>
+                {submitTitle}
               </Button>
-            )}
-            <CustomOrangeButton
-              title={submitTitle}
-              handleClick={handleSubmit}
-            />
-          </SpaceBetween>
-        </Box>
-      }
-    >
-      {description}
-    </Modal>
-  );
-};
+            </CloudscapeSpaceBetween>
+          </CloudscapeBox>
+        }
+      >
+        {description}
+      </CloudscapeModal>
+    );
+  }
+);
 
 export default ConfirmDeleteModal;
