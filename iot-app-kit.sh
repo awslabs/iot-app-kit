0089e25579b14b7c3a2ce895d750167f86b7ab0d
Tracy French <tracfren@amazon.com>
2025-02-12 17:39:19 -0700

    chore: improve iot-app-kit CLI

--- iot-app-kit.sh
+++ iot-app-kit.sh
@@ -2,6 +2,10 @@
 
 bold=$(tput bold)
 normal=$(tput sgr0)
+red='\033[0;31m'
+green='\033[0;32m'
+blue='\033[0;34m'
+no_color='\033[0m'
 
 PACKAGE_LIST=(
   "@iot-app-kit/core"
@@ -23,7 +27,8 @@ function iot-app-kit() {
   elif [ $cmd = tarball ]; then
     iot-app-kit__tarball "$@"
   elif [ $cmd = --help ]; then
-    echo "
+    echo "iot-app-kit CLI
+
 ${bold}USAGE${normal}
   iot-app-kit <command> <subcommand> [flags]
 
@@ -32,13 +37,22 @@ ${bold}CORE COMMANDS${normal}
   tarball:   Manage iot-app-kit package tarballs
 
 ${bold}FLAGS${normal}
-  --help     Show help for a command
+  --help     Show help for command
   --version  Show iot-app-kit version
+
+${bold}EXAMPLES${normal}
+  $ iot-app-kit package build core
+  $ iot-app-kit package pack core-util
+  $ iot-app-kit package publish dashboard --simulate
+  $ iot-app-kit package list react-components
+  $ iot-app-kit tarball copy iot-app-kit-source-iotsitewise-12.5.0.tgz --destinations "dashboard react-components"
+  $ iot-app-kit tarball link iot-app-kit-source-iottwinmaker-12.5.0.tgz --destinations react-components
     "
+    exit 0
   elif [ $cmd = --version ]; then
     npm pkg get version | tr -d '"'
   else
-    echo "Invalid iot-app-kit command: $cmd"
+    echo -e ${red}${bold}"Error: Invalid iot-app-kit command: $cmd."${normal}${no_color}
     exit 1
   fi
 }
@@ -57,8 +71,32 @@ function iot-app-kit__package() {
     iot-app-kit__package__publish "$@"
   elif [ $cmd = list ]; then
     iot-app-kit__package__list "$@"
+  elif [ $cmd = --help ]; then
+    echo "Work with iot-app-kit packages.
+
+${bold}USAGE${normal}
+  iot-app-kit package <subcommand> [<package-name>] [flags]
+
+${bold}GENERAL COMMANDS${normal}
+  build:   Build a package
+  pack:    Pack a package
+  publish: Publish a package
+
+${bold}TARGETED COMMANDS${normal}
+  list:    List the packages dependent on the package
+
+${bold}INHERITED FLAGS${normal}
+  --help   Show help for command
+
+${bold}EXAMPLES${normal}
+  $ iot-app-kit package build core
+  $ iot-app-kit package pack core-util
+  $ iot-app-kit package publish dashboard --simulate
+  $ iot-app-kit package list react-components
+    "
+    exit 0
   else
-    echo "Invalid iot-app-kit package command: $cmd"
+    echo -e ${red}${bold}"Error: Invalid iot-app-kit package command: $cmd."${normal}${no_color}
     exit 1
   fi
 }
@@ -73,83 +111,212 @@ function iot-app-kit__tarball() {
     iot-app-kit__tarball__copy "$@"
   elif [ $cmd = link ]; then
     iot-app-kit__tarball__link "$@"
+  elif [ $cmd = --help ]; then
+    echo "Work with iot-app-kit package tarballs.
+
+${bold}USAGE${normal}
+  iot-app-kit tarball <subcommand> [<tarball-name>] [flags]
+
+${bold}GENERAL COMMANDS${normal}
+  copy:    Copy a package tarball into one or more destination packages
+  link:    Link a package tarball to the package.json file for one or more destination packages
+
+${bold}INHERITED FLAGS${normal}
+  --help   Show help for command
+
+${bold}EXAMPLES${normal}
+  $ iot-app-kit tarball copy iot-app-kit-source-iotsitewise-12.5.0.tgz --destinations "dashboard react-components"
+  $ iot-app-kit tarball link iot-app-kit-source-iottwinmaker-12.5.0.tgz --destinations react-components
+    "
+    exit 0
   else
-    echo "Invalid iot-app-kit tarball command: $cmd"
+    echo -e ${red}${bold}"Error: Invalid iot-app-kit tarball command: $cmd."${normal}${no_color}
     exit 1
   fi
 }
 
 function iot-app-kit__package__build() {
+  if [ $1 = "-h" ] || [ $1 = "--help" ]; then
+    echo "Build iot-app-kit packages.
+
+${bold}USAGE${normal}
+  iot-app-kit package build [<package-name>]
+
+${bold}INHERITED FLAGS${normal}
+  --help   Show help for command
+
+${bold}EXAMPLES${normal}
+  $ iot-app-kit package build core
+    "
+    exit 0
+  fi
+
   local package=$1
+  local needs_help=false
+  while [ $# -gt 0 ]; do
+    case $1 in
+      -h | --help) needs_help=true ;;
+    esac
+    shift
+  done
+
+  echo -e ${blue}${bold}"Building package ($package)..."${normal}${no_color}
 
   npx turbo build --filter=@iot-app-kit/$package
 }
 
 # Create iot-app-kit package tarballs
 function iot-app-kit__package__pack() {
+  if [ $1 = "-h" ] || [ $1 = "--help" ]; then
+    echo "Pack iot-app-kit packages.
+
+${bold}USAGE${normal}
+  iot-app-kit package pack [<package-name>] [flags]
+
+${bold}FLAGS${normal}
+  -d, --destination   Specify a path to output the tarball
+
+${bold}INHERITED FLAGS${normal}
+  --help   Show help for command
+
+${bold}EXAMPLES${normal}
+  $ iot-app-kit package pack core
+  $ iot-app-kit package pack dashboard --destination testing/smoke-test
+    "
+    exit 0
+  fi
+
   # get the package name and then shift the argument stack
   local package=$1; shift
-
-  # get optional pack destination from flag (default is ".")
-  declare pack_destination
-  while [ $# -gt 0 ] ; do
+  local current_dir=$(pwd)
+  local destination=undefined
+  while [ $# -gt 0 ]; do
     case $1 in
-      -d | --destination) pack_destination="$2" ;;
+      -d | --destination) destination="$2" ;;
     esac
     shift
   done
 
-  # re-direct the output to avoid echoing custom prepack/postpack npm scripts
-  npm pack -w @iot-app-kit/$package --pack-destination=$pack_destination > /dev/null
+  declare pack_destination
+  if [ "$destination" = undefined ]; then
+    pack_destination=$current_dir
+  elif [ "$destination" = "testing/smoke-test" ]; then
+    pack_destination="$current_dir/testing/smoke-test"
+  else
+    pack_destination="$current_dir/packages/$destination"
+  fi
 
-  # get the filename of the newly created tarball
-  local tarball=$(find . -name "iot-app-kit-$package-*.tgz" -type f -exec basename {} \; | head -n 1)
+  echo -e ${blue}${bold}"Packing package ($package)..."${normal}${no_color}
 
-  # echo the tarball filename to allow consumer to utilize
-  echo $tarball
+  npx turbo pack --filter @iot-app-kit/$package --cwd $current_dir --output-logs=errors-only -- --pack-destination=$pack_destination
+
+  # integrity check
+  local tarball=$(get_package_tarball_filename $package)
+  if [ $(is_file_created $pack_destination/$tarball) = false ]; then
+    echo -e ${red}${bold}"Error: Package ($package) was incorrectly packaged."${normal}${no_color}
+    exit 1
+  else
+    echo -e ${green}${bold}"Success: Package ($package) was correctly packaged."${no_color}${normal}
+  fi
 }
 
 # Publish iot-app-kit packages
 function iot-app-kit__package__publish() {
+  if [ $1 = "-h" ] || [ $1 = "--help" ]; then
+    echo "Publish iot-app-kit packages.
+
+${bold}USAGE${normal}
+  iot-app-kit package publish [<package-name>] [flags]
+
+${bold}FLAGS${normal}
+  -s, --simulate   Simulate publishing the package
+
+${bold}INHERITED FLAGS${normal}
+  --help   Show help for command
+
+${bold}EXAMPLES${normal}
+  $ iot-app-kit package publish core --simulate
+    "
+    exit 0
+  fi
+
   local package=$1
 
   # get optional simulation parameter from flag
-  local is_sim=0
-  while [ $# -gt 0 ] ; do
+  local is_sim=false
+  while [ $# -gt 0 ]; do
     case $1 in
-      --simulate | -s) is_sim=1 ;;
+      --simulate | -s) is_sim=true ;;
     esac
     shift
   done
 
   # exit until supported
-  if [ $is_sim -eq 0 ]; then
-    echo "publish currently only supports simulation for testing."
-    exit 1
+  if [ "$is_sim" = false ]; then
+    echo -e ${blue}${bold}"Publishing package ($package)..."${normal}${no_color}
+    npm publish -w @iot-app-kit/$package
+    echo -e ${green}${bold}"Success: Package ($package) was successfully published."${no_color}${normal}
+  else
+    echo -e ${blue}${bold}"Publishing package ($package) (simulated)..."${normal}${no_color}
+    # create the package tarball
+    iot-app-kit package pack $package
+    local tarball=$(get_package_tarball_filename $package)
+    # get the list of consuming iot-app-kit package directories
+    local destinations=$(iot-app-kit package list $package)
+    # copy the tarball into each consuming package's directory
+    iot-app-kit tarball copy $tarball -d "$destinations"
+    # link the tarball to each consuming package's package.json
+    iot-app-kit tarball link $tarball -d "$destinations"
+    echo -e ${green}${bold}"Success: Package ($package) was successfully published (simulated)."${no_color}${normal}
   fi
-
-  # create the package tarball
-  local tarball=$(iot-app-kit package pack $package)
-  # get the list of consuming iot-app-kit package directories
-  local destinations=$(iot-app-kit package list $package)
-  # copy the tarball into each consuming package's directory
-  iot-app-kit tarball copy $tarball -d "$destinations"
-  # link the tarball to each consuming package's package.json
-  iot-app-kit tarball link $tarball -d "$destinations"
 }
 
 # List a package's iot-app-kit dependencies
 function iot-app-kit__package__list() {
+  if [ $1 = "-h" ] || [ $1 = "--help" ]; then
+    echo "List the internal dependencies of an iot-app-kit package.
+
+${bold}USAGE${normal}
+  iot-app-kit package list [<package-name>]
+
+${bold}INHERITED FLAGS${normal}
+  --help   Show help for command
+
+${bold}EXAMPLES${normal}
+  $ iot-app-kit package list core
+    "
+    exit 0
+  fi
+
   local package=$1
   # get the list of iot-app-kit packages a package depends on at runtime from the package's package.json
   local deps=$(npm pkg get dependencies --ws | jq -r 'to_entries[] | select(.value | has("@iot-app-kit/'"$package"'")) | .key')
 
-  # format json to create flat package list and echo the packages
+  # format json to create flat package list and echo the packages (e.g., core core-util dashboard)
   echo "$deps" | grep -Fx -f <(printf "%s\n" "${PACKAGE_LIST[@]}") |  awk -F'/' '{print $2}'
 }
 
 # Copy iot-app-kit package tarballs into a list of package directories
 function iot-app-kit__tarball__copy() {
+  if [ $1 = "-h" ] || [ $1 = "--help" ]; then
+    echo "Copy iot-app-kit package tarballs to one or more destination packages.
+
+${bold}USAGE${normal}
+  iot-app-kit tarball copy [<tarball-name>] [flags]
+
+${bold}FLAGS${normal}
+  -d, --destinations   List of destination packages to copy package tarball into.
+
+${bold}INHERITED FLAGS${normal}
+  --help   Show help for command
+
+${bold}EXAMPLES${normal}
+  $ iot-app-kit tarball copy iot-app-kit-core-12.5.0.tgz --destinations react-components
+  $ iot-app-kit tarball copy iot-app-kit-core-12.5.0.tgz --destinations "dashboard react-components"
+    "
+    exit 0
+  fi
+
   local tarball=$1; shift
   local package_dirs=()
 
@@ -170,12 +337,53 @@ function iot-app-kit__tarball__copy() {
   # always copy tarball into smoke-test for testing
   package_dirs+="testing/smoke-test"
 
+  for destination in $destinations; do
+    echo -e ${blue}${bold}"Copying package tarball ($tarball) into destination ($destination)..."${normal}${no_color}
+  done
+  echo -e ${blue}${bold}"Copying package tarball ($tarball) into destination (smoke-test)..."${normal}${no_color}
+
   # copy the tarball into each package directory (no echo)
   find $package_dirs -type d -maxdepth 0 -exec cp $tarball {} \; > /dev/null
+
+  # integrity check
+  for destination in $destinations; do
+    if [ $(is_file_created packages/$destination/$tarball) = false ]; then
+      echo -e ${red}${bold}"Error: Package tarball ($tarball) was incorrectly copied into destination ($destination)."${normal}${no_color}
+      exit 1
+    else
+      echo -e ${green}${bold}"Success: Package tarball ($tarball) was correctly copied into destination ($destination)."${normal}${no_color}
+    fi
+  done
+
+  if [ $(is_file_created testing/smoke-test/$tarball) = false ]; then
+    echo -e ${red}${bold}"Error: Package tarball ($tarball) was incorrectly copied into destination (smoke-test)."${normal}${no_color}
+    exit 1
+  else
+    echo -e ${green}${bold}"Success: Package tarball ($tarball) was correctly copied into destination (smoke-test)."${normal}${no_color}
+  fi
 }
 
 # Update a list of packages to install an iot-app-kit from a tarball
 function iot-app-kit__tarball__link() {
+  if [ $1 = "-h" ] || [ $1 = "--help" ]; then
+    echo "Link iot-app-kit package tarballs to the package.json of one or more destination packages.
+
+${bold}USAGE${normal}
+  iot-app-kit tarball link [<tarball-name>] [flags]
+
+${bold}FLAGS${normal}
+  -d, --destinations   List of destination packages to link package tarball to.
+
+${bold}INHERITED FLAGS${normal}
+  --help   Show help for command
+
+${bold}EXAMPLES${normal}
+  $ iot-app-kit tarball link iot-app-kit-core-12.5.0.tgz --destinations react-components
+  $ iot-app-kit tarball link iot-app-kit-core-12.5.0.tgz --destinations "dashboard react-components"
+    "
+    exit 0
+  fi
+
   local tarball=$1; shift
   # get the package name from the tarball
   local package=${tarball#iot-app-kit-}
@@ -191,10 +399,44 @@ function iot-app-kit__tarball__link() {
   done
 
   for destination in $destinations; do
+    echo -e ${blue}${bold}"Linking package tarball ($tarball) to destination ($destination)..."${normal}${no_color}
     # append the tarball file name to the destination package's files list to include it in the destination package's tarball
     npm pkg -w @iot-app-kit/$destination set files[]="$tarball"
     # update the destination package's dependencies to install the tarball instead of installing from npm
     npm pkg -w @iot-app-kit/$destination set dependencies."@iot-app-kit/$package"="file:$tarball"
   done
+
+  # integrity check
+  for destination in $destinations; do
+    if [ $(is_tarball_linked $tarball $destination) = false ]; then
+      echo -e ${red}${bold}"Error: Package tarball ($tarball) was incorrectly linked to destination ($destination)."${normal}${no_color}
+      exit 1
+    else
+      echo -e ${green}${bold}"Success: Package tarball ($tarball) was correctly linked to destination ($destination)."${normal}${no_color}
+    fi
+  done
 }
 
+function get_package_tarball_filename() {
+  local package=$1;
+  local tarball=$(find . -name "iot-app-kit-$package-*.tgz" -type f -exec basename {} \; | head -n 1)
+
+  echo $tarball
+}
+
+function is_file_created() {
+  local filename=$1
+
+  if [ -f $filename ]; then
+    echo true
+  else
+    echo false
+  fi
+}
+
+function is_tarball_linked() {
+  local tarball=$1
+  local destination=$2
+
+  npm pkg -w @iot-app-kit/$destination get files | grep -q $tarball | echo true || echo false
+}
--- prep-smoke-test-env.sh
+++ prep-smoke-test-env.sh
@@ -3,10 +3,14 @@
 # Smoke test environment setup script
 
 source ./iot-app-kit.sh
-iot-app-kit package build dashboard
+
+echo -e ${blue}${bold}"Preparing smoke smoke test environment..."${normal}${no_color}
+
 iot-app-kit package publish core --simulate
 iot-app-kit package publish core-util --simulate
 iot-app-kit package publish source-iotsitewise --simulate
 iot-app-kit package publish source-iottwinmaker --simulate
 iot-app-kit package publish react-components --simulate
 iot-app-kit package pack dashboard --destination testing/smoke-test
+
+echo -e ${green}${bold}"Success: Smoke test environment prepared."${normal}${no_color}
