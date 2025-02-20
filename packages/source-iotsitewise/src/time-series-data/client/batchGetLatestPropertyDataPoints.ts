fcdc7029ab41c408e0d9c3421db1214eac58b00f
chejimmy <chejimmy@amazon.com>
2025-02-20 09:13:04 -0800

    fix: optional property check for error metadata

--- packages/source-iotsitewise/src/time-series-data/client/batchGetLatestPropertyDataPoints.ts
+++ packages/source-iotsitewise/src/time-series-data/client/batchGetLatestPropertyDataPoints.ts
@@ -143,7 +143,7 @@ const sendRequest = ({
       Object.entries(callbackCache).forEach(([entryId, { onError }]) => {
         onError({
           entryId,
-          errorCode: e.$metadata.httpStatusCode,
+          errorCode: e.$metadata?.httpStatusCode,
           errorMessage: e.message,
         });
       });
